0e26f771920008e0b80ef17550acf00e73899a14
Chris Xue <chrisxue815@gmail.com>
2017-02-28 19:03:57 +0000

    129

---
+++ p129.py
@@ -0,0 +1,46 @@
+import unittest
+from tree import TreeNode, null
+
+
+class Solution(object):
+
+    def __init__(self):
+        self.sum = 0
+
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        self._sum(root, 0)
+        return self.sum
+
+    def _sum(self, node, parent_sum):
+        parent_sum += node.val
+
+        if node.left is None and node.right is None:
+            self.sum += parent_sum
+            return
+
+        parent_sum *= 10
+        if node.left != None:
+            self._sum(node.left, parent_sum)
+        if node.right != None:
+            self._sum(node.right, parent_sum)
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self._test([1, 2, 3])
+
+    def _test(self, vals):
+        root = TreeNode.from_array(vals)
+        self.assertEqual(Solution().sumNumbers(root), 25)
+
+
+if __name__ == '__main__':
+    unittest.main()
