1a17ebbb2d9f2a57fc83b372e1afce0b1f8f114d
Chris Xue <chrisxue815@gmail.com>
2017-03-01 19:55:27 +0000

    103

---
+++ p103.py
@@ -0,0 +1,63 @@
+import unittest
+from tree import TreeNode, null
+
+
+class Solution(object):
+
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+
+        vals = []
+        stack = []
+        stack.append(root)
+        direction = 1
+
+        while True:
+            size = len(stack)
+            if size == 0:
+                break
+
+            level_stack = []
+            level_vals = []
+
+            for _ in xrange(size):
+                node = stack.pop()
+                level_vals.append(node.val)
+
+                if direction == 1:
+                    left, right = node.left, node.right
+                else:
+                    left, right = node.right, node.left
+
+                if left != None:
+                    level_stack.append(left)
+                if right != None:
+                    level_stack.append(right)
+
+            vals.append(level_vals)
+            stack = level_stack
+            direction = -direction
+
+        return vals
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self._test([3, 9, 20, null, null, 15, 7],
+                   [[3], [20, 9], [15, 7]])
+
+    def _test(self, vals, expected):
+        root = TreeNode.from_array(vals)
+        self.assertEqual(
+            Solution().zigzagLevelOrder(root),
+            expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
