5253ab98d1c469d96cb8af317c451a4f9a23fb8d
Chris Xue <chrisxue815@gmail.com>
2017-03-01 19:15:48 +0000

    117

---
+++ p117.py
@@ -0,0 +1,60 @@
+import unittest
+from tree import TreeLinkNode, null
+
+
+# Definition for binary tree with next pointer.
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+
+class Solution(object):
+    # @param root, a tree link node
+    # @return nothing
+
+    def connect(self, root):
+        if root is None:
+            return
+
+        curr = root
+        phantom = TreeLinkNode(0)
+        prev = phantom
+
+        while True:
+            if curr.left != None:
+                prev.next = curr.left
+                if curr.right != None:
+                    curr.left.next = curr.right
+                    prev = curr.right
+                else:
+                    prev = curr.left
+            elif curr.right != None:
+                prev.next = curr.right
+                prev = curr.right
+
+            curr = curr.next
+
+            if curr is None:
+                if phantom.next is None:
+                    break
+                prev = phantom
+                curr = phantom.next
+                phantom.next = None
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        self._test([4, 2, 6, 1, 3, 5, 7])
+
+    def _test(self, vals):
+        root = TreeLinkNode.from_array(vals)
+        Solution().connect(root)
+        self.assertEqual(root.to_array_bfs_fulltree(), vals)
+
+
+if __name__ == '__main__':
+    unittest.main()
