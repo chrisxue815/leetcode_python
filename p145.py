79e9cdb52161aff425c49dade7b106fef1630918
Chris Xue <chrisxue815@gmail.com>
2017-03-04 12:11:39 +0000

    145

---
+++ p145.py
@@ -0,0 +1,37 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        vals = []
+        stack = []
+        cur = root
+
+        while cur is not None or len(stack) > 0:
+            if cur is not None:
+                stack.append(cur)
+                vals.append(cur.val)
+                cur = cur.right
+            else:
+                cur = stack.pop()
+                cur = cur.left
+
+        vals.reverse()
+        return vals
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        root = TreeNode.from_array([1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(
+            Solution().postorderTraversal(root),
+            [4, 5, 2, 6, 7, 3, 1])
+
+
+if __name__ == '__main__':
+    unittest.main()
