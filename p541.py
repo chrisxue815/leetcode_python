3eb9a9ca58afcc7d199f6259d072be74e634d26c
Chris Xue <chrisxue815@gmail.com>
2017-03-10 19:18:59 +0000

    541. Reverse String II

---
+++ p541.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        chars = list(s)
+        l = len(s)
+        left = 0
+
+        while left < l:
+            reverse_len = min(k, l - left)
+            right = left + reverse_len - 1
+            for i in xrange(reverse_len // 2):
+                tmp = chars[left + i]
+                chars[left + i] = chars[right - i]
+                chars[right - i] = tmp
+            left = right + 1 + k
+
+        return ''.join(chars)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('123456', 3, '321456')
+        self._test('1234567', 3, '3214567')
+        self._test('12345678', 3, '32145687')
+        self._test('123456789', 3, '321456987')
+        self._test('1234567890', 3, '3214569870')
+
+    def _test(self, s, k, expected):
+        self.assertEqual(Solution().reverseStr(s, k), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
