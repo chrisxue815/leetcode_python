d02c777077f0d36df18bbcde52c4daa330049fc5
Chris Xue <chrisxue815@gmail.com>
2017-03-10 18:51:28 +0000

    520. Detect Capital

---
+++ p520.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        l = len(word)
+        if l == 1:
+            return True
+
+        is_uppercase = word[1] <= 'Z'
+
+        if word[0] > 'Z' and is_uppercase:
+            return False
+
+        for c in word[2:]:
+            if (c <= 'Z') != is_uppercase:
+                return False
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('USA', True)
+        self._test('leetcode', True)
+        self._test('Google', True)
+        self._test('a', True)
+        self._test('A', True)
+        self._test('USa', False)
+        self._test('uSA', False)
+
+    def _test(self, word, expected):
+        self.assertEqual(Solution().detectCapitalUse(word), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
