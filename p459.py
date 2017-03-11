9cf2c4ec0c03faff6c766e0800eeb0f0bcbd7011
Chris Xue <chrisxue815@gmail.com>
2017-03-11 11:04:30 +0000

    459. Repeated Substring Pattern

---
+++ p459.py
@@ -0,0 +1,48 @@
+import math
+import unittest
+
+
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        l = len(s)
+        if l == 1:
+            return False
+
+        if self._repeated(s, 1):
+            return True
+
+        sqrt_l = int(math.sqrt(l))
+        for repeated_len in xrange(2, sqrt_l + 1):
+            if l % repeated_len != 0:
+                continue
+            if self._repeated(s, repeated_len):
+                return True
+            elif self._repeated(s, l / repeated_len):
+                return True
+        return False
+
+    def _repeated(self, s, repeated_len):
+        for start in xrange(repeated_len, len(s), repeated_len):
+            for i in xrange(repeated_len):
+                if s[i] != s[start + i]:
+                    return False
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('abab', True)
+        self._test('aba', False)
+        self._test('abcabcabcabc', True)
+        self._test('abcabdabcabd', True)
+
+    def _test(self, s, expected):
+        self.assertEqual(Solution().repeatedSubstringPattern(s), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
