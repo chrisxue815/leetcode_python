6a9edbda279b1b1902c6d5e43b1ed4469c8d4428
Chris Xue <chrisxue815@gmail.com>
2017-03-18 11:17:04 +0000

    367. Valid Perfect Square

---
+++ p367.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        lo = 0
+        hi = num
+
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            midsq = mid * mid
+            if midsq > num:
+                hi = mid - 1
+            elif midsq < num:
+                lo = mid + 1
+            else:
+                return True
+
+        return False
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(16, True)
+        self._test(14, False)
+
+    def _test(self, num, expected):
+        actual = Solution().isPerfectSquare(num)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
