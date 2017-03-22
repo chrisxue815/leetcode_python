e4e44fe571123ba91b3810f9e29b56a36cb5d169
Chris Xue <chrisxue815@gmail.com>
2017-03-22 19:31:08 +0000

    287. Find the Duplicate Number. Floyd's tortoise and hare

---
+++ p287_floyd.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        t = nums[0]
+        h = nums[t]
+
+        while t != h:
+            t = nums[t]
+            h = nums[nums[h]]
+
+        t = 0
+        while t != h:
+            t = nums[t]
+            h = nums[h]
+
+        return t
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 4, 5], 4)
+        self._test([5, 1, 3, 4, 2, 4], 4)
+        self._test([1, 2, 3, 4, 5, 5, 6], 5)
+        self._test([1, 3, 4, 5, 6, 6, 6], 6)
+        self._test([1, 3, 4, 5, 6, 6, 6, 7], 6)
+        self._test([1, 3, 4, 2, 1], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().findDuplicate(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
