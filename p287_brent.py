b3ad7a7735d55a91682ea6798e6ebcfcf94b1969
Chris Xue <chrisxue815@gmail.com>
2017-03-22 19:46:33 +0000

    287. Find the Duplicate Number. Brent's

---
+++ p287_brent.py
@@ -0,0 +1,50 @@
+import unittest
+
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        t = nums[0]
+        h = nums[t]
+        max_loop_length = 1
+        loop_length = 1
+
+        while t != h:
+            if loop_length == max_loop_length:
+                t = h
+                max_loop_length <<= 1
+                loop_length = 0
+            h = nums[h]
+            loop_length += 1
+
+        t = 0
+        h = 0
+        for i in xrange(loop_length):
+            h = nums[h]
+
+        while t != h:
+            t = nums[t]
+            h = nums[h]
+
+        return t
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 4, 5], 4)
+        self._test([5, 1, 3, 4, 2, 4], 4)
+        self._test([1, 2, 3, 4, 5, 5, 6], 5)
+        self._test([1, 3, 4, 5, 6, 6, 6], 6)
+        self._test([1, 3, 4, 5, 6, 6, 6, 7], 6)
+        self._test([1, 3, 4, 2, 1], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().findDuplicate(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
