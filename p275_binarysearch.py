28475499660cc4b612fddb6ea76524c93689ade9
Chris Xue <chrisxue815@gmail.com>
2017-03-26 11:37:24 +0100

    275. H-Index II. Binary search

---
+++ p275_binarysearch.py
@@ -0,0 +1,40 @@
+import unittest
+
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        lo = 0
+        hi = n - 1
+        while lo <= hi:
+            mid = lo + ((hi - lo) >> 1)
+            mid_val = citations[mid]
+            h = n - mid
+            if h > mid_val:
+                lo = mid + 1
+            elif h < mid_val:
+                hi = mid - 1
+            else:
+                return h
+        return n - lo
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 5], 3)
+        self._test([1, 2, 3, 4, 5, 6], 3)
+        self._test([1, 2, 3, 3, 4], 3)
+        self._test([1, 2, 3, 3, 3, 4], 3)
+        self._test([0], 0)
+
+    def _test(self, citations, expected):
+        actual = Solution().hIndex(citations)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
