d9a894fa2414641805ce7a2738319df247252f5d
Chris Xue <chrisxue815@gmail.com>
2017-03-26 10:42:34 +0100

    274. H-Index. Sort

---
+++ p274_sort.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        if not citations:
+            return 0
+
+        citations.sort(reverse=True)
+
+        h = 0
+        for i in citations:
+            if i > h:
+                h += 1
+            else:
+                break
+        return h
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([3, 0, 6, 1, 5], 3)
+        self._test([2, 1], 1)
+
+    def _test(self, citations, expected):
+        actual = Solution().hIndex(citations)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
