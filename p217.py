592357a594f1eb4ee611c490e906b603372cbff0
Chris Xue <chrisxue815@gmail.com>
2017-03-29 18:39:28 +0100

    217. Contains Duplicate. Hash set

---
+++ p217.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        visited = set()
+        for num in nums:
+            if num in visited:
+                return True
+            visited.add(num)
+        return False
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4], False)
+        self._test([1, 2, 3, 4, 3], True)
+
+    def _test(self, nums, expected):
+        actual = Solution().containsDuplicate(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
