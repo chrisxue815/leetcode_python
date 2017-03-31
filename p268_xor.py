bb06c16059f3af18aff6e5591164e11e6286cd34
Chris Xue <chrisxue815@gmail.com>
2017-03-31 18:42:01 +0100

    268. Missing Number. Xor

---
+++ p268_xor.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        for i, num in enumerate(nums):
+            n ^= i ^ num
+        return n
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([0, 1, 3], 2)
+        self._test([0, 1, 2], 3)
+
+    def _test(self, nums, expected):
+        actual = Solution().missingNumber(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
