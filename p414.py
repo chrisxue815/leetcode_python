3d7700d5f2e8fc9ab14d19c6de59a53521439066
Chris Xue <chrisxue815@gmail.com>
2017-04-01 14:31:35 +0100

    414. Third Maximum Number. Brute-force

---
+++ p414.py
@@ -0,0 +1,43 @@
+import sys
+import unittest
+
+
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        minint = -sys.maxint - 1
+        a = b = c = minint
+        
+        for num in nums:
+            if num > a:
+                a, b, c = num, a, b
+            elif num == a:
+                continue
+            elif num > b:
+                b, c = num, b
+            elif num == b:
+                continue
+            elif num > c:
+                c = num
+
+        if c != minint:
+            return c
+        return a
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([3, 2, 1], 1)
+        self._test([2, 1], 2)
+        self._test([2, 2, 3, 1], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().thirdMax(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
