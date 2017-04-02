023feed399382745cf78d99243b831beef0c1a2a
Chris Xue <chrisxue815@gmail.com>
2017-04-02 10:48:40 +0100

    189. Rotate Array. O(k) Auxiliary array

---
+++ p189_aux_array_k.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        if k < 0:
+            k += n
+        m = n - k
+        if k < m:
+            aux = nums[m:]
+            nums[k:] = nums[:m]
+            nums[:k] = aux
+        else:
+            aux = nums[:m]
+            nums[:k] = nums[m:]
+            nums[k:] = aux
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], 10, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], -4, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], 4, [4, 5, 6, 7, 1, 2, 3])
+
+    def _test(self, nums, k, expected):
+        Solution().rotate(nums, k)
+        self.assertEqual(nums, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
