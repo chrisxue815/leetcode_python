30a9a9e466a3e614dc47493b7c3b421efedf6379
Chris Xue <chrisxue815@gmail.com>
2017-04-02 11:44:59 +0100

    189. Rotate Array. Rotate in-place, calculate number of iterations (greatest common divisor of n and k) beforehand

---
+++ p189_inplace_gcd.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+def gcd_euclid(a, b):
+    if a == 0 or b == 0:
+        return 0
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+        n = len(nums)
+        k %= n
+        if k == 0:
+            return
+        if k < 0:
+            k += n
+
+        gcd = gcd_euclid(n, k)
+        inner_loops = n / gcd
+
+        for i in xrange(gcd):
+            index = i
+            prev = nums[index]
+            for j in xrange(inner_loops):
+                index = (index + k) % n
+                nums[index], prev = prev, nums[index]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], 10, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], -4, [5, 6, 7, 1, 2, 3, 4])
+        self._test([1, 2, 3, 4, 5, 6, 7], 4, [4, 5, 6, 7, 1, 2, 3])
+
+    def _test(self, nums, k, expected):
+        Solution().rotate(nums, k)
+        self.assertEqual(nums, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
