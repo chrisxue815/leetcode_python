7559cac64ee0fac710920deb69d79090b9743f35
Chris Xue <chrisxue815@gmail.com>
2017-04-04 19:25:16 +0100

    53. Maximum Subarray. Greedy

---
+++ p053_greedy.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        max_sum = nums[0]
+        cur_sum = max_sum
+
+        for num in nums[1:]:
+            if cur_sum <= 0:
+                cur_sum = num
+            else:
+                cur_sum += num
+
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+
+        return max_sum
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6)
+        self._test([-2, -1], -1)
+
+    def _test(self, nums, expected):
+        actual = Solution().maxSubArray(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
