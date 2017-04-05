eacee4836a23cabd47aff3bae4a86a4688f56bfb
Chris Xue <chrisxue815@gmail.com>
2017-04-05 19:25:53 +0100

    442. Find All Duplicates in an Array. In-place counting

---
+++ p442_inplace_counting.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        duplicates = []
+
+        for i in xrange(n):
+            num = nums[i]
+            if num <= 0:
+                continue
+            nums[i] = 0
+
+            while True:
+                next_num = nums[num - 1]
+                if next_num < 0:
+                    duplicates.append(num)
+                    break
+                elif next_num == 0:
+                    nums[num - 1] = -1
+                    break
+                else:
+                    nums[num - 1] = -1
+                    num = next_num
+
+        return duplicates
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([4, 3, 2, 7, 8, 2, 3, 1], [2, 3])
+        self._test([2, 1], [])
+        self._test([3, 3, 1], [3])
+        self._test([1], [])
+        self._test([1, 1], [1])
+        self._test([2, 1, 1], [1])
+
+    def _test(self, nums, expected):
+        actual = Solution().findDuplicates(nums)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
