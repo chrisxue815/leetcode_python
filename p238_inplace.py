c794350e1a318da57ad1bdad70ed1efb4f3ae4e5
Chris Xue <chrisxue815@gmail.com>
2017-04-08 11:38:29 +0100

    238. Product of Array Except Self. In-place

---
+++ p238_inplace.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        last = n - 1
+
+        product = 1
+        products = [1] * n
+        for i in xrange(last):
+            product *= nums[i]
+            products[i + 1] = product
+
+        product = 1
+        for i in xrange(last, 0, -1):
+            product *= nums[i]
+            products[i - 1] *= product
+
+        return products
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4], [24, 12, 8, 6])
+
+    def _test(self, nums, expected):
+        actual = Solution().productExceptSelf(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
