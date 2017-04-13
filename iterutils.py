31a24df04c429c1068382b34873fa066a7024b64
Chris Xue <chrisxue815@gmail.com>
2017-04-13 19:47:22 +0100

    Combinations. Include and exclude elements

--- iterutils.py
+++ iterutils.py
@@ -12,11 +12,31 @@ def combinations_fix_elements(pool, r=None):
 def _combinations_fix_elements(pool, r, result, combination, start_index):
     if r == 0:
         result.append(tuple(combination))
-    else:
+    elif r > 0:
         for i in xrange(start_index, len(pool)):
             combination.append(pool[i])
             _combinations_fix_elements(pool, r - 1, result, combination, i + 1)
             combination.pop()
+
+
+def combinations_include_exclude(pool, r=None):
+    r = len(pool) if r is None else r
+    result = []
+    _combinations_include_exclude(pool, r, result, [], 0)
+    return result
+
+
+def _combinations_include_exclude(pool, r, result, combination, index):
+    if r == 0:
+        result.append(tuple(combination))
+    elif r > 0 and index < len(pool):
+        # include
+        combination.append(pool[index])
+        _combinations_include_exclude(pool, r - 1, result, combination, index + 1)
+        combination.pop()
+
+        # exclude
+        _combinations_include_exclude(pool, r, result, combination, index + 1)
 
 
 def permutations_dummy(pool, r=None):
@@ -30,29 +50,26 @@ class Test(unittest.TestCase):
             range(3),
         ]
 
-        cls.combination_funcs = [
-            combinations_fix_elements,
-        ]
-
-        cls.permutation_funcs = [
-            permutations_dummy,
-        ]
-
     def test_combinations(self):
+        self._test_combinations(combinations_fix_elements)
+        self._test_combinations(combinations_include_exclude)
+
+    def _test_combinations(self, func):
         for pool in Test.pools:
-            for r in xrange(len(pool) + 1):
+            for r in xrange(len(pool) + 2):
+                actual = func(pool, r)
                 expected = itertools.combinations(pool, r)
-                for func in Test.combination_funcs:
-                    actual = func(pool, r)
-                    self.assertItemsEqual(actual, expected)
+                self.assertItemsEqual(actual, expected)
 
     def test_permutations(self):
+        self._test_permutations(permutations_dummy)
+
+    def _test_permutations(self, func):
         for pool in Test.pools:
-            for r in xrange(len(pool) + 1):
+            for r in xrange(len(pool) + 2):
+                actual = func(pool, r)
                 expected = itertools.permutations(pool, r)
-                for func in Test.permutation_funcs:
-                    actual = func(pool, r)
-                    self.assertItemsEqual(actual, expected)
+                self.assertItemsEqual(actual, expected)
 
 
 if __name__ == '__main__':
