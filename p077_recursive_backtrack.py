63761f5b64c3ea654117aa221a0e828e2282d548
Chris Xue <chrisxue815@gmail.com>
2017-04-15 15:39:01 +0100

    77. Combinations. Fix elements and recur

---
+++ p077_recursive_backtrack.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class Solution(object):
+    def __init__(self):
+        self.end = 0
+        self.combination = []
+        self.result = []
+
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        self.end = n + 1
+        self._combine(k, 1)
+        return self.result
+
+    def _combine(self, k, start):
+        if k == 0:
+            self.result.append(list(self.combination))
+        elif start < self.end:
+            for i in xrange(start, self.end):
+                self.combination.append(i)
+                self._combine(k - 1, i + 1)
+                self.combination.pop()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(4, 2, [
+            [2, 4],
+            [3, 4],
+            [2, 3],
+            [1, 2],
+            [1, 3],
+            [1, 4],
+        ])
+
+    def _test(self, n, k, expected):
+        actual = Solution().combine(n, k)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
