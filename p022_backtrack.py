54d9238158039c8a76622932fdce7edb896015b7
Chris Xue <chrisxue815@gmail.com>
2017-04-21 19:27:48 +0100

    22. Generate Parentheses. Recursive backtracking

---
+++ p022_backtrack.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+class Solution(object):
+    def __init__(self):
+        self.result = []
+
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        self._generate('', n, n, 0)
+        return self.result
+
+    def _generate(self, s, left, right, sum_):
+        if left == 0:
+            if right == 0:
+                self.result.append(s)
+            else:
+                self.result.append(s + ')' * right)
+        else:
+            self._generate(s + '(', left - 1, right, sum_ + 1)
+            if sum_ > 0:
+                self._generate(s + ')', left, right - 1, sum_ - 1)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(3, [
+            "((()))",
+            "(()())",
+            "(())()",
+            "()(())",
+            "()()()"
+        ])
+
+    def _test(self, n, expected):
+        actual = Solution().generateParenthesis(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
