6b9785ef8e372eacd85c6fe3843c3a3890495499
Chris Xue <chrisxue815@gmail.com>
2017-04-22 11:56:27 +0100

    89. Gray Code. Iteration

---
+++ p089_iteration.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = [0]
+        for i in xrange(n):
+            prefix = 1 << i
+            for j in xrange(len(result) - 1, -1, -1):
+                result.append(prefix + result[j])
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, [0])
+        self._test(1, [0, 1])
+        self._test(2, [0, 1, 3, 2])
+
+    def _test(self, n, expected):
+        actual = Solution().grayCode(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
