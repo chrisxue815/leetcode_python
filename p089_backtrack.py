0868120766b02edc7ea10341ca58d12c77129504
Chris Xue <chrisxue815@gmail.com>
2017-04-22 12:33:39 +0100

    89. Gray Code. Backtracking

---
+++ p089_backtrack.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution(object):
+    def __init__(self):
+        self.num = 0
+        self.result = []
+
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        self._backtrack(n)
+        return self.result
+
+    def _backtrack(self, n):
+        if n == 0:
+            self.result.append(self.num)
+        else:
+            n -= 1
+            self._backtrack(n)
+            self.num ^= 1 << n
+            self._backtrack(n)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, [0])
+        self._test(1, [0, 1])
+        self._test(2, [0, 1, 3, 2])
+
+    def _test(self, n, expected):
+        actual = Solution().grayCode(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
