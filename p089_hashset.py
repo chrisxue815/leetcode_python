82d8d22775a28ce9659c2832a969128ef4c38f2a
Chris Xue <chrisxue815@gmail.com>
2017-04-22 10:26:12 +0100

    89. Gray Code. Hash set

---
+++ p089_hashset.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        last = 0
+        result = [last]
+        visited = set(result)
+        while True:
+            for i in xrange(n):
+                curr = last ^ (1 << i)
+                if curr not in visited:
+                    visited.add(curr)
+                    result.append(curr)
+                    last = curr
+                    break
+            else:
+                return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, [0])
+        self._test(1, [0, 1])
+        self._test(2, [0, 1, 3, 2])
+
+    def _test(self, n, expected):
+        actual = Solution().grayCode(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
