42ed2f9629ce6001fe04d8c5a9a0e5517e23a665
Chris Xue <chrisxue815@gmail.com>
2017-04-23 12:17:23 +0100

    17. Letter Combinations of a Phone Number. Iterative backtracking

---
+++ p017_iterative_backtrack.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        digit_letters = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+        zero = ord('0')
+        digits = [ord(i) - zero for i in digits]
+        n = len(digits)
+
+        result = []
+        indices = [0] * n
+        result.append(''.join(digit_letters[digits[i]][0] for i in xrange(n)))
+
+        while True:
+            for i in xrange(n - 1, -1, -1):
+                indices[i] += 1
+                j = indices[i]
+                if j < len(digit_letters[digits[i]]):
+                    result.append(''.join(digit_letters[digits[i]][indices[i]] for i in xrange(n)))
+                    break
+                else:
+                    indices[i] = 0
+            else:
+                return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test("23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+        self._test("", [])
+
+    def _test(self, digits, expected):
+        actual = Solution().letterCombinations(digits)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
