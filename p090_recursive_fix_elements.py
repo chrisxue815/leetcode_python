f9152412b2b7cbdc657a3702c140d2e0508c55f9
Chris Xue <chrisxue815@gmail.com>
2017-04-24 19:43:17 +0100

    90. Subsets II. Recursive backtracking

---
+++ p090_recursive_fix_elements.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class Solution(object):
+    def __init__(self):
+        self.nums = None
+        self.n = 0
+        self.combination = []
+        self.result = [[]]
+
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        self.nums = nums
+        self.n = len(nums)
+
+        self._subsets(0)
+
+        return self.result
+
+    def _subsets(self, start):
+        for i in xrange(start, self.n):
+            if i > start and self.nums[i] == self.nums[i - 1]:
+                continue
+            self.combination.append(self.nums[i])
+            self.result.append(list(self.combination))
+            self._subsets(i + 1)
+            self.combination.pop()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 2], [
+            [2],
+            [1],
+            [1, 2, 2],
+            [2, 2],
+            [1, 2],
+            []
+        ])
+
+    def _test(self, nums, expected):
+        actual = Solution().subsetsWithDup(nums)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
