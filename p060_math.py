dd17b0432129f8a0369b84abc2cf4f5a43e8e3ef
Chris Xue <chrisxue815@gmail.com>
2017-04-30 11:20:32 +0100

    60. Permutation Sequence. Math

---
+++ p060_math.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        k -= 1
+        factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][:n]
+        candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9][:n]
+        result = ''
+        for factorial in reversed(factorials):
+            index = k // factorial
+            result += str(candidates[index])
+            candidates.pop(index)
+            k -= factorial * index
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(3, 5, '312')
+
+    def _test(self, n, k, expected):
+        actual = Solution().getPermutation(n, k)
+        print(actual)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
