c42fbca55aba573007a983e08793e71670768494
Chris Xue <chrisxue815@gmail.com>
2017-05-06 10:16:30 +0100

    557. Reverse Words in a String III. String

---
+++ p557.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        n = len(s)
+        s = list(s)
+        lo = 0
+        for hi in xrange(len(s)):
+            if s[hi] == ' ':
+                s[lo:hi] = reversed(s[lo:hi])
+                lo = hi + 1
+        if lo < n:
+            s[lo:n] = reversed(s[lo:n])
+
+        return ''.join(s)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")
+
+    def _test(self, s, expected):
+        actual = Solution().reverseWords(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
