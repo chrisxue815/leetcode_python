e5636cbca04e68a3d08b9c96d2bf0feb35b4f049
Chris Xue <chrisxue815@gmail.com>
2017-05-08 12:47:16 +0100

    151. Reverse Words in a String. Split

---
+++ p151_split.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ' '.join(reversed(s.split()))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test("  the sky is    blue   ", "blue is sky the")
+        self._test("     ", "")
+
+    def _test(self, s, expected):
+        actual = Solution().reverseWords(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
