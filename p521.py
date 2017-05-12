ccdd558ac967641c61485ce49b095d44ab520e72
Chris Xue <chrisxue815@gmail.com>
2017-05-12 18:59:32 +0100

    521. Longest Uncommon Subsequence I. Comprehension

---
+++ p521.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        na = len(a)
+        nb = len(b)
+
+        if na > nb:
+            return na
+        elif na < nb:
+            return nb
+        elif a == b:
+            return -1
+        return na
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('aba', 'cdc', 3)
+        self._test('abcd', 'abc', 4)
+        self._test('abc', 'dbc', 3)
+        self._test('abc', 'abc', -1)
+
+    def _test(self, a, b, expected):
+        actual = Solution().findLUSlength(a, b)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
