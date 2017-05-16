5b5773c5d4b765e66c873962cf90bead2aa8790a
Chris Xue <chrisxue815@gmail.com>
2017-05-16 18:57:52 +0100

    434. Number of Segments in a String. Split

---
+++ p434_split.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.split())
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('Hello, my name is John', 5)
+        self._test('  Hello, my  name is  John  ', 5)
+
+    def _test(self, s, expected):
+        actual = Solution().countSegments(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
