9e1f4323f3c07a3392752ff5f7c4cabfe9140e96
Chris Xue <chrisxue815@gmail.com>
2017-05-16 19:42:37 +0100

    49. Group Anagrams. Counting sort and hash table

---
+++ p049_counting_sort.py
@@ -0,0 +1,46 @@
+import collections
+import unittest
+
+_a = ord('a')
+
+
+def _count_sort(s):
+    counts = [0] * 26
+    for ch in s:
+        counts[ord(ch) - _a] += 1
+
+    s = ''
+    for i in xrange(26):
+        s += chr(i + _a) * counts[i]
+    return s
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = collections.defaultdict(list)
+
+        for s in strs:
+            anagrams[_count_sort(s)].append(s)
+
+        return anagrams.values()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(["eat", "tea", "tan", "ate", "nat", "bat"], [
+            ["eat", "tea", "ate"],
+            ["tan", "nat"],
+            ["bat"]
+        ])
+
+    def _test(self, strs, expected):
+        actual = Solution().groupAnagrams(strs)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
