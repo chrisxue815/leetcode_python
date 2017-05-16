a6d5c6f2938786ab34a9f79d435a85ecd1a7e3c4
Chris Xue <chrisxue815@gmail.com>
2017-05-16 19:18:47 +0100

    434. Number of Segments in a String. Iterative

---
+++ p434_iterative.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        was_space = True
+        count = 0
+
+        for ch in s:
+            if ch != ' ':
+                if was_space:
+                    count += 1
+                was_space = False
+            else:
+                was_space = True
+
+        return count
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('Hello, my name is John', 5)
+        self._test('  Hello, my  name is  John  ', 5)
+
+    def _test(self, s, expected):
+        actual = Solution().countSegments(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
