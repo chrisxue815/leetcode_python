6418333123d408c030f12a6dbe39e960ee718afd
Chris Xue <chrisxue815@gmail.com>
2017-05-17 18:43:06 +0100

    49. Group Anagrams. Built-in sort and hash table

---
+++ p049_builtin_sort.py
@@ -0,0 +1,33 @@
+import collections
+import unittest
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams = collections.defaultdict(list)
+
+        for s in strs:
+            anagrams[''.join((sorted(s)))].append(s)
+
+        return anagrams.values()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(["eat", "tea", "tan", "ate", "nat", "bat"], [
+            ["eat", "tea", "ate"],
+            ["tan", "nat"],
+            ["bat"]
+        ])
+
+    def _test(self, strs, expected):
+        actual = Solution().groupAnagrams(strs)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
