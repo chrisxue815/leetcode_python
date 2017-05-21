1badc190b3b2b33134daead077caa2b998c87cc5
Chris Xue <chrisxue815@gmail.com>
2017-05-21 14:03:56 +0100

    503. Next Greater Element II. Stack

---
+++ p503.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = [-1] * len(nums)
+        stack = []
+
+        for i in xrange(len(nums)):
+            num = nums[i]
+            while stack and num > nums[stack[-1]]:
+                result[stack.pop()] = num
+            stack.append(i)
+
+        i = 1
+        while i < len(stack) and stack[i] == stack[0]:
+            i += 1
+        stack = stack[i:]
+
+        for num in nums:
+            while stack:
+                if num <= nums[stack[-1]]:
+                    break
+                result[stack.pop()] = num
+            else:
+                break
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 1], [2, -1, 2])
+
+    def _test(self, nums, expected):
+        actual = Solution().nextGreaterElements(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
