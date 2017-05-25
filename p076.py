1ee94c52dc5241f76599d09492938dd32cef9724
Chris Xue <chrisxue815@gmail.com>
2017-05-25 20:32:03 +0100

    76. Minimum Window Substring. Improving performance

--- p076.py
+++ p076.py
@@ -11,39 +11,46 @@ class Solution(object):
         if not t or len(s) < len(t):
             return ''
 
+        if len(t) == 1:
+            return t if t in s else ''
+
         counts = [0] * 128
 
         for ch in t:
             counts[ord(ch)] += 1
 
         lo = 0
+        while lo < len(s) and counts[ord(s[lo])] == 0:
+            lo += 1
+
         num_pending = len(t)
         min_lo = 0
         min_len = len(s) + 1
 
-        for hi in xrange(len(s)):
+        for hi in xrange(lo, len(s)):
             ch = ord(s[hi])
 
-            if counts[ch] > 0:
-                num_pending -= 1
             counts[ch] -= 1
+            if counts[ch] >= 0:
+                num_pending -= 1
 
-            while num_pending == 0:
+            if num_pending == 0:
+                while True:
+                    ch = ord(s[lo])
+                    lo += 1
+                    counts[ch] += 1
+                    if counts[ch] == 1:
+                        num_pending = 1
+                        break
+
                 new_len = hi - lo
                 if new_len < min_len:
                     min_len = new_len
                     min_lo = lo
 
-                ch = ord(s[lo])
-                lo += 1
-
-                if counts[ch] == 0:
-                    num_pending += 1
-                counts[ch] += 1
-
         if min_len > len(s):
             return ''
-        return s[min_lo:min_lo + min_len + 1]
+        return s[min_lo - 1:min_lo + min_len + 1]
 
 
 class Test(unittest.TestCase):
