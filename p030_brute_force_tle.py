f199a60ea20b7774f6a07b40774b152e9e9a639a
Chris Xue <chrisxue815@gmail.com>
2017-05-27 19:19:04 +0100

    30. Substring with Concatenation of All Words. Brute-force, TLE

---
+++ p030_brute_force_tle.py
@@ -0,0 +1,40 @@
+import unittest
+import collections
+
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        indices = []
+        sub_len = len(words[0])
+        total_len = sub_len * len(words)
+        words = collections.Counter(words)
+
+        for i in xrange(len(s) - total_len + 1):
+            pending = words.copy()
+            for j in xrange(i, i + total_len, sub_len):
+                sub = s[j:j + sub_len]
+                if pending[sub] == 0:
+                    break
+                pending[sub] -= 1
+            else:
+                indices.append(i)
+        return indices
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('barfoothefoobarman', ['foo', 'bar'], [0, 9])
+        self._test('wordgoodgoodgoodbestword', ["word", "good", "best", "good"], [8])
+
+    def _test(self, s, words, expected):
+        actual = Solution().findSubstring(s, words)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
