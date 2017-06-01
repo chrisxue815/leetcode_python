bb5f5796d53e943d5a295514d2fe990e4e71e635
Chris Xue <chrisxue815@gmail.com>
2017-06-01 18:33:56 +0100

    137. Single Number II. Hash table

---
+++ p137_hash.py
@@ -0,0 +1,27 @@
+import unittest
+import collections
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counter = collections.Counter(nums)
+        for k, v in counter.items():
+            if v == 1:
+                return k
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([3, 3, 3, 1, 5, 5, 5], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().singleNumber(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
