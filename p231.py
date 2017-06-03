c14fd5d8fdc39722f095691ec7482a46f47c1ccc
Chris Xue <chrisxue815@gmail.com>
2017-06-03 14:22:07 +0100

    231. Power of Two

---
+++ p231.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and not (n & (n - 1))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(1, True)
+        self._test(2, True)
+        self._test(3, False)
+        self._test(4, True)
+        self._test(0, True)
+
+    def _test(self, n, expected):
+        actual = Solution().isPowerOfTwo(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
