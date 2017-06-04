cc4de3148cb4d427f7224b6aebfea449a3efc51f
Chris Xue <chrisxue815@gmail.com>
2017-06-04 15:33:32 +0100

    201. Bitwise AND of Numbers Range. Shift

---
+++ p201.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        shift = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            shift += 1
+        return m << shift
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(5, 7, 4)
+        self._test(1, 3, 0)
+        self._test(5, 5, 5)
+        self._test(10, 11, 10)
+
+    def _test(self, m, n, expected):
+        actual = Solution().rangeBitwiseAnd(m, n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
