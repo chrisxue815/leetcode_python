985f0a35e883e9fc18ed534c939a0b352a46bbfc
Chris Xue <chrisxue815@gmail.com>
2017-06-05 18:45:20 +0100

    187. Repeated DNA Sequences. Bit manipulation and hash table

---
+++ p187.py
@@ -0,0 +1,57 @@
+import unittest
+
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) < 11:
+            return []
+
+        chars = ['A', 'C', 'G', 'T']
+        nums = [0] * 20
+        nums[2] = 1
+        nums[6] = 2
+        nums[19] = 3
+
+        num = 0
+        for ch in s[9::-1]:
+            num = num << 2 | nums[ord(ch) - ord('A')]
+
+        dupes = set()
+        existing = set([num])
+        for ch in s[10:]:
+            num = num >> 2 | nums[ord(ch) - ord('A')] << 18
+            if num in existing:
+                dupes.add(num)
+            existing.add(num)
+
+        result = [None] * len(dupes)
+        i = 0
+        for num in dupes:
+            s = []
+            for _ in xrange(10):
+                s.append(chars[num & 3])
+                num >>= 2
+            result[i] = ''.join(s)
+            i += 1
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT', [
+            'AAAAACCCCC',
+            'CCCCCAAAAA',
+        ])
+
+    def _test(self, s, expected):
+        actual = Solution().findRepeatedDnaSequences(s)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
