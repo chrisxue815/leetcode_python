da225a7111219baa1fe2b5c12402e0757fa1e945
Chris Xue <chrisxue815@gmail.com>
2017-06-06 18:52:28 +0100

    397. Integer Replacement

---
+++ p397.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution(object):
+    def integerReplacement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while n != 1:
+            if not n & 1:
+                n >>= 1
+                count += 1
+            elif n == 3 or n & 3 == 1:
+                n >>= 1
+                count += 2
+            else:
+                n += 1
+                count += 1
+        return count
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(8, 3)
+        self._test(7, 4)
+        self._test(15, 5)
+        self._test(3, 2)
+        self._test(100000000, 31)
+        self._test(1234, 14)
+
+    def _test(self, n, expected):
+        actual = Solution().integerReplacement(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
