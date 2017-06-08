e8ecd2c2bea1f65b8b44feda88258a7b89d15c0d
Chris Xue <chrisxue815@gmail.com>
2017-06-08 19:25:01 +0100

    316. Remove Duplicate Letters. Greedy

---
+++ p316.py
@@ -0,0 +1,63 @@
+import unittest
+
+
+def _find_max_possible_index(s, count, counts):
+    counts = list(counts)
+    for i in xrange(len(s) - 1, -1, -1):
+        ch = ord(s[i])
+        if counts[ch]:
+            counts[ch] = 0
+            count -= 1
+            if count == 0:
+                return i
+
+
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        counts = [0] * (ord('z') + 1)
+
+        for ch in s:
+            counts[ord(ch)] = 1
+
+        count = sum(counts)
+        hi = _find_max_possible_index(s, count, counts)
+        lo = 0
+
+        for _ in xrange(count):
+            min_ch = 256
+            min_i = 0
+            for i in xrange(lo, hi + 1):
+                ch = ord(s[i])
+                if counts[ch] and ch < min_ch:
+                    min_ch = ch
+                    min_i = i
+
+            result.append(chr(min_ch))
+            counts[min_ch] = 0
+            count -= 1
+            lo = min_i + 1
+
+            if min_ch == ord(s[hi]):
+                hi = _find_max_possible_index(s, count, counts)
+
+        return ''.join(result)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('bcab', 'bca')
+        self._test('bcabc', 'abc')
+        self._test('cbacdcbc', 'acdb')
+
+    def _test(self, s, expected):
+        actual = Solution().removeDuplicateLetters(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
