cacf53c21e2d5be9eff51e0346eec2a08b3bb77e
Chris Xue <chrisxue815@gmail.com>
2017-06-10 18:15:03 +0100

    500. Keyboard Row

---
+++ p500.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        rows = [0] * ord('a') + [3, 4, 4, 3, 2, 3, 3, 3, 2, 3, 3, 3, 4, 4, 2, 2, 2, 2, 3, 2, 2, 4, 2, 4, 2, 4]
+        ret = []
+        for word in words:
+            if word:
+                word_lower = word.lower()
+                row = rows[ord(word_lower[0])]
+                if all(rows[ord(ch)] == row for ch in word_lower):
+                    ret.append(word)
+        return ret
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(['Hello', 'Alaska', 'Dad', 'Peace'], ['Alaska', 'Dad'])
+
+    def _test(self, words, expected):
+        actual = Solution().findWords(words)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
