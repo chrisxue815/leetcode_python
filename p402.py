66bfdd1ea7966e816eafd0d1bfee218b52210f59
Chris Xue <chrisxue815@gmail.com>
2017-06-10 15:06:35 +0100

    402. Remove K Digits. Stack

---
+++ p402.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        if not k:
+            return num
+        n = len(num)
+        stack = []
+        for i in xrange(n):
+            digit = num[i]
+            while k and stack and ord(digit) < ord(stack[-1]):
+                stack.pop()
+                k -= 1
+            if digit != '0' or stack:
+                stack.append(digit)
+        while k and stack:
+            stack.pop()
+            k -= 1
+        return ''.join(stack) if stack else '0'
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('1432219', 3, '1219')
+        self._test('10200', 1, '200')
+        self._test('10', 2, '0')
+        self._test('10', 1, '0')
+        self._test('123', 1, '12')
+
+    def _test(self, num, k, expected):
+        actual = Solution().removeKdigits(num, k)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
