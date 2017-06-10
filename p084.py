67b7f880554bb075751a88a267554fd1db577a54
Chris Xue <chrisxue815@gmail.com>
2017-06-10 16:47:33 +0100

    84. Largest Rectangle in Histogram. Stack

---
+++ p084.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        n = len(heights)
+        max_ = 0
+        stack = []
+        for i in xrange(n):
+            height = heights[i]
+            left_index = i
+            while stack and stack[-1][0] >= height:
+                left_height, left_index = stack.pop()
+                area = left_height * (i - left_index)
+                if area > max_:
+                    max_ = area
+            stack.append((height, left_index))
+        for height, index in stack:
+            area = height * (n - index)
+            if area > max_:
+                max_ = area
+
+        return max_
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([2, 1, 5, 6, 2, 3], 10)
+        self._test([1], 1)
+        self._test([2, 1, 2], 3)
+
+    def _test(self, heights, expected):
+        actual = Solution().largestRectangleArea(heights)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
