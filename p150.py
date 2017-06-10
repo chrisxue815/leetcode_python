5de4326a85b2904b2c72aa472dd32ee23514601e
Chris Xue <chrisxue815@gmail.com>
2017-06-10 13:35:02 +0100

    150. Evaluate Reverse Polish Notation. Stack

---
+++ p150.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif token == '-':
+                stack.append(-stack.pop() + stack.pop())
+            elif token == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif token == '/':
+                op = stack.pop()
+                stack.append(int(float(stack.pop()) / op))
+            else:
+                stack.append(int(token))
+        return stack.pop()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(['2', '1', '+', '3', '*'], 9)
+        self._test(['4', '13', '5', '/', '+'], 6)
+        self._test(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+'], 22)
+
+    def _test(self, tokens, expected):
+        actual = Solution().evalRPN(tokens)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
