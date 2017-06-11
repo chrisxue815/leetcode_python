c7610e335fc6cc9bb087a8ecfadd71e9549586c5
Chris Xue <chrisxue815@gmail.com>
2017-06-11 11:33:26 +0100

    347. Top K Frequent Elements. Hash table, maxheap. Built-in functions

---
+++ p347_builtin.py
@@ -0,0 +1,26 @@
+import unittest
+import collections
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        return nums if k == len(nums) else [num for num, _ in collections.Counter(nums).most_common(k)]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 1, 1, 2, 2, 3], 2, [1, 2])
+        self._test([1], 1, [1])
+
+    def _test(self, nums, k, expected):
+        actual = Solution().topKFrequent(nums, k)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
