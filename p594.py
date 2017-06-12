72d9c5e7b891ac81c4d3a2c9d2680ab38c29a57f
Chris Xue <chrisxue815@gmail.com>
2017-06-12 18:26:35 +0100

    594. Longest Harmonious Subsequence. Hash table

---
+++ p594.py
@@ -0,0 +1,32 @@
+import unittest
+import collections
+
+
+class Solution(object):
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_len = 0
+        counter = collections.Counter(nums)
+        for num, count1 in counter.items():
+            count2 = counter[num + 1]
+            if count2:
+                length = count1 + count2
+                if length > max_len:
+                    max_len = length
+        return max_len
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 3, 2, 2, 5, 2, 3, 7], 5)
+
+    def _test(self, wall, expected):
+        actual = Solution().findLHS(wall)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
