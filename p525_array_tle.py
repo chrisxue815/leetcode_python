34be21749a0e42563c2f1c6912a2ae2a7c26091c
Chris Xue <chrisxue815@gmail.com>
2017-06-12 20:13:20 +0100

    525. Contiguous Array. Array, TLE

---
+++ p525_array_tle.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+def max_length(sums, lo, hi):
+    sum_ = (sums[hi] - sums[lo]) << 1
+    length = hi - lo
+    if sum_ > length:
+        more = 1
+    elif sum_ < length:
+        more = 0
+    else:
+        return length
+    if sums[lo] == more:
+        return max_length(sums, lo + 1, hi)
+    elif sums[hi] == more:
+        return max_length(sums, lo, hi - 1)
+    else:
+        return max(max_length(sums, lo + 1, hi), max_length(sums, lo, hi - 1))
+
+
+class Solution(object):
+    def findMaxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        sums = [0] * (n + 1)
+        sum_ = 0
+        for i in xrange(n):
+            sum_ += nums[i]
+            sums[i + 1] = sum_
+        return max_length(sums, 0, n)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([0, 1], 2)
+        self._test([0, 1, 0], 2)
+        self._test([1, 1, 0, 1, 0], 4)
+        self._test([1, 0, 1, 0, 1], 4)
+        self._test([0, 1, 1, 0, 1], 4)
+        self._test([0, 1, 1, 1, 1, 0, 0], 4)
+        self._test([0, 1, 1, 1, 0], 2)
+        self._test([0, 1, 0, 0, 1, 1, 1, 1, 0], 6)
+        self._test([0, 1, 1, 1, 1, 0, 0, 1, 0], 6)
+
+    def _test(self, nums, expected):
+        actual = Solution().findMaxLength(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
