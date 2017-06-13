55afda8ac0cec2ee1aef33b9d97b95b29f7852fd
Chris Xue <chrisxue815@gmail.com>
2017-06-13 19:15:30 +0100

    438. Find All Anagrams in a String. Array counting, sliding window substring

---
+++ p438.py
@@ -0,0 +1,44 @@
+import unittest
+
+_a = ord('a')
+
+
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        np = len(p)
+        ret = []
+        cp = [0] * 26
+        for ch in p:
+            cp[ord(ch) - _a] += 1
+        cs = [0] * 26
+        for ch in s[:np]:
+            cs[ord(ch) - _a] += 1
+        if cs == cp:
+            ret.append(0)
+        lo = 0
+        for hi in xrange(np, len(s)):
+            cs[ord(s[lo]) - _a] -= 1
+            cs[ord(s[hi]) - _a] += 1
+            lo += 1
+            if cs == cp:
+                ret.append(lo)
+        return ret
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('cbaebabacd', 'abc', [0, 6])
+        self._test('abab', 'ab', [0, 1, 2])
+
+    def _test(self, s, p, expected):
+        actual = Solution().findAnagrams(s, p)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
