c372f77e84d281797256fe08b25233fe0f6619a0
Chris Xue <chrisxue815@gmail.com>
2017-06-16 21:08:35 +0100

    203. Remove Linked List Elements. Two pointers

---
+++ p203_two_pointers.py
@@ -0,0 +1,47 @@
+import unittest
+from linkedlist import ListNode
+
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head and head.val == val:
+            head = head.next
+
+        if not head:
+            return None
+
+        result = head
+        prev = head
+        head = head.next
+        while head:
+            if head.val == val:
+                prev.next = head.next
+            else:
+                prev = head
+            head = head.next
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 6, 3, 4, 5, 6], 6, [1, 2, 3, 4, 5])
+        self._test([1], 6, [1])
+        self._test([6], 6, [])
+        self._test([], 6, [])
+
+    def _test(self, head, x, expected):
+        head = ListNode.from_array(head)
+
+        actual = Solution().removeElements(head, x)
+
+        actual = ListNode.to_array(actual)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
