e52b33907408abf397432016a9c4507631fd3729
Chris Xue <chrisxue815@gmail.com>
2017-06-17 14:55:13 +0100

    Adding mid_left, mid_right and unit tests to linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -43,3 +46,69 @@ class ListNode(object):
             result += 1
             root = root.next
         return result
+
+    @staticmethod
+    def mid_right(head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    @staticmethod
+    def mid_left(head):
+        slow = head
+        fast = head
+        while fast and fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+class Test(unittest.TestCase):
+    def test_reverse_list(self):
+        self._test_reverse_list([1, 2, 3], [3, 2, 1])
+        self._test_reverse_list([1, 2], [2, 1])
+        self._test_reverse_list([1], [1])
+        self._test_reverse_list([], [])
+
+    def _test_reverse_list(self, head, expected):
+        head = ListNode.from_array(head)
+        actual = ListNode.reverse_list(head)
+        actual = ListNode.to_array(actual)
+        self.assertEqual(actual, expected)
+
+    def test_count(self):
+        self._test_count([1, 2], 2)
+        self._test_count([1], 1)
+        self._test_count([], 0)
+
+    def _test_count(self, head, expected):
+        head = ListNode.from_array(head)
+        actual = ListNode.count(head)
+        self.assertEqual(actual, expected)
+
+    def test_mid_right(self):
+        self._test_mid_right([1, 2, 3], 2)
+        self._test_mid_right([1, 2], 2)
+        self._test_mid_right([], None)
+
+    def _test_mid_right(self, head, expected):
+        head = ListNode.from_array(head)
+        actual = ListNode.mid_right(head)
+        self.assertEqual(actual.val if actual else None, expected)
+
+    def test_mid_left(self):
+        self._test_mid_left([1, 2, 3], 2)
+        self._test_mid_left([1, 2], 1)
+        self._test_mid_left([], None)
+
+    def _test_mid_left(self, head, expected):
+        head = ListNode.from_array(head)
+        actual = ListNode.mid_left(head)
+        self.assertEqual(actual.val if actual else None, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
