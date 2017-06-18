770bcb728e16123b82470b694364f69685e939ab
Chris Xue <chrisxue815@gmail.com>
2017-06-18 10:05:11 +0100

    61. Rotate List

---
+++ p061.py
@@ -0,0 +1,53 @@
+import unittest
+from linkedlist import ListNode
+
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head or not k:
+            return head
+
+        length = 1
+        tail = head
+        while tail.next:
+            length += 1
+            tail = tail.next
+
+        k %= length
+        if not k:
+            return head
+
+        tail.next = head
+
+        for _ in xrange(length - k - 1):
+            head = head.next
+
+        new_head = head.next
+        head.next = None
+        return new_head
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 4, 5], 2, [4, 5, 1, 2, 3])
+        self._test([1, 2, 3, 4, 5], 7, [4, 5, 1, 2, 3])
+        self._test([1, 2, 3, 4, 5], 0, [1, 2, 3, 4, 5])
+        self._test([1, 2, 3, 4, 5], 5, [1, 2, 3, 4, 5])
+        self._test([], 2, [])
+
+    def _test(self, head, k, expected):
+        head = ListNode.from_array(head)
+
+        actual = Solution().rotateRight(head, k)
+
+        actual = ListNode.to_array(actual)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
