5e33ecb0855135fc7f7ece87cd46f7c0cb042be4
Chris Xue <chrisxue815@gmail.com>
2017-06-19 19:40:36 +0100

    389. Find the Difference. Counting

---
+++ p389_counting.py
@@ -0,0 +1,31 @@
+import unittest
+
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        counts = [0] * 256
+        for ch in t:
+            counts[ord(ch)] += 1
+        for ch in s:
+            counts[ord(ch)] -= 1
+        for i in xrange(ord('a'), ord('z') + 1):
+            if counts[i]:
+                return chr(i)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('abcd', 'abcde', 'e')
+
+    def _test(self, s, t, expected):
+        actual = Solution().findTheDifference(s, t)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
