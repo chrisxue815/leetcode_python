2f376020f92b27c36abc71a20a836cc35038a5ee
Chris Xue <chrisxue815@gmail.com>
2017-06-19 19:21:36 +0100

    326. Power of Three. Math, power of prime numbers

---
+++ p326_power_of_prime_numbers.py
@@ -0,0 +1,38 @@
+import unittest
+import math
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        max_power_3 = int(math.pow(3, math.floor(math.log(0x7fffffff, 3))))
+        return not max_power_3 % n
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(1, True)
+        self._test(3, True)
+        self._test(9, True)
+        self._test(27, True)
+        self._test(81, True)
+
+        self._test(2, False)
+        self._test(4, False)
+        self._test(33, False)
+        self._test(45, False)
+
+        self._test(0, False)
+
+    def _test(self, n, expected):
+        actual = Solution().isPowerOfThree(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
