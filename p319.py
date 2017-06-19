a2e95d0c35399e9bc47b395fbe034f94f53ad2ec
Chris Xue <chrisxue815@gmail.com>
2017-06-19 18:04:51 +0100

    319. Bulb Switcher. Math

---
+++ p319.py
@@ -0,0 +1,34 @@
+import unittest
+import math
+
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, 0)
+        self._test(1, 1)
+        self._test(2, 1)
+        self._test(3, 1)
+        self._test(4, 2)
+        self._test(5, 2)
+        self._test(6, 2)
+        self._test(7, 2)
+        self._test(8, 2)
+        self._test(9, 3)
+        self._test(10, 3)
+
+    def _test(self, n, expected):
+        actual = Solution().bulbSwitch(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
