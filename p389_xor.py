5c454f0fd62535b322a8bca6563ac24fd9b1efba
Chris Xue <chrisxue815@gmail.com>
2017-06-19 19:38:30 +0100

    389. Find the Difference. Xor

---
+++ p389_xor.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        result = 0
+        for ch in s:
+            result ^= ord(ch)
+        for ch in t:
+            result ^= ord(ch)
+        return chr(result)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('abcd', 'abcde', 'e')
+
+    def _test(self, s, t, expected):
+        actual = Solution().findTheDifference(s, t)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
