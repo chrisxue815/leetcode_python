d682682334e06bdbc8dd7a02a4231fa489f69148
Chris Xue <chrisxue815@gmail.com>
2017-06-23 23:09:25 +0100

    136. Single Number

---
+++ p136.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return reduce(lambda x, y: x ^ y, nums)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 2, 3, 1], 3)
+
+    def _test(self, nums, expected):
+        actual = Solution().singleNumber(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
