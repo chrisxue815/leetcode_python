28d43563d57e90b4e2b4eccb55e0e17ccec872a5
Chris Xue <chrisxue815@gmail.com>
2017-06-24 12:17:32 +0100

    233. Number of Digit One. Math, induction

---
+++ p233.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Solution(object):
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result = 0
+        m = 1
+        while m <= n:
+            m2 = m * 10
+            result += n // m2 * m + min(m, max(0, n % m2 - m + 1))
+            m = m2
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(13, 6)
+        self._test(1, 1)
+        self._test(10, 2)
+        self._test(11, 4)
+
+    def _test(self, n, expected):
+        actual = Solution().countDigitOne(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
