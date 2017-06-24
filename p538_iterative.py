9a29a573d9283d22ce345a98a7beeac27d68c98a
Chris Xue <chrisxue815@gmail.com>
2017-06-24 10:58:15 +0100

    538. Convert BST to Greater Tree. Iterative

---
+++ p538_iterative.py
@@ -0,0 +1,38 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        stack = []
+        sum_ = 0
+        curr = root
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr = curr.right
+            else:
+                curr = stack.pop()
+                sum_ += curr.val
+                curr.val = sum_
+                curr = curr.left
+        return root
+
+
+class Test(unittest.TestCase):
+    def test_serialize(self):
+        self._test([5, 2, 13], [18, 20, 13])
+
+    def _test(self, vals, expected):
+        root = TreeNode.from_array(vals)
+        actual = Solution().convertBST(root)
+        actual = actual.to_array()
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
