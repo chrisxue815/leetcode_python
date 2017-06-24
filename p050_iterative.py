ef38dc8a4665ab1aa05b4d3bd31aced162bf18ff
Chris Xue <chrisxue815@gmail.com>
2017-06-24 13:47:02 +0100

    50. Pow(x, n). Iterative

---
+++ p050_iterative.py
@@ -0,0 +1,54 @@
+import unittest
+
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if not n:
+            return 1
+        if not x:
+            return 0
+
+        if n < 0:
+            pos = False
+            n = -n
+        else:
+            pos = True
+
+        result = 1
+
+        while n:
+            if n & 1:
+                result *= x
+            x *= x
+            n >>= 1
+
+        return result if pos else 1.0 / result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(2, -2, 0.25)
+        self._test(2, -1, 0.5)
+        self._test(2, 0, 1)
+        self._test(2, 1, 2)
+        self._test(2, 2, 4)
+        self._test(2, 3, 8)
+        self._test(2, 4, 16)
+        self._test(2, 5, 32)
+        self._test(0, 1, 0)
+        self._test(0, 0, 1)
+        self._test(-2, 2, 4)
+        self._test(-2, 3, -8)
+
+    def _test(self, x, n, expected):
+        actual = Solution().myPow(x, n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
