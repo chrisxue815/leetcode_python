544614973362bf2b81a302de3e57b753d5553141
Chris Xue <chrisxue815@gmail.com>
2017-06-25 13:54:29 +0100

    Using bit manipulation to implement arithmetic operations. Add

---
+++ arithmetic.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def add(a, b):
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
+
+    while b:
+        a, b = a ^ b, ((a & b) << 1)
+
+    if a & 0x80000000:
+        a |= -0x80000000
+    else:
+        a &= 0xFFFFFFFF
+
+    return a
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        for i in xrange(20):
+            for j in xrange(20):
+                self._test(i, j)
+
+    def _test(self, a, b):
+        self._test_arithmetic(a, b)
+        self._test_arithmetic(a, -b)
+        self._test_arithmetic(-a, b)
+        self._test_arithmetic(-a, -b)
+
+    def _test_arithmetic(self, a, b):
+        self.assertEqual(add(a, b), a + b)
+        self.assertEqual(add(b, a), b + a)
+
+
+if __name__ == '__main__':
+    unittest.main()
