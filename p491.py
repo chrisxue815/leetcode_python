a91d742a17918d237b8aff62f55de6a1e713ec55
Chris Xue <chrisxue815@gmail.com>
2017-06-25 19:33:38 +0100

    491. Increasing Subsequences. DFS

---
+++ p491.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Solution(object):
+    def findSubsequences(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = set()
+        stack = []
+
+        def dfs(i):
+            if i >= len(nums):
+                return
+            if not stack or nums[i] >= stack[-1]:
+                stack.append(nums[i])
+                if len(stack) > 1:
+                    result.add(tuple(stack))
+                dfs(i + 1)
+                stack.pop()
+            dfs(i + 1)
+
+        dfs(0)
+
+        return [list(item) for item in result]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [4, 6, 7, 7],
+            [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7, 7], [4, 7, 7]]
+        )
+
+    def _test(self, nums, expected):
+        actual = Solution().findSubsequences(nums)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
