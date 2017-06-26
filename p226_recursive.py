58a5dbdabe99b581d1613d585d2c1a31e952e19f
Chris Xue <chrisxue815@gmail.com>
2017-06-26 22:31:52 +0100

    226. Invert Binary Tree. Recursive

---
+++ p226_recursive.py
@@ -0,0 +1,29 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+
+class Test(unittest.TestCase):
+    def test_serialize(self):
+        self._test([4, 2, 7, 1, 3, 6, 9], [4, 7, 2, 9, 6, 3, 1])
+
+    def _test(self, vals, expected):
+        root = TreeNode.from_array(vals)
+        actual = Solution().invertTree(root)
+        self.assertEqual(actual.to_array(), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
