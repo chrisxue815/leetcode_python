7154a8d555941854deadbef74ec431a99157c82f
Chris Xue <chrisxue815@gmail.com>
2017-06-28 19:00:18 +0100

    404. Sum of Left Leaves. Iterative

---
+++ p404_iterative.py
@@ -0,0 +1,39 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        sum_ = 0
+        stack = []
+
+        while root or stack:
+            if root:
+                if root.left and not root.left.left and not root.left.right:
+                    sum_ += root.left.val
+                    root = root.right
+                else:
+                    stack.append(root.right)
+                    root = root.left
+            else:
+                root = stack.pop()
+
+        return sum_
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([3, 9, 20, None, None, 15, 7], 24)
+
+    def _test(self, vals, expected):
+        root = TreeNode.from_array(vals)
+        actual = Solution().sumOfLeftLeaves(root)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
