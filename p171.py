cd2183b5896fbbb88b78fce59aa2ce55e977babb
Chris Xue <chrisxue815@gmail.com>
2017-06-28 19:07:09 +0100

    171. Excel Sheet Column Number. Math

---
+++ p171.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        for ch in s:
+            num = num * 26 + ord(ch) - ord('A') + 1
+        return num
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('A', 1)
+        self._test('B', 2)
+        self._test('Z', 26)
+        self._test('AA', 27)
+        self._test('AB', 28)
+        self._test('AZ', 52)
+        self._test('BA', 53)
+        self._test('BB', 54)
+        self._test('BZ', 78)
+
+    def _test(self, s, expected):
+        actual = Solution().titleToNumber(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
