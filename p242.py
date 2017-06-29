258ca215aa0e4a2453fa727fe962046548693c90
Chris Xue <chrisxue815@gmail.com>
2017-06-29 19:39:30 +0100

    242. Valid Anagram. Counting

---
+++ p242.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        counts = [0] * 256
+
+        for ch in s:
+            counts[ord(ch)] += 1
+
+        for ch in t:
+            count = counts[ord(ch)]
+            if not count:
+                return False
+            counts[ord(ch)] = count - 1
+
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('anagram', 'nagaram', True)
+        self._test('rat', 'car', False)
+
+    def _test(self, s, t, expected):
+        actual = Solution().isAnagram(s, t)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
