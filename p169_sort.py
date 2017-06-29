dc83075c1c737d3dfb39546549965e7a8d5b61db
Chris Xue <chrisxue815@gmail.com>
2017-06-29 18:42:26 +0100

    169. Majority Element. Sorting

---
+++ p169_sort.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sorted(nums)[len(nums) >> 1]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 2, 2], 2)
+        self._test([-1], -1)
+
+    def _test(self, nums, expected):
+        actual = Solution().majorityElement(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
