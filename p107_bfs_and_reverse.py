3ea442e1bf0aed5f097a870458bb41f7fa1d2db6
Chris Xue <chrisxue815@gmail.com>
2017-07-02 11:04:09 +0100

    107. Binary Tree Level Order Traversal II. BFS and reverse

---
+++ p107_bfs_and_reverse.py
@@ -0,0 +1,52 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        result = []
+        q = [root]
+
+        while q:
+            level = []
+            next_q = []
+
+            for node in q:
+                level.append(node.val)
+                if node.left:
+                    next_q.append(node.left)
+                if node.right:
+                    next_q.append(node.right)
+
+            result.append(level)
+            q = next_q
+
+        result.reverse()
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [3, 9, 20, None, None, 15, 7],
+            [
+                [15, 7],
+                [9, 20],
+                [3],
+            ])
+
+    def _test(self, root, expected):
+        root = TreeNode.from_array(root)
+        actual = Solution().levelOrderBottom(root)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
