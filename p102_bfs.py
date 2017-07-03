84e7f08c9f2dcda506dff93acc846509ef935130
Chris Xue <chrisxue815@gmail.com>
2017-07-03 18:38:48 +0100

    102. Binary Tree Level Order Traversal. BFS

---
+++ p102_bfs.py
@@ -0,0 +1,51 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        result = []
+        q = [root]
+
+        while q:
+            level = []
+            next_q = []
+
+            for node in q:
+                level.append(node.val)
+                if node.left:
+                    next_q.append(node.left)
+                if node.right:
+                    next_q.append(node.right)
+
+            result.append(level)
+            q = next_q
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [3, 9, 20, None, None, 15, 7],
+            [
+                [3],
+                [9, 20],
+                [15, 7],
+            ])
+
+    def _test(self, root, expected):
+        root = TreeNode.from_array(root)
+        actual = Solution().levelOrder(root)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
