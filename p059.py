c052cd958580ea42bd3600aacf8134ed35fa1730
Chris Xue <chrisxue815@gmail.com>
2017-07-06 20:04:18 +0100

    59. Spiral Matrix II

---
+++ p059.py
@@ -0,0 +1,63 @@
+import unittest
+
+
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        m = [[0] * n for _ in xrange(n)]
+        num = 1
+        lo = 0
+        hi = n
+
+        while lo < hi:
+            end = hi - 1
+            for i in xrange(lo, hi):
+                m[lo][i] = num
+                num += 1
+            for i in xrange(lo + 1, hi):
+                m[i][end] = num
+                num += 1
+            for i in xrange(end - 1, lo - 1, -1):
+                m[end][i] = num
+                num += 1
+            for i in xrange(end - 1, lo, -1):
+                m[i][lo] = num
+                num += 1
+
+            lo += 1
+            hi -= 1
+
+        return m
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            3,
+            [
+                [1, 2, 3],
+                [8, 9, 4],
+                [7, 6, 5],
+            ]
+        )
+
+        self._test(
+            4,
+            [
+                [1, 2, 3, 4],
+                [12, 13, 14, 5],
+                [11, 16, 15, 6],
+                [10, 9, 8, 7],
+            ]
+        )
+
+    def _test(self, n, expected):
+        actual = Solution().generateMatrix(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
