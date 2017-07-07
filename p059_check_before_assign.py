c0657097b3848305563e4f61d024203c456d27fb
Chris Xue <chrisxue815@gmail.com>
2017-07-07 18:14:15 +0100

    59. Spiral Matrix II. Checking before assignment

---
+++ p059_check_before_assign.py
@@ -0,0 +1,50 @@
+import unittest
+
+
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        m = [[0] * n for _ in xrange(n)]
+        i, j, di, dj = 0, 0, 0, 1
+
+        for num in xrange(1, n * n + 1):
+            m[i][j] = num
+            if m[(i + di) % n][(j + dj) % n]:
+                di, dj = dj, -di
+            i += di
+            j += dj
+
+        return m
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            3,
+            [
+                [1, 2, 3],
+                [8, 9, 4],
+                [7, 6, 5],
+            ]
+        )
+
+        self._test(
+            4,
+            [
+                [1, 2, 3, 4],
+                [12, 13, 14, 5],
+                [11, 16, 15, 6],
+                [10, 9, 8, 7],
+            ]
+        )
+
+    def _test(self, n, expected):
+        actual = Solution().generateMatrix(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
