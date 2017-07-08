6807cbdadeee21b5063ff57f65c9223a062d313f
Chris Xue <chrisxue815@gmail.com>
2017-07-08 18:52:39 +0100

    119. Pascal's Triangle II. 2k space

---
+++ p119_2k_space.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        prev = [1]
+        for i in xrange(1, rowIndex + 1):
+            row = [1] * (i + 1)
+            for j in xrange(1, i):
+                row[j] = prev[j - 1] + prev[j]
+            prev = row
+        return prev
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, [1])
+        self._test(1, [1, 1])
+        self._test(2, [1, 2, 1])
+        self._test(3, [1, 3, 3, 1])
+
+    def _test(self, rowIndex, expected):
+        actual = Solution().getRow(rowIndex)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
