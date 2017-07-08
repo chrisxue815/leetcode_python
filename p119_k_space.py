b71811c855e83b8dfa5df25ba4e7dc41573267ad
Chris Xue <chrisxue815@gmail.com>
2017-07-08 19:05:52 +0100

    119. Pascal's Triangle II. k space

---
+++ p119_k_space.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1] * (rowIndex + 1)
+        for i in xrange(2, rowIndex + 1):
+            prev = 1
+            for j in xrange(1, i):
+                curr = row[j]
+                row[j] = curr + prev
+                prev = curr
+        return row
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(0, [1])
+        self._test(1, [1, 1])
+        self._test(2, [1, 2, 1])
+        self._test(3, [1, 3, 3, 1])
+
+    def _test(self, rowIndex, expected):
+        actual = Solution().getRow(rowIndex)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
