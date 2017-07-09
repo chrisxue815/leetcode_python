178dad3c4545317007afd6922edf0902cc57752f
Chris Xue <chrisxue815@gmail.com>
2017-07-09 13:36:57 +0100

    73. Set Matrix Zeroes. O(m+n) space

---
+++ p073_m_plus_n_space.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        rows = [0] * len(matrix)
+        cols = [0] * len(matrix[0])
+
+        for i, row in enumerate(matrix):
+            for j, num in enumerate(row):
+                if not num:
+                    rows[i] = 1
+                    cols[j] = 1
+
+        for row, num in enumerate(rows):
+            if num:
+                for j in xrange(len(matrix[0])):
+                    matrix[row][j] = 0
+
+        for col, num in enumerate(cols):
+            if num:
+                for i in xrange(len(matrix)):
+                    matrix[i][col] = 0
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [
+                [1, 2, 0],
+                [1, 2, 3],
+                [0, 2, 3],
+            ],
+            [
+                [0, 0, 0],
+                [0, 2, 0],
+                [0, 0, 0],
+            ]
+        )
+
+    def _test(self, matrix, expected):
+        Solution().setZeroes(matrix)
+        self.assertEqual(matrix, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
