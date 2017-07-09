b75df1942cbb77e8cfb5ef56ab2e970868b5d631
Chris Xue <chrisxue815@gmail.com>
2017-07-09 13:14:30 +0100

    26. Remove Duplicates from Sorted Array. Compacting

---
+++ p026_compact.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        write_index = 0
+        prev = nums[0] - 1
+
+        for num in nums:
+            if num != prev:
+                nums[write_index] = num
+                write_index += 1
+                prev = num
+
+        return write_index
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 1, 2], [1, 2])
+
+    def _test(self, nums, expected):
+        actual = Solution().removeDuplicates(nums)
+        self.assertEqual(actual, len(expected))
+        self.assertEqual(nums[:actual], expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
