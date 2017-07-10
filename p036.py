986f4b1c0680a93a783feae82d301c32da73713f
Chris Xue <chrisxue815@gmail.com>
2017-07-10 19:21:24 +0100

    36. Valid Sudoku

---
+++ p036.py
@@ -0,0 +1,60 @@
+import unittest
+
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        rows = [[0] * 9 for _ in xrange(9)]
+        cols = [[0] * 9 for _ in xrange(9)]
+        boxes = [[0] * 9 for _ in xrange(9)]
+
+        for i, row in enumerate(board):
+            for j, num in enumerate(row):
+                if num == '.':
+                    continue
+
+                num = ord(num) - ord('1')
+
+                if rows[i][num]:
+                    return False
+                rows[i][num] = 1
+
+                if cols[j][num]:
+                    return False
+                cols[j][num] = 1
+
+                box_index = i // 3 * 3 + j // 3
+                if boxes[box_index][num]:
+                    return False
+                boxes[box_index][num] = 1
+
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [
+                ".87654321",
+                "2........",
+                "3........",
+                "4........",
+                "5........",
+                "6........",
+                "7........",
+                "8........",
+                "9........",
+            ],
+            True
+        )
+
+    def _test(self, board, expected):
+        actual = Solution().isValidSudoku(board)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
