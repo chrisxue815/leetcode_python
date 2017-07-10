1367a409eba272a06ac3b21ebaae6a485027c8dd
Chris Xue <chrisxue815@gmail.com>
2017-07-10 19:07:18 +0100

    73. Set Matrix Zeroes. O(1) space

---
+++ p073_constant_space.py
@@ -0,0 +1,58 @@
+import unittest
+
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        is_top_0 = any(not num for num in matrix[0])
+        is_left_0 = any(not matrix[i][0] for i in xrange(len(matrix)))
+
+        for i in xrange(1, len(matrix)):
+            for j in xrange(1, len(matrix[0])):
+                if not matrix[i][j]:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+        for j in xrange(1, len(matrix[0])):
+            if not matrix[0][j]:
+                for i in xrange(1, len(matrix)):
+                    matrix[i][j] = 0
+
+        for i in xrange(1, len(matrix)):
+            if not matrix[i][0]:
+                for j in xrange(1, len(matrix[0])):
+                    matrix[i][j] = 0
+
+        if is_top_0:
+            for j in xrange(len(matrix[0])):
+                matrix[0][j] = 0
+        if is_left_0:
+            for i in xrange(len(matrix)):
+                matrix[i][0] = 0
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [
+                [1, 2, 0],
+                [1, 2, 3],
+                [0, 2, 3],
+            ],
+            [
+                [0, 0, 0],
+                [0, 2, 0],
+                [0, 0, 0],
+            ]
+        )
+
+    def _test(self, matrix, expected):
+        Solution().setZeroes(matrix)
+        self.assertEqual(matrix, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
