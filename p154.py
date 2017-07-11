8486fea4f8230e42c43b50245969b6617be15710
Chris Xue <chrisxue815@gmail.com>
2017-07-11 20:51:03 +0100

    154. Find Minimum in Rotated Sorted Array II. Binary search

---
+++ p154.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            lo_val = nums[lo]
+            hi_val = nums[hi]
+
+            if lo_val < hi_val:
+                return lo_val
+
+            mid = lo + ((hi - lo) >> 1)
+            mid_val = nums[mid]
+
+            if mid_val < lo_val:
+                hi = mid
+            elif mid_val > lo_val or lo_val > hi_val:
+                lo = mid + 1
+            else:
+                lo += 1
+
+        return nums[lo]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([4, 5, 5, 6, 7, 0, 1, 2], 0)
+        self._test([4, 5, 6, 7, 0, 1, 2, 2], 0)
+        self._test([4, 4, 5, 6, 7, 0, 1, 2], 0)
+        self._test([4, 4, 5, 6, 7, 0, 1, 2, 4], 0)
+        self._test([0, 1, 2, 4, 4, 5, 6, 7], 0)
+        self._test([1], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().findMin(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
