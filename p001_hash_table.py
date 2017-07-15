fd4f00d5b1ce3f66297642a26113817b14a30163
Chris Xue <chrisxue815@gmail.com>
2017-07-15 10:11:42 +0100

    1. Two Sum. Improving performance

--- p001_hash_table.py
+++ p001_hash_table.py
@@ -9,30 +9,11 @@ class Solution(object):
         :rtype: List[int]
         """
         num_to_index = {}
-        half = target >> 1
 
         for i, num in enumerate(nums):
-            if num in num_to_index and num == half:
-                return [num_to_index[num], i]
+            if target - num in num_to_index:
+                return [num_to_index[target - num], i]
             num_to_index[num] = i
-
-        nums.sort()
-
-        for i, num in enumerate(nums):
-            diff = target - num
-            lo = i + 1
-            hi = len(nums) - 1
-
-            while lo <= hi:
-                mid = lo + ((hi - lo) >> 1)
-                mid_num = nums[mid]
-
-                if mid_num < diff:
-                    lo = mid + 1
-                elif mid_num > diff:
-                    hi = mid - 1
-                else:
-                    return [num_to_index[num], num_to_index[mid_num]]
 
 
 class Test(unittest.TestCase):
