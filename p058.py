3777895d88fb4cf6d2fa8905b1d89faa022ba2dc
Chris Xue <chrisxue815@gmail.com>
2017-07-16 13:49:15 +0100

    58. Length of Last Word

---
+++ p058.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+
+        count = 0
+        for ch in reversed(s):
+            if ch != ' ':
+                count += 1
+            elif count:
+                break
+        return count
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('Hello World', 5)
+        self._test('World', 5)
+        self._test('World  ', 5)
+        self._test('', 0)
+        self._test(None, 0)
+
+    def _test(self, s, expected):
+        actual = Solution().lengthOfLastWord(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
