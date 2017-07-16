bb83da3f40aefc1c3cbfeaf9cb2a24c2c32e4f91
Chris Xue <chrisxue815@gmail.com>
2017-07-16 12:43:05 +0100

    20. Valid Parentheses. Stack

---
+++ p020.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+
+        for ch in s:
+            # is "if" faster or slower than hash table in python?
+            if ch == '(' or ch == '[' or ch == '{':
+                stack.append(ch)
+            elif ch == ')':
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    return False
+            elif ch == ']':
+                if stack and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+            elif ch == '}':
+                if stack and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+
+        return not stack
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('()', True)
+        self._test('()[]{}', True)
+        self._test('(]', False)
+        self._test('([)]', False)
+        self._test(')', False)
+
+    def _test(self, n, expected):
+        actual = Solution().isValid(n)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
