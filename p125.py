e69b22dfcfc673975d75ba2b4daf45b6bda22069
Chris Xue <chrisxue815@gmail.com>
2017-07-17 19:28:49 +0100

    125. Valid Palindrome. Two pointers

---
+++ p125.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        lo = 0
+        hi = len(s) - 1
+
+        while lo < hi:
+            while lo < hi and not s[lo].isalnum():
+                lo += 1
+
+            while lo < hi and not s[hi].isalnum():
+                hi -= 1
+
+            if s[lo].lower() != s[hi].lower():
+                return False
+
+            lo += 1
+            hi -= 1
+
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('A man, a plan, a canal: Panama', True)
+        self._test('race a car', False)
+        self._test('', True)
+        self._test('  ', True)
+
+    def _test(self, s, expected):
+        actual = Solution().isPalindrome(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
