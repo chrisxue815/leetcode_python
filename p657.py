b50e2b466a613feb9b01043095298acab0688fc4
Chris Xue <chrisxue815@gmail.com>
2017-07-23 15:55:57 +0100

    657. Judge Route Circle

---
+++ p657.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        lr = ud = 0
+        for move in moves:
+            if move == 'L':
+                lr += 1
+            elif move == 'R':
+                lr -= 1
+            elif move == 'U':
+                ud += 1
+            else:
+                ud -= 1
+        return lr == 0 and ud == 0
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('UD', True)
+        self._test('LL', False)
+
+    def _test(self, moves, expected):
+        actual = Solution().judgeCircle(moves)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
