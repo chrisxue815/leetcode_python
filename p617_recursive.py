c0ccb1d8173ebba1938690920b105c43a0c7289d
Chris Xue <chrisxue815@gmail.com>
2017-07-24 18:37:12 +0100

    617. Merge Two Binary Trees. Improving performance

--- p617_recursive.py
+++ p617_recursive.py
@@ -9,19 +9,16 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
+        if not t1:
+            return t2
+        if not t2:
+            return t1
 
-        def merge(a, b):
-            if a:
-                if b:
-                    a.val += b.val
-                    a.left = merge(a.left, b.left)
-                    a.right = merge(a.right, b.right)
-                    return a
-                else:
-                    return a
-            return b
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
 
-        return merge(t1, t2)
+        return t1
 
 
 class Test(unittest.TestCase):
