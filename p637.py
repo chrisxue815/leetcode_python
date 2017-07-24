e747be6f7f621fd7e30b24ac63079f1cb978f040
Chris Xue <chrisxue815@gmail.com>
2017-07-24 18:56:43 +0100

    637. Average of Levels in Binary Tree. Level-order traversal

---
+++ p637.py
@@ -0,0 +1,45 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        if not root:
+            return []
+
+        result = []
+        q = [root]
+
+        while q:
+            sum_ = 0
+            next_q = []
+            for node in q:
+                sum_ += node.val
+                if node.left:
+                    next_q.append(node.left)
+                if node.right:
+                    next_q.append(node.right)
+            result.append(float(sum_) / len(q))
+            q = next_q
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [3, 9, 20, None, None, 15, 7],
+            [3, 14.5, 11])
+
+    def _test(self, root, expected):
+        root = TreeNode.from_array(root)
+        actual = Solution().averageOfLevels(root)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
