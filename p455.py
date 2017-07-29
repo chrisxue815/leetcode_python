78f3eb14efea33dec5954867d1a94e7ccd9ee09b
Chris Xue <chrisxue815@gmail.com>
2017-07-29 10:47:14 +0100

    455. Assign Cookies. Binary search

---
+++ p455.py
@@ -0,0 +1,36 @@
+import unittest
+import bisect
+
+
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+        lo = 0
+
+        for greed_index, greed in enumerate(g):
+            lo = bisect.bisect_left(s, greed, lo)
+            if lo < len(s):
+                lo += 1
+            else:
+                return greed_index
+        return len(g)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3], [1, 1], 1)
+        self._test([1, 2], [1, 2, 3], 2)
+
+    def _test(self, g, s, expected):
+        actual = Solution().findContentChildren(g, s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
