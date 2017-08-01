a5130f239f4411927ac4507eed9503e6ce820acd
Chris Xue <chrisxue815@gmail.com>
2017-08-01 18:27:28 +0100

    504. Base 7

---
+++ p504.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+            return '0'
+
+        result = []
+        tmp = abs(num)
+        while tmp:
+            tmp, r = divmod(tmp, 7)
+            result.append(str(r))
+        if num < 0:
+            result.append('-')
+        return ''.join(reversed(result))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(100, '202')
+        self._test(-7, '-10')
+        self._test(0, '0')
+
+    def _test(self, num, expected):
+        actual = Solution().convertToBase7(num)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
