1180c1c39dddecffd81ba44af8aa3dbf90b4280f
Chris Xue <chrisxue815@gmail.com>
2017-08-05 10:35:02 +0100

    633. Sum of Square Numbers. Two pointers

---
+++ p633_two_pointers.py
@@ -0,0 +1,36 @@
+import unittest
+import math
+
+
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        lo = 0
+        hi = int(math.sqrt(c))
+
+        while lo <= hi:
+            d = lo * lo + hi * hi
+            if d < c:
+                lo += 1
+            elif d > c:
+                hi -= 1
+            else:
+                return True
+        return False
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(5, True)
+        self._test(3, False)
+
+    def _test(self, nums, expected):
+        actual = Solution().judgeSquareSum(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
