f3647ae89d19b73350dc538f1a70020923fc2334
Chris Xue <chrisxue815@gmail.com>
2017-08-05 10:40:13 +0100

    633. Sum of Square Numbers. Minimize search space

---
+++ p633_minimize_search_space.py
@@ -0,0 +1,28 @@
+import unittest
+import math
+
+
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        for a in xrange(int(math.sqrt(c / 2)), int(math.sqrt(c)) + 1):
+            if math.sqrt(c - a * a).is_integer():
+                return True
+        return False
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(5, True)
+        self._test(3, False)
+
+    def _test(self, nums, expected):
+        actual = Solution().judgeSquareSum(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
