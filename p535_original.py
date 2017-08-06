5592b98200b429711b535583c4c1d7785b96d034
Chris Xue <chrisxue815@gmail.com>
2017-08-06 14:20:10 +0100

    535. Encode and Decode TinyURL. Original string

---
+++ p535_original.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Codec:
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+
+        :type longUrl: str
+        :rtype: str
+        """
+        return longUrl
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+
+        :type shortUrl: str
+        :rtype: str
+        """
+        return shortUrl
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('https://leetcode.com/problems/design-tinyurl')
+
+    def _test(self, url):
+        codec = Codec()
+        self.assertEqual(codec.decode(codec.encode(url)), url)
+
+
+if __name__ == '__main__':
+    unittest.main()
