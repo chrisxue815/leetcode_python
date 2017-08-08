806b2c36a541ac84eccae7ce803ca3b01b4a9d20
Chris Xue <chrisxue815@gmail.com>
2017-08-08 18:29:35 +0100

    479. Largest Palindrome Product. Simplify palindrome generation, still TLE

--- p479_tle.py
+++ p479_tle.py
@@ -3,32 +3,10 @@ import itertools
 import math
 
 
-def generate_palindromes(n):
-    for num_digits in xrange(n << 1, 0, -1):
-        hi_i = num_digits >> 1
-        lo_i = (num_digits - 1) >> 1
-        p = 0
-        for _ in xrange(num_digits):
-            p = p * 10 + 9
-        while p:
-            yield p
-
-            while True:
-                hi = 10 ** hi_i
-                lo = 10 ** lo_i
-                digit = p % (hi * 10) // hi
-                if digit:
-                    p += (digit - 1) * hi - digit * hi
-                    if hi != lo:
-                        p += (digit - 1) * lo - digit * lo
-                    hi_i = num_digits >> 1
-                    lo_i = (num_digits - 1) >> 1
-                    break
-                else:
-                    p += 9 * hi + 9 * lo
-                    hi_i += 1
-                    lo_i -= 1
-    yield 0
+def generate_palindromes(hi):
+    for hi in xrange(hi, -1, -1):
+        s = str(hi)
+        yield int(s + s[::-1])
 
 
 class Solution(object):
@@ -40,31 +18,41 @@ class Solution(object):
         if n == 1:
             return 9
 
-        hi = 10 ** n
-        for p in generate_palindromes(n):
+        hi = 10 ** n - 1
+
+        for p in generate_palindromes(hi):
             root = int(math.sqrt(p))
-            for factor in xrange(root, hi):
+
+            for factor in xrange(hi, root - 1, -1):
                 q, r = divmod(p, factor)
-                if not r and q < hi:
+
+                if q > hi:
+                    break
+                if not r:
                     return p % 1337
-        return 0
 
 
 class Test(unittest.TestCase):
     def test(self):
         self._test(1, 9)
         self._test(2, 987)
+        self._test(3, 123)
+        self._test(4, 597)
+        self._test(5, 677)
+        self._test(6, 1218)
+        self._test(7, 877)
+        self._test(8, 475)
 
     def _test(self, heights, expected):
         actual = Solution().largestPalindrome(heights)
         self.assertEqual(actual, expected)
 
     def test_generate_palindromes(self):
-        self._test_generate_palindromes(1, [
+        self._test_generate_palindromes(9, [
             99, 88, 77, 66, 55, 44, 33, 22, 11,
-            9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+            0
         ])
-        self._test_generate_palindromes(3, [
+        self._test_generate_palindromes(999, [
             999999, 998899, 997799, 996699, 995599, 994499, 993399, 992299, 991199, 990099,
             989989, 988889, 987789, 986689, 985589, 984489, 983389, 982289, 981189, 980089,
             979979
