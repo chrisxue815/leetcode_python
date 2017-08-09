2aa176a6f51f5064c58eb8452f5ec762975ed66a
Chris Xue <chrisxue815@gmail.com>
2017-08-09 19:06:03 +0100

    419. Battleships in a Board

---
+++ p419.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+class Solution(object):
+    def countBattleships(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: int
+        """
+        result = 0
+        for r, row in enumerate(board):
+            for c, cell in enumerate(row):
+                if cell == 'X' and (not r or board[r - 1][c] != 'X') and (not c or board[r][c - 1] != 'X'):
+                    result += 1
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([
+            'X..X',
+            '...X',
+            '...X',
+        ], 2)
+
+    def _test(self, board, expected):
+        actual = Solution().countBattleships(board)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
