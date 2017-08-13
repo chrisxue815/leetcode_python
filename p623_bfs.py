6ea78c438fc44809e0a586be82e1d09e48bfd0fa
Chris Xue <chrisxue815@gmail.com>
2017-08-13 14:40:54 +0100

    623. Add One Row to Tree. BFS

---
+++ p623_bfs.py
@@ -0,0 +1,54 @@
+import unittest
+from tree import TreeNode
+
+
+class Solution(object):
+    def addOneRow(self, root, v, d):
+        """
+        :type root: TreeNode
+        :type v: int
+        :type d: int
+        :rtype: TreeNode
+        """
+        if d == 1:
+            node = TreeNode(v)
+            node.left = root
+            return node
+        q = [root]
+        for _ in xrange(d - 2):
+            q = [child for node in q for child in (node.left, node.right) if child]
+        for node in q:
+            child = TreeNode(v)
+            child.left, node.left = node.left, child
+            child = TreeNode(v)
+            child.right, node.right = node.right, child
+        return root
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(
+            [4, 2, 6, 3, 1, 5],
+            1, 2,
+            [4, 1, 1, 2, None, None, 6, 3, 1, 5]
+        )
+        self._test(
+            [4, 2, None, 3, 1],
+            1, 3,
+            [4, 2, None, 1, 1, 3, None, None, 1]
+        )
+        self._test(
+            [1, 2, 3],
+            1, 1,
+            [1, 1, None, 2, 3]
+        )
+
+    def _test(self, root, v, d, expected):
+        root = TreeNode.from_array(root)
+        actual = Solution().addOneRow(root, v, d)
+        actual = actual.to_array()
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
