c964e8e5421de07d00ba1771e2ff030c6870e11f
Chris Xue <chrisxue815@gmail.com>
2017-08-15 21:16:49 +0100

    421. Maximum XOR of Two Numbers in an Array. Shifting bits

---
+++ p421_shift.py
@@ -0,0 +1,29 @@
+import unittest
+
+
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ret = 0
+        for i in xrange(31, -1, -1):
+            ret <<= 1
+            prefixes = set(num >> i for num in nums)
+            candidate = ret | 1
+            ret += any(candidate ^ prefix in prefixes for prefix in prefixes)
+        return ret
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([3, 10, 5, 25, 2, 8], 28)
+
+    def _test(self, nums, expected):
+        actual = Solution().findMaximumXOR(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
