6c0b372e1d90b8ec4a2ea110adc03b5d3fde24f2
Chris Xue <chrisxue815@gmail.com>
2017-09-02 11:39:19 +0100

    388. Longest Absolute File Path. O(n). Built-in functions, hash table

---
+++ p388_builtin_functions.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Solution(object):
+    def lengthLongestPath(self, input):
+        """
+        :type input: str
+        :rtype: int
+        """
+        max_len = 0
+        path_lengths = {0: 0}
+
+        for seg in input.split('\n'):
+            name = seg.lstrip('\t')
+            depth = len(seg) - len(name)
+
+            if '.' in name:
+                max_len = max(max_len, path_lengths[depth] + len(name))
+            else:
+                path_lengths[depth + 1] = path_lengths[depth] + len(name) + 1
+
+        return max_len
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext', 20)
+        self._test('dir\n\tsubdir1\n\tsubdir2\n\t\tfile', 0)
+        self._test('dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext', 32)
+
+    def _test(self, input, expected):
+        actual = Solution().lengthLongestPath(input)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
