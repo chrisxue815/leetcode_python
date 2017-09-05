84b09f5e92d4ff5f353e152ae95239ae63f48746
Chris Xue <chrisxue815@gmail.com>
2017-09-05 19:33:10 +0100

    395. Longest Substring with At Least K Repeating Characters. O(n). Recursion

---
+++ p395.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if len(s) < k:
+            return 0
+        counts = [0] * 26
+        for ch in s:
+            counts[ord(ch) - ord('a')] += 1
+        for ch, count in enumerate(counts):
+            if 0 < count < k:
+                return max(self.longestSubstring(seg, k) for seg in s.split(chr(ch + ord('a'))))
+        return len(s)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('aaabb', 3, 3)
+        self._test('ababbc', 2, 5)
+
+    def _test(self, s, k, expected):
+        actual = Solution().longestSubstring(s, k)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
