2ccfa9660ae1e3303c0b2f5b28e714ed0dc6a416
Chris Xue <chrisxue815@gmail.com>
2017-09-09 15:13:48 +0100

    284. Peeking Iterator. Design

---
+++ p284.py
@@ -0,0 +1,84 @@
+import unittest
+
+
+# Below is the interface for Iterator, which is already defined for you.
+
+class Iterator(object):
+    def __init__(self, nums):
+        """
+        Initializes an iterator object to the beginning of a list.
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.curr = -1
+
+    def hasNext(self):
+        """
+        Returns true if the iteration has more elements.
+        :rtype: bool
+        """
+        return self.curr + 1 < len(self.nums)
+
+    def next(self):
+        """
+        Returns the next element in the iteration.
+        :rtype: int
+        """
+        self.curr += 1
+        return self.nums[self.curr]
+
+
+class PeekingIterator(object):
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.it = iterator
+        # We need to introduce a hasNext variable if None is a valid item in the iterator
+        self.next_item = iterator.next() if iterator.hasNext() else None
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self.next_item
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        next_item = self.next_item
+        self.next_item = self.it.next() if self.it.hasNext() else None
+        return next_item
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.next_item is not None
+
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([0, 1, 2, 3])
+
+    def _test(self, nums):
+        it = PeekingIterator(Iterator(nums))
+        for num in nums:
+            self.assertEqual(it.hasNext(), True)
+            self.assertEqual(it.peek(), num)
+            self.assertEqual(it.next(), num)
+        self.assertEqual(it.hasNext(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
