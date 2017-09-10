905d56399d6cada12085e61853dac5d5f9da3a62
Chris Xue <chrisxue815@gmail.com>
2017-09-10 10:41:48 +0100

    659. Split Array into Consecutive Subsequences. O(nlog(n)). Improving performance

--- p659.py
+++ p659.py
@@ -18,20 +18,19 @@ class Solution(object):
         for num in nums:
             if num != curr_num:
                 if num > curr_num + 1:
-                    if next_queue and next_queue[0][0] < 3:
+                    if next_queue and next_queue[0] < 3:
                         return False
                     next_queue = []
-                if curr_queue and curr_queue[0][0] < 3:
+                if curr_queue and curr_queue[0] < 3:
                     return False
                 curr_num = num
                 curr_queue = next_queue
                 next_queue = []
 
-            count, seq = heapq.heappop(curr_queue) if curr_queue else (0, [])
-            seq.append(num)
-            heapq.heappush(next_queue, (count + 1, seq))
+            count = heapq.heappop(curr_queue) if curr_queue else 0
+            heapq.heappush(next_queue, count + 1)
 
-        return (not curr_queue or curr_queue[0][0] >= 3) and (not next_queue or next_queue[0][0] >= 3)
+        return (not curr_queue or curr_queue[0] >= 3) and (not next_queue or next_queue[0] >= 3)
 
 
 class Test(unittest.TestCase):
