a5998a6bccbebb9bbaa39a1bc138da84304a6a8b
Chris Xue <chrisxue815@gmail.com>
2017-09-14 19:22:15 +0100

    15. 3Sum. O(n^2). Two pointers. Optimization

--- p015_two_pointers.py
+++ p015_two_pointers.py
@@ -9,24 +9,26 @@ class Solution(object):
         """
         nums.sort()
         result = []
-        for ia, a in enumerate(nums):
-            if ia > 0 and nums[ia - 1] == a:
+
+        for i in xrange(len(nums) - 2):
+            a = nums[i]
+            if a + nums[i + 1] + nums[i + 2] > 0:
+                break
+            if i > 0 and nums[i - 1] == a or a + nums[-1] + nums[-2] < 0:
                 continue
+
             residual = -a
-            lo = ia + 1
+            lo = i + 1
             hi = len(nums) - 1
+
             while lo < hi:
                 lo_num = nums[lo]
                 hi_num = nums[hi]
                 s = lo_num + hi_num
                 if s < residual:
                     lo += 1
-                    while lo < hi and nums[lo] == lo_num:
-                        lo += 1
                 elif s > residual:
                     hi -= 1
-                    while lo < hi and nums[hi] == hi_num:
-                        hi -= 1
                 else:
                     result.append([a, lo_num, hi_num])
                     lo += 1
