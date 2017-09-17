7616df277d10a47ae2db2d75bdb54b604c86fd93
Chris Xue <chrisxue815@gmail.com>
2017-09-17 18:33:15 +0100

    31. Next Permutation. O(n). Induction, binary search

---
+++ p031.py
@@ -0,0 +1,53 @@
+import unittest
+
+
+# O(n). Induction, binary search
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in xrange(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                break
+        else:
+            i = -1
+
+        lo = i + 1
+        hi = len(nums) - 1
+        while lo < hi:
+            nums[lo], nums[hi] = nums[hi], nums[lo]
+            lo += 1
+            hi -= 1
+
+        if i == -1:
+            return
+
+        target = nums[i]
+        lo = i + 1
+        hi = len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) >> 1
+            mid_val = nums[mid]
+            if mid_val <= target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        nums[i], nums[lo] = nums[lo], nums[i]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3], [1, 3, 2])
+        self._test([3, 2, 1], [1, 2, 3])
+        self._test([1, 1, 5], [1, 5, 1])
+
+    def _test(self, nums, expected):
+        Solution().nextPermutation(nums)
+        self.assertEqual(nums, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
