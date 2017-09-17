e2b677f931afecf669079481b7af8747370555fe
Chris Xue <chrisxue815@gmail.com>
2017-09-17 17:43:20 +0100

    55. Jump Game. O(n). Linear iteration

---
+++ p055.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+# O(n). Linear iteration
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hi = 0
+        for lo, num in enumerate(nums):
+            if lo > hi:
+                return False
+            hi = max(hi, lo + num)
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([2, 3, 1, 1, 4], True)
+        self._test([3, 2, 1, 0, 4], False)
+
+    def _test(self, nums, expected):
+        actual = Solution().canJump(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
