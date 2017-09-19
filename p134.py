328386b0f5d9db980b0a11422d2e7f7af5f8a408
Chris Xue <chrisxue815@gmail.com>
2017-09-19 18:21:40 +0100

    134. Gas Station. O(n). Mathematical modelling and reasoning

---
+++ p134.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+# O(n). Mathematical modelling and reasoning
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        start = total = curr = 0
+
+        for i in xrange(len(gas)):
+            diff = gas[i] - cost[i]
+            total += diff
+            curr += diff
+            if curr < 0:
+                start = i + 1
+                curr = 0
+
+        return start if total >= 0 else -1
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 3], [2, 1, 5, 1], 3)
+
+    def _test(self, gas, cost, expected):
+        actual = Solution().canCompleteCircuit(gas, cost)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
