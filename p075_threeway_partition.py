85de51b8c7539dc6e1d3689a06afe987210a56c2
Chris Xue <chrisxue815@gmail.com>
2017-09-23 12:24:44 +0100

    75. Sort Colors. O(n) time, O(1) space. Three-way partitioning

---
+++ p075_threeway_partition.py
@@ -0,0 +1,48 @@
+import unittest
+
+
+# O(n), three-way partitioning
+# https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+
+        n = len(nums)
+
+        lo = mid = 0
+        hi = n - 1
+
+        while mid <= hi:
+            if nums[mid] == 0:
+                nums[lo], nums[mid] = nums[mid], nums[lo]
+                lo += 1
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[hi] = nums[hi], nums[mid]
+                hi -= 1
+            else:
+                mid += 1
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([], [])
+        self._test([0], [0])
+        self._test([2, 0, 1], [0, 1, 2])
+        self._test([2, 0], [0, 2])
+        self._test([1, 2, 0, 0], [0, 0, 1, 2])
+        self._test([1, 2, 0], [0, 1, 2])
+        self._test([2, 1, 0, 1], [0, 1, 1, 2])
+
+    def _test(self, nums, expected):
+        Solution().sortColors(nums)
+        self.assertEqual(nums, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
