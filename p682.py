c61159d90c865fae0bf220d5fa4cf31581f867b3
Chris Xue <chrisxue815@gmail.com>
2017-09-30 09:42:29 +0100

    682. Baseball Game. O(n)

---
+++ p682.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+# O(n)
+class Solution(object):
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        stack = []
+        for op in ops:
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif op == 'D':
+                stack.append(stack[-1] * 2)
+            elif op == 'C':
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(["5", "2", "C", "D", "+"], 30)
+        self._test(["5", "-2", "4", "C", "D", "9", "+", "+"], 27)
+
+    def _test(self, ops, expected):
+        actual = Solution().calPoints(ops)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
