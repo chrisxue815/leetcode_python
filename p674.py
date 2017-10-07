8798cb27f70dcf7a5195327e4ade93276d76495b
Chris Xue <chrisxue815@gmail.com>
2017-10-07 10:12:34 +0100

    674. Longest Continuous Increasing Subsequence. O(n).

---
+++ p674.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+# O(n)
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        result = 1
+        lo = 0
+        prev = nums[0]
+        for hi, curr in enumerate(nums):
+            if prev >= curr:
+                result = max(result, hi - lo)
+                lo = hi
+            prev = curr
+        return max(result, len(nums) - lo)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 3, 5, 4, 7], 3)
+        self._test([1, 3, 5, 6, 7], 5)
+        self._test([2, 2, 2, 2, 2], 1)
+
+    def _test(self, nums, expected):
+        actual = Solution().findLengthOfLCIS(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
