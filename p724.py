5fee138d0c6be9639065e0edabe144830271dc2a
Chris Xue <chrisxue815@gmail.com>
2017-10-07 10:28:36 +0100

    724. Find Pivot Index. O(n).

---
+++ p724.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+# O(n).
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return -1
+
+        hi_sum = sum(nums)
+        lo_sum = 0
+        for i, num in enumerate(nums):
+            hi_sum -= num
+            if lo_sum == hi_sum:
+                return i
+            lo_sum += num
+        return -1
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 7, 3, 6, 5, 6], 3)
+        self._test([1, 2, 3], -1)
+
+    def _test(self, nums, expected):
+        actual = Solution().pivotIndex(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
