cead238cf0afa7a8bb3692cd04fb706e47e9a4d1
Chris Xue <chrisxue815@gmail.com>
2017-10-12 18:54:16 +0100

    680. Valid Palindrome II. O(n). Two pointers, backtracking.

---
+++ p680.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+def _palindrome(s, lo, hi, tolerate):
+    while lo < hi:
+        if s[lo] == s[hi]:
+            lo += 1
+            hi -= 1
+        else:
+            if not tolerate:
+                return False
+            return _palindrome(s, lo + 1, hi, False) or _palindrome(s, lo, hi - 1, False)
+
+    return True
+
+
+# O(n). Two pointers, backtracking.
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return _palindrome(s, 0, len(s) - 1, True)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('aba', True)
+        self._test('abca', True)
+
+    def _test(self, s, expected):
+        actual = Solution().validPalindrome(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
