bbd969f5a27f9d1a38415d688a5840794da9fe3e
Chris Xue <chrisxue815@gmail.com>
2017-10-13 19:46:57 +0100

    678. Valid Parenthesis String. O(n) time. O(1) space.

---
+++ p678.py
@@ -0,0 +1,45 @@
+import unittest
+
+
+# O(n) time. O(1) space.
+class Solution(object):
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        lo = hi = 0
+        for ch in s:
+            if ch == '(':
+                hi += 1
+                lo += 1
+            elif ch == ')':
+                if hi == 0:
+                    return False
+                hi -= 1
+                if lo > 0:
+                    lo -= 1
+            else:
+                hi += 1
+                if lo > 0:
+                    lo -= 1
+        return lo == 0
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test('()', True)
+        self._test('(*)', True)
+        self._test('(*))', True)
+        self._test('(*()', True)
+        self._test('*', True)
+        self._test('**', True)
+        self._test('())*', False)
+
+    def _test(self, s, expected):
+        actual = Solution().checkValidString(s)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
