8950e2fa99982419ae0745921abbe2178bff3517
Chris Xue <chrisxue815@gmail.com>
2017-10-14 10:47:11 +0100

    665. Non-decreasing Array. O(n) time. O(1) space.

---
+++ p665.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+# O(n) time. O(1) space.
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        allow_mod = True
+        for i in xrange(len(nums) - 1):
+            if nums[i] <= nums[i + 1]:
+                continue
+
+            if not allow_mod:
+                return False
+
+            allow_mod = False
+
+            if i < 1 or nums[i - 1] <= nums[i + 1]:
+                continue
+
+            if i + 2 >= len(nums) or nums[i] <= nums[i + 2]:
+                nums[i + 1] = nums[i]
+                continue
+
+            return False
+        return True
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([4, 2, 3], True)
+        self._test([4, 2, 1], False)
+
+    def _test(self, nums, expected):
+        actual = Solution().checkPossibility(nums)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
