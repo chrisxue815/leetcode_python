1c761ec860cc31e96eb8927a37f3d7ac20139517
Chris Xue <chrisxue815@gmail.com>
2017-10-22 14:29:18 +0100

    668. Kth Smallest Number in Multiplication Table. O(min(m, n) * log(mn) time. O(1) space. Binary search.

---
+++ p668_binary_search.py
@@ -0,0 +1,50 @@
+import unittest
+
+
+def _count_smaller_numbers(num, m, n):
+    count = 0
+    for a in xrange(1, m + 1):
+        count += min(num // a, n)
+    return count
+
+
+# O(min(m, n) * log(mn) time. O(1) space. Binary search.
+class Solution(object):
+    def findKthNumber(self, m, n, k):
+        """
+        :type m: int
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if m > n:
+            m, n = n, m
+
+        lo = 1
+        hi = m * n
+
+        while lo <= hi:
+            mid = lo + ((hi - lo) >> 1)
+            count = _count_smaller_numbers(mid, m, n)
+            if count < k:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        return lo
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(3, 3, 5, 3)
+        self._test(3, 3, 4, 3)
+        self._test(2, 3, 6, 6)
+        self._test(42, 34, 401, 126)
+
+    def _test(self, m, n, k, expected):
+        actual = Solution().findKthNumber(m, n, k)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
