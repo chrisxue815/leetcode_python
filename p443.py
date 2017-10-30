9efe5c0cc5795cf3d08e32fcdc571f72691934ff
Chris Xue <chrisxue815@gmail.com>
2017-10-30 18:10:51 +0000

    443. String Compression. O(n) time. O(1) space.

---
+++ p443.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+# O(n) time. O(1) space.
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        if not chars:
+            return 0
+
+        prev = chars[0]
+        count = 0
+        w = 0
+
+        for r in xrange(len(chars) + 1):
+            ch = chars[r] if r < len(chars) else '\0'
+            if ch == prev:
+                count += 1
+            else:
+                chars[w] = prev
+                w += 1
+                if count > 1:
+                    count_str = str(count)
+                    chars[w:w + len(count_str)] = count_str
+                    w += len(count_str)
+                prev = ch
+                count = 1
+
+        return w
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test(["a", "a", "b", "b", "c", "c", "c"], ["a", "2", "b", "2", "c", "3"])
+        self._test(["a"], ["a"])
+        self._test(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"], ["a", "b", "1", "2"])
+
+    def _test(self, chars, expected):
+        actual = Solution().compress(chars)
+        self.assertEqual(chars[:actual], expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
