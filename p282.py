252b7711e6075d1e13867681529baafd8e2c865c
Chris Xue <chrisxue815@gmail.com>
2017-10-31 19:04:16 +0000

    282. Expression Add Operators. Backtracking.

---
+++ p282.py
@@ -0,0 +1,49 @@
+import unittest
+
+
+def _dfs(num, target, result, path, lo, val, multi):
+    if lo == len(num):
+        if val == target:
+            result.append(''.join(path))
+        return
+
+    max_hi = lo + 2 if num[lo] == '0' else len(num) + 1
+
+    for hi in xrange(lo + 1, max_hi):
+        curr_str = num[lo:hi]
+        curr_int = int(curr_str)
+        if lo == 0:
+            _dfs(num, target, result, curr_str, hi, curr_int, curr_int)
+        else:
+            _dfs(num, target, result, path + '+' + curr_str, hi, val + curr_int, curr_int)
+            _dfs(num, target, result, path + '-' + curr_str, hi, val - curr_int, -curr_int)
+            _dfs(num, target, result, path + '*' + curr_str, hi, val - multi + multi * curr_int, multi * curr_int)
+
+
+class Solution(object):
+    def addOperators(self, num, target):
+        """
+        :type num: str
+        :type target: int
+        :rtype: List[str]
+        """
+        result = []
+        _dfs(num, target, result, '', 0, 0, 0)
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test("123", 6, ["1+2+3", "1*2*3"])
+        self._test("232", 8, ["2*3+2", "2+3*2"])
+        self._test("105", 5, ["1*0+5", "10-5"])
+        self._test("00", 0, ["0+0", "0-0", "0*0"])
+        self._test("3456237490", 9191, [])
+
+    def _test(self, num, target, expected):
+        actual = Solution().addOperators(num, target)
+        self.assertItemsEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
