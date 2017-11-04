f8208515370e999e64a72eb16044bbf553604fea
Chris Xue <chrisxue815@gmail.com>
2017-11-04 10:35:18 +0000

    164. Maximum Gap. O(nlog(n)) time. Built-in sort (Timsort).

---
+++ p164_builtin_sort.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+# O(nlog(n)) time. Built-in sort (Timsort).
+class Solution(object):
+    def maximumGap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+
+        gap = 0
+        for i in xrange(len(nums) - 1):
+            gap = max(gap, nums[i + 1] - nums[i])
+
+        return gap
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([1, 2, 3, 5, 6, 7, 0], 2)
+        self._test([0, 1], 1)
+        self._test([1, 1], 0)
+
+    def _test(self, nums, expected):
+        actual = Solution().maximumGap(nums)
+        self.assertEquals(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
