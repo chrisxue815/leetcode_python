c1f052cb5087a954612f72ac9cf0841316fb7cbd
Chris Xue <chrisxue815@gmail.com>
2017-11-06 20:45:14 +0000

    630. Course Schedule III. O(nlog(n)) time. O(n) space. Greedy.

---
+++ p630.py
@@ -0,0 +1,42 @@
+import unittest
+import heapq
+
+
+# O(nlog(n)) time. O(n) space. Greedy.
+class Solution(object):
+    def scheduleCourse(self, courses):
+        """
+        :type courses: List[List[int]]
+        :rtype: int
+        """
+        courses.sort(key=lambda it: it[1])
+
+        time = 0
+        q = []
+
+        for t, d in courses:
+            time += t
+            if time <= d:
+                heapq.heappush(q, -t)
+            else:
+                time += heapq.heappushpop(q, -t)
+
+        return len(q)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([[100, 200], [200, 1300], [1000, 1250], [2000, 3200]], 3)
+        self._test([[1, 1]], 1)
+        self._test([[1, 2], [2, 3]], 2)
+        self._test([[10, 20], [4, 13], [4, 4], [3, 11], [3, 5], [3, 5]], 4)
+        self._test([[9, 14], [7, 12], [1, 11], [4, 7]], 3)
+        self._test([[9, 20], [4, 14], [4, 10], [6, 7], [2, 14], [8, 10], [6, 6], [5, 7]], 4)
+
+    def _test(self, courses, expected):
+        actual = Solution().scheduleCourse(courses)
+        self.assertEquals(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
