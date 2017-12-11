b3f30830c944f14450f21b67d5f41d2630e298a7
Chris Xue <chrisxue815@gmail.com>
2017-12-11 19:20:27 +0000

    744. Find Smallest Letter Greater Than Target.

---
+++ p744.py
@@ -0,0 +1,43 @@
+import unittest
+
+
+# O(log(n)) time. O(1) space. Binary search.
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        lo = 0
+        hi = len(letters) - 1
+
+        while lo <= hi:
+            mid = lo + ((hi - lo) >> 1)
+            mid_val = letters[mid]
+            if mid_val <= target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        return letters[lo] if lo < len(letters) else letters[0]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        test_cases = [
+            [["c", "f", "j"], "a", "c"],
+            [["c", "f", "j"], "c", "f"],
+            [["c", "f", "j"], "d", "f"],
+            [["c", "f", "j"], "g", "j"],
+            [["c", "f", "j"], "j", "c"],
+            [["c", "f", "j"], "k", "c"],
+        ]
+
+        for test_case in test_cases:
+            actual = Solution().nextGreatestLetter(*test_case[:-1])
+            self.assertEqual(actual, test_case[-1])
+
+
+if __name__ == '__main__':
+    unittest.main()
