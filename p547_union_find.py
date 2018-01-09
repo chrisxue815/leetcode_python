2ac6fcc99ca29f0d2323beb80adfeed5d613cbeb
Chris Xue <chrisxue815@gmail.com>
2018-01-09 18:06:03 +0000

    547. Friend Circles.

---
+++ p547_union_find.py
@@ -0,0 +1,75 @@
+import unittest
+
+
+class UF(object):
+    def __init__(self, n):
+        self._id = range(n)
+        self._count = n
+
+    def find(self, p):
+        while p != self._id[p]:
+            p = self._id[p]
+        return p
+
+    def union(self, p, q):
+        p = self.find(p)
+        q = self.find(q)
+
+        if p == q:
+            return
+
+        self._id[q] = p
+        self._count -= 1
+
+    def connected(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def count(self):
+        return self._count
+
+
+# O(nlog(n)) time. O(n) space. Union-find.
+class Solution(object):
+    def findCircleNum(self, m):
+        """
+        :type m: List[List[int]]
+        :rtype: int
+        """
+        uf = UF(len(m))
+
+        for i, row in enumerate(m):
+            for j in xrange(i + 1, len(m)):
+                if row[j]:
+                    uf.union(i, j)
+
+        return uf.count()
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        self._test([
+            [1, 1, 0],
+            [1, 1, 0],
+            [0, 0, 1],
+        ], 2)
+
+        self._test([
+            [1, 1, 0],
+            [1, 1, 1],
+            [0, 1, 1],
+        ], 1)
+
+        self._test([
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+            [0, 1, 1, 1],
+            [1, 0, 1, 1],
+        ], 1)
+
+    def _test(self, m, expected):
+        actual = Solution().findCircleNum(m)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
