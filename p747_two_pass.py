312280ac2eabb028d35831318ea1d186ad669b7c
Chris Xue <chrisxue815@gmail.com>
2018-01-15 18:15:02 +0000

    747. Largest Number At Least Twice of Others.

---
+++ p747_two_pass.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+# O(n) time. O(1) space.
+class Solution(object):
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        largest = -0x80000000
+        largest_index = -1
+        for i, num in enumerate(nums):
+            if num > largest:
+                largest = num
+                largest_index = i
+        half = largest // 2
+        return largest_index if all(num <= half or num == largest for num in nums) else -1
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = [
+            ([3, 6, 1, 0], 1),
+            ([1, 2, 3, 4], -1),
+        ]
+
+        for case in cases:
+            actual = Solution().dominantIndex(*case[:-1])
+            self.assertEqual(actual, case[-1])
+
+
+if __name__ == '__main__':
+    unittest.main()
