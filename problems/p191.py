424ab36afdb227eef1d366ac185fc18add0a23c7
Chris Xue <chrisxue815@gmail.com>
2018-01-19 18:49:23 +0000

    Adding comment to bit count algorithm

--- problems/p191.py
+++ problems/p191.py
@@ -10,7 +10,7 @@ class Solution(object):
         # Hacker's Delight, Figure 5-2
         # OpenJDK:
         # http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/lang/Integer.java#1082
-        n = n - ((n >> 1) & 0x55555555)
+        n = n - ((n >> 1) & 0x55555555)  # (xx & 01) + ((xx >> 1) & 01) == xx - ((xx >> 1) & 01)
         n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
         n = (n + (n >> 4)) & 0x0f0f0f0f
         n += n >> 8
--- problems/p461_xor.py
+++ problems/p461_xor.py
@@ -13,7 +13,7 @@ class Solution(object):
         # Hacker's Delight, Figure 5-2
         # OpenJDK:
         # http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/lang/Integer.java#1082
-        x = x - ((x >> 1) & 0x55555555)
+        x = x - ((x >> 1) & 0x55555555)  # (xx & 01) + ((xx >> 1) & 01) == xx - ((xx >> 1) & 01)
         x = (x & 0x33333333) + ((x >> 2) & 0x33333333)
         x = (x + (x >> 4)) & 0x0f0f0f0f
         x += x >> 8
