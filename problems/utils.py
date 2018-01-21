f4f5285dc6d92e9de02fc25f6bdd1eb331b4de72
Chris Xue <chrisxue815@gmail.com>
2018-01-21 10:51:04 +0000

    Utility method for loading json from path

---
+++ problems/utils.py
@@ -0,0 +1,11 @@
+import collections
+import json
+
+
+def json_object_hook(d):
+    return collections.namedtuple('X', d.keys())(*d.values())
+
+
+def load_json_from_path(path):
+    with open(path) as f:
+        return json.load(f, object_hook=json_object_hook)
