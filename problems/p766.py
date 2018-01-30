f04e9fcedd622abc39f691fd80c50a3c4808f422
Chris Xue <chrisxue815@gmail.com>
2018-01-30 17:56:18 +0000

    766. Toeplitz Matrix

--- problems/p766.py
+++ problems/p766.py
@@ -9,27 +9,10 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
-        start_row = rows - 1
-        start_col = 0
-
-        for _ in xrange(rows + cols - 1):
-            num = matrix[start_row][start_col]
-            row = start_row + 1
-            col = start_col + 1
-
-            while row < rows and col < cols:
-                if matrix[row][col] != num:
+        for row in xrange(len(matrix) - 1):
+            for col in xrange(len(matrix[0]) - 1):
+                if matrix[row][col] != matrix[row + 1][col + 1]:
                     return False
-                row += 1
-                col += 1
-
-            if start_row > 0:
-                start_row -= 1
-            else:
-                start_col += 1
-
         return True
 
 
