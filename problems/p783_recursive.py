7f1b39e9f812fa1ca762b5a86a23fa45596194c4
Chris Xue <chrisxue815@gmail.com>
2018-02-02 18:12:10 +0000

    783. Minimum Distance Between BST Nodes

---
+++ problems/p783_recursive.py
@@ -0,0 +1,44 @@
+import unittest
+import utils
+from tree import TreeNode
+
+
+# O(n) time. O(log(n)) space. Recursive in-order traversal.
+class Solution(object):
+    def __init__(self):
+        self.min_diff = 0x7FFFFFFF
+        self.prev = None
+
+    def minDiffInBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, node.val - self.prev)
+            self.prev = node.val
+
+            dfs(node.right)
+
+        dfs(root)
+        return self.min_diff
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p783.json').test_cases
+
+        for case in cases:
+            root = TreeNode.from_array(case.root)
+            actual = Solution().minDiffInBST(root)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
