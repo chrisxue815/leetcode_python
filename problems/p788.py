bd9195e8cc0c18390272f64a1014b87ad857f694
Chris Xue <chrisxue815@gmail.com>
2018-02-05 18:15:03 +0000

    788. Rotated Digits

---
+++ problems/p788.py
@@ -0,0 +1,45 @@
+import unittest
+import utils
+
+rotate_to_self = {0, 1, 8}
+rotate_to_other = {2, 5, 6, 9}
+invalid = {3, 4, 7}
+
+
+def is_good(num):
+    valid = False
+    q = num
+    while q:
+        q, r = divmod(q, 10)
+        if r in invalid:
+            return False
+        if r in rotate_to_other:
+            valid = True
+    return valid
+
+
+# O(n) time. O(1) space. Math, hash table.
+class Solution(object):
+    def rotatedDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        for num in xrange(1, n + 1):
+            if is_good(num):
+                count += 1
+        return count
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p788.json').test_cases
+
+        for case in cases:
+            actual = Solution().rotatedDigits(case.n)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
