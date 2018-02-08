bed0db945da0219a807c5d07d340e0e90da98b2e
Chris Xue <chrisxue815@gmail.com>
2018-02-08 17:58:26 +0000

    796. Rotate String

---
+++ problems/p796_naive.py
@@ -0,0 +1,40 @@
+import unittest
+import utils
+
+
+# O(n^2) time. O(1) space. Naive string searching.
+class Solution(object):
+    def rotateString(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: bool
+        """
+        if len(a) != len(b):
+            return False
+        if not a:
+            return True
+
+        for start in xrange(len(a)):
+            i = start - len(a)
+            for j in xrange(len(a)):
+                if a[i] != b[j]:
+                    break
+                i += 1
+            else:
+                return True
+
+        return False
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p796.json').test_cases
+
+        for case in cases:
+            actual = Solution().rotateString(case.a, case.b)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
