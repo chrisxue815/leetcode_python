15399d3d86f3f22b593a7a6112ab3793b40b0a73
Chris Xue <chrisxue815@gmail.com>
2018-03-04 10:10:21 +0000

    399. Evaluate Division

---
+++ problems/p399_dfs.py
@@ -0,0 +1,61 @@
+import collections
+import unittest
+import utils
+
+
+def calc(graph, visited, a, c):
+    adj = graph[a]
+    a_to_c = adj.get(c, None)
+
+    if a_to_c is not None:
+        return a_to_c
+
+    for b, a_to_b in adj.iteritems():
+        if b in visited:
+            continue
+        visited.add(b)
+        b_to_c = calc(graph, visited, b, c)
+        if b_to_c is not None:
+            return a_to_b * b_to_c
+
+    return None
+
+
+# O(n^2) time. O(n^2) space. DFS.
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        """
+        :type equations: List[List[str]]
+        :type values: List[float]
+        :type queries: List[List[str]]
+        :rtype: List[float]
+        """
+        graph = collections.defaultdict(dict)
+
+        for (a, b), k in zip(equations, values):
+            graph[a][b] = k
+            graph[b][a] = 1 / k
+
+        result = []
+
+        for a, b in queries:
+            visited = {a}
+            k = calc(graph, visited, a, b)
+            if k is None:
+                k = -1.0
+            result.append(k)
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p399.json').test_cases
+
+        for case in cases:
+            actual = Solution().calcEquation(case.equations, case.values, case.queries)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
