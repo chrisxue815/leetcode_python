5b215170e9514bbbbb1d2819d355f6242f0bc734
Chris Xue <chrisxue815@gmail.com>
2018-03-24 12:04:26 +0000

    310. Minimum Height Trees

---
+++ problems/p310.py
@@ -0,0 +1,53 @@
+import collections
+import heapq
+import unittest
+import utils
+
+
+# O(V+E) time. O(V) space. BFS.
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = [set() for _ in xrange(n)]
+
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        leaves = []
+
+        for node, successors in enumerate(graph):
+            if len(successors) <= 1:
+                leaves.append(node)
+
+        while n > 2:
+            n -= len(leaves)
+            new_leaves = []
+
+            for leaf in leaves:
+                successor = graph[leaf].pop()
+                graph[successor].remove(leaf)
+
+                if len(graph[successor]) == 1:
+                    new_leaves.append(successor)
+
+            leaves = new_leaves
+
+        return leaves
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p310.json').test_cases
+
+        for case in cases:
+            actual = Solution().findMinHeightTrees(case.n, case.edges)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
