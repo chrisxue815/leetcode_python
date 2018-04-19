a16e63f99c4674a60df0e400715d1a26a9770da6
Chris Xue <chrisxue815@gmail.com>
2018-04-19 17:23:41 +0100

    781. Rabbits in Forest

---
+++ problems/p781.py
@@ -0,0 +1,27 @@
+import collections
+import unittest
+import utils
+
+
+# O(n) time. O(n) space. Hash table, math.
+class Solution(object):
+    def numRabbits(self, answers):
+        """
+        :type answers: List[int]
+        :rtype: int
+        """
+        return sum((answer + 1) * ((count - 1) // (answer + 1) + 1) for answer, count in
+                   collections.Counter(answers).iteritems())
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p781.json').test_cases
+
+        for case in cases:
+            actual = Solution().numRabbits(case.answers)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
