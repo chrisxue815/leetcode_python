1c78d814aa140a1d1d7e71e578eee05a0b29c8bf
Chris Xue <chrisxue815@gmail.com>
2018-04-24 17:21:15 +0100

    807. Max Increase to Keep City Skyline

---
+++ problems/p807.py
@@ -0,0 +1,34 @@
+import unittest
+import utils
+
+
+# O(mn) time. O(m+n) space. Array.
+class Solution(object):
+    def maxIncreaseKeepingSkyline(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        max_height_by_row_num = [0] * len(grid)
+        max_height_by_col_num = [0] * len(grid[0])
+
+        for row_num, row in enumerate(grid):
+            for col_num, cell in enumerate(row):
+                max_height_by_col_num[col_num] = max(max_height_by_col_num[col_num], cell)
+                max_height_by_row_num[row_num] = max(max_height_by_row_num[row_num], cell)
+
+        return sum(min(max_height_by_row_num[row_num], max_height_by_col_num[col_num]) - cell
+                   for row_num, row in enumerate(grid) for col_num, cell in enumerate(row))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p807.json').test_cases
+
+        for case in cases:
+            actual = Solution().maxIncreaseKeepingSkyline(case.grid)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
