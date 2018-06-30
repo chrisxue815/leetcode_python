a4e1af1d63f7b90721ba58e4da96b33a1deae974
Chris Xue <chrisxue815@gmail.com>
2018-06-30 15:46:43 +0100

    309. Best Time to Buy and Sell Stock with Cooldown

--- problems/p309_dp.py
+++ problems/p309_dp.py
@@ -2,28 +2,23 @@ import unittest
 import utils
 
 
-# O(n) time. O(n) space. DP.
+# O(n) time. O(1) space. Space-optimized DP.
 class Solution(object):
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices) < 2:
-            return 0
+        buy = -0x80000000
+        sell = prev_sell = 0
 
-        buy = [0] * len(prices)
-        sell = [0] * len(prices)
+        for price in prices:
+            curr_sell = sell
+            sell = max(sell, buy + price)
+            buy = max(buy, prev_sell - price)
+            prev_sell = curr_sell
 
-        buy[0] = -prices[0]
-        buy[1] = max(-prices[0], -prices[1])
-        sell[1] = max(0, buy[0] + prices[1])
-
-        for i in xrange(2, len(prices)):
-            buy[i] = max(buy[i - 1], sell[i - 2] - prices[i])
-            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i])
-
-        return sell[-1]
+        return sell
 
 
 class Test(unittest.TestCase):
