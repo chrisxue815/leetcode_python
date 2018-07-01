66fd0b0748a0e5ce2ca7a89486f2b55c32a3bd3b
Chris Xue <chrisxue815@gmail.com>
2018-07-01 11:03:54 +0100

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ problems/p714_dp.py
@@ -0,0 +1,33 @@
+import unittest
+import utils
+
+
+# O(n) time. O(1) space. Space-optimized DP.
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        """
+        :type prices: List[int]
+        :type fee: int
+        :rtype: int
+        """
+        sell = 0
+        buy = -0x80000000
+
+        for price in prices:
+            sell = max(sell, buy + price)
+            buy = max(buy, sell - price - fee)
+
+        return sell
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p714.json').test_cases
+
+        for case in cases:
+            actual = Solution().maxProfit(case.prices, case.fee)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
