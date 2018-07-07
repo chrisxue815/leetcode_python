52e5beae4252ab8e7a2c42dd8bed689d4ce190eb
Chris Xue <chrisxue815@gmail.com>
2018-07-07 11:15:03 +0100

    357. Count Numbers with Unique Digits

---
+++ problems/p357.py
@@ -0,0 +1,38 @@
+import unittest
+import utils
+
+
+# O(n) time. O(1) space. DP.
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 0:
+            return 0
+        if n == 0:
+            return 1
+
+        n = min(n, 10)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 9
+
+        for i in xrange(2, n + 1):
+            dp[i] = dp[i - 1] * (11 - i)
+
+        return sum(dp)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p357.json').test_cases
+
+        for case in cases:
+            actual = Solution().countNumbersWithUniqueDigits(case.n)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
