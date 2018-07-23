28ca5a9dc487963a046f21ad9dd92c93cc02b5ed
Chris Xue <chrisxue815@gmail.com>
2018-07-23 18:26:14 +0100

    638. Shopping Offers

--- problems/p638.py
+++ problems/p638.py
@@ -1,10 +1,30 @@
-import itertools
 import unittest
 import utils
 
 
-def compute_index(needs):
-    return sum(item_need << (i * 3) for i, item_need in enumerate(needs))
+def compute_index(sizes):
+    index = 0
+    for size in sizes:
+        index = (index << 3) | size
+    return index
+
+
+def iter_sizes(lo, hi):
+    if all(lo[i] <= hi[i] for i in xrange(len(lo))):
+        sizes = list(lo)
+        yield sizes
+
+        last = len(lo) - 1
+        i = last
+
+        while i >= 0:
+            if sizes[i] == hi[i]:
+                sizes[i] = lo[i]
+                i -= 1
+            else:
+                sizes[i] += 1
+                i = last
+                yield sizes
 
 
 # O(n*m^n) time. O(2^(3*m)) space. Space-optimized DP.
@@ -18,23 +38,22 @@ class Solution(object):
         """
         n = len(price)
         dp = [0] * (compute_index(needs) + 1)
-        all_needs = [range(need + 1) for need in needs]
 
-        for need in itertools.product(*all_needs):
-            market_price = sum(price[i] * need[i] for i in xrange(n))
-            dp[compute_index(need)] = market_price
+        for sizes in iter_sizes([0] * n, needs):
+            market_price = sum(price[i] * sizes[i] for i in xrange(n))
+            dp[compute_index(sizes)] = market_price
 
         for offer in special:
-            market_price = sum(price[i] * offer[i] for i in xrange(n))
             offer_price = offer[-1]
+            market_price = sum(price[i] * offer[i] for i in xrange(n))
             if market_price <= offer_price:
                 continue
 
-            offer_index = compute_index(offer[:-1])
-            all_needs = [range(offer[i], needs[i] + 1) for i in xrange(n)]
+            offer_sizes = offer[:-1]
+            offer_index = compute_index(offer_sizes)
 
-            for curr_needs in itertools.product(*all_needs):
-                curr_index = compute_index(curr_needs)
+            for sizes in iter_sizes(offer_sizes, needs):
+                curr_index = compute_index(sizes)
                 dp[curr_index] = min(dp[curr_index], dp[curr_index - offer_index] + offer_price)
 
         return dp[-1]
