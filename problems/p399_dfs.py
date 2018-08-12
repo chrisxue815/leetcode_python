938b53ab7d19a714c2ba78cdfa80b3b0bb1e3e76
Chris Xue <chrisxue815@gmail.com>
2018-08-12 15:16:50 +0100

    399. Evaluate Division

--- problems/p399_dfs.py
+++ problems/p399_dfs.py
@@ -4,16 +4,16 @@ import utils
 
 
 def calc(graph, visited, a, c):
-    adj = graph[a]
-    a_to_c = adj.get(c, None)
+    if a in visited:
+        return None
+    visited.add(a)
 
+    successors = graph[a]
+    a_to_c = successors.get(c, None)
     if a_to_c is not None:
         return a_to_c
 
-    for b, a_to_b in adj.iteritems():
-        if b in visited:
-            continue
-        visited.add(b)
+    for b, a_to_b in successors.iteritems():
         b_to_c = calc(graph, visited, b, c)
         if b_to_c is not None:
             return a_to_b * b_to_c
@@ -30,19 +30,23 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
+        result = []
         graph = collections.defaultdict(dict)
 
         for (a, b), a_to_b in zip(equations, values):
             graph[a][b] = a_to_b
-            graph[b][a] = 1 / a_to_b
-
-        result = []
+            graph[b][b] = 1.0
+            if a_to_b:
+                graph[b][a] = 1.0 / a_to_b
+                graph[a][a] = 1.0
 
         for a, b in queries:
-            visited = {a}
+            visited = set()
             a_to_b = calc(graph, visited, a, b)
+
             if a_to_b is None:
                 a_to_b = -1.0
+
             result.append(a_to_b)
 
         return result
