18e714ad0f809a06325320e2370d82f1cb10770d
Chris Xue <chrisxue815@gmail.com>
2018-08-16 18:15:55 +0100

    653. Two Sum IV - Input is a BST

--- problems/p653_dfs.py
+++ problems/p653_dfs.py
@@ -1,7 +1,9 @@
 import unittest
+import utils
 from tree import TreeNode
 
 
+# O(nlog(n)) time. O(log(n)) space. Recursive pre-order traversal.
 class Solution(object):
     def findTarget(self, root, k):
         """
@@ -13,11 +15,15 @@ class Solution(object):
         def find_val(node, val, exclude):
             if not node:
                 return False
-            if node.val == val and node is not exclude:
-                return True
-            if node.val >= val and find_val(node.left, val, exclude):
-                return True
-            return node.val <= val and find_val(node.right, val, exclude)
+
+            if node.val == val:
+                if node is not exclude:
+                    return True
+                return find_val(node.left, val, exclude) or find_val(node.right, val, exclude)
+            elif node.val > val:
+                return find_val(node.left, val, exclude)
+            else:
+                return find_val(node.right, val, exclude)
 
         def find_complement(node):
             if not node:
@@ -31,14 +37,12 @@ class Solution(object):
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([5, 3, 6, 2, 4, None, 7], 9, True)
-        self._test([5, 3, 6, 2, 4, None, 7], 28, False)
-        self._test([1], 2, False)
+        cases = utils.load_json_from_path('../leetcode_test_cases/p653.json').test_cases
 
-    def _test(self, root, k, expected):
-        root = TreeNode.from_array(root)
-        actual = Solution().findTarget(root, k)
-        self.assertEqual(expected, actual)
+        for case in cases:
+            root = TreeNode.from_array(case.root)
+            actual = Solution().findTarget(root, case.k)
+            self.assertEqual(case.expected, actual)
 
 
 if __name__ == '__main__':
