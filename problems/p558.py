cbc6ef175d0add4f2a58eb5ea3d9fa3246a2c01a
Chris Xue <chrisxue815@gmail.com>
2018-08-23 18:08:37 +0100

    558. Quad Tree Intersection

---
+++ problems/p558.py
@@ -0,0 +1,36 @@
+class Node(object):
+    """
+    # Definition for a QuadTree node.
+    """
+
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+# O(n^2) time. O(n) space. Recursion.
+class Solution(object):
+    def intersect(self, a, b):
+        """
+        :type a: Node
+        :type b: Node
+        :rtype: Node
+        """
+        if a.isLeaf:
+            return a if a.val else b
+        if b.isLeaf:
+            return b if b.val else a
+
+        tl = self.intersect(a.topLeft, b.topLeft)
+        tr = self.intersect(a.topRight, b.topRight)
+        bl = self.intersect(a.bottomLeft, b.bottomLeft)
+        br = self.intersect(a.bottomRight, b.bottomRight)
+
+        if tl.isLeaf and tr.isLeaf and bl.isLeaf and br.isLeaf and tl.val == tr.val == bl.val == br.val:
+            return tl
+        else:
+            return Node(False, False, tl, tr, bl, br)
