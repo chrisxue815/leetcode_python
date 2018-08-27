087cce0356730ae0044e34f9f5dee4daafa8a9f6
Chris Xue <chrisxue815@gmail.com>
2018-08-27 17:26:35 +0100

    589. N-ary Tree Preorder Traversal

---
+++ problems/p589_iterative_dfs.py
@@ -0,0 +1,28 @@
+class Node(object):
+    """
+    # Definition for a Node.
+    """
+
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        result = []
+        stack = [root]
+
+        while stack:
+            curr = stack.pop()
+            result.append(curr.val)
+            stack += reversed(curr.children)
+
+        return result
