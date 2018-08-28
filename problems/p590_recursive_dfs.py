fb80b1290c30cf2e30c7e810d63cefd76845e0dc
Chris Xue <chrisxue815@gmail.com>
2018-08-28 17:09:55 +0100

    590. N-ary Tree Postorder Traversal

---
+++ problems/p590_recursive_dfs.py
@@ -0,0 +1,29 @@
+class Node(object):
+    """
+    # Definition for a Node.
+    """
+
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        result = []
+
+        def dfs(curr):
+            if not curr:
+                return
+
+            for child in curr.children:
+                dfs(child)
+
+            result.append(curr.val)
+
+        dfs(root)
+        return result
