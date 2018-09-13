c57392e8c4103b9d6cf0eb2f559b47292118ade3
Chris Xue <chrisxue815@gmail.com>
2018-09-13 19:36:22 +0100

    709. To Lower Case

---
+++ problems/p709.py
@@ -0,0 +1,25 @@
+import unittest
+import utils
+
+
+# O(n) time. O(1) space. Iteration.
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join(chr(ord(ch) - ord('A') + ord('a')) if ord('A') <= ord(ch) <= ord('Z') else ch for ch in s)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p709.json').test_cases
+
+        for case in cases:
+            actual = Solution().toLowerCase(case.str)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
