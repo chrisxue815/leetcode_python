4d264e9bac8bdd66f8b3749e5ccf23fda774c6b7
Chris Xue <chrisxue815@gmail.com>
2018-10-13 11:10:23 +0100

    884. Uncommon Words from Two Sentences

---
+++ problems/p884.py
@@ -0,0 +1,46 @@
+import collections
+import unittest
+import utils
+
+
+# O(n) time. O(n) space. Hash table.
+class Solution(object):
+    def uncommonFromSentences(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: List[str]
+        """
+        count_a = collections.Counter()
+        count_b = collections.Counter()
+
+        for word in a.split():
+            count_a[word] += 1
+
+        for word in b.split():
+            count_b[word] += 1
+
+        result = []
+
+        for word, count in count_a.iteritems():
+            if count == 1 and word not in count_b:
+                result.append(word)
+
+        for word, count in count_b.iteritems():
+            if count == 1 and word not in count_a:
+                result.append(word)
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p884.json').test_cases
+
+        for case in cases:
+            actual = Solution().uncommonFromSentences(case.a, case.b)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
