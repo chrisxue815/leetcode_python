757fecd28068e7185aef0db70b4b4ce88d4622dd
Chris Xue <chrisxue815@gmail.com>
2018-10-24 11:14:06 +0100

    910. Smallest Range II

---
+++ problems/p910.py
@@ -0,0 +1,32 @@
+import unittest
+import utils
+
+
+# O(nlog(n)) time. O(1) space. Greedy.
+class Solution(object):
+    def smallestRangeII(self, a, k):
+        """
+        :type a: List[int]
+        :type k: int
+        :rtype: int
+        """
+        a.sort()
+        result = a[-1] - a[0]
+
+        for i in xrange(0, len(a) - 1):
+            result = min(result, max(a[-1] - k, a[i] + k) - min(a[0] + k, a[i + 1] - k))
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p910.json').test_cases
+
+        for case in cases:
+            actual = Solution().smallestRangeII(case.a, case.k)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
