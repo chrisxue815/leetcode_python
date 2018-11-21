d3e1d71e1dceaf19102ce49554071d9279812bb2
Chris Xue <chrisxue815@gmail.com>
2018-11-21 12:16:17 +0000

    938. Range Sum of BST

---
+++ problems/p938.py
@@ -0,0 +1,43 @@
+import unittest
+import utils
+from tree import TreeNode
+
+
+# O(n) time. O(log(n)) space. Recursive pre-order DFS.
+class Solution(object):
+    def rangeSumBST(self, root, l, r):
+        """
+        :type root: TreeNode
+        :type l: int
+        :type r: int
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        if l <= root.val <= r:
+            result = root.val
+        else:
+            result = 0
+
+        if l <= root.val:
+            result += self.rangeSumBST(root.left, l, r)
+
+        if root.val <= r:
+            result += self.rangeSumBST(root.right, l, r)
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p938.json').test_cases
+
+        for case in cases:
+            root = TreeNode.from_array(case.root)
+            actual = Solution().rangeSumBST(root, case.l, case.r)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
