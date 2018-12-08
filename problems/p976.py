caa8e03f04171939c279c41dfe41ba08ab6f41dd
Chris Xue <chrisxue815@gmail.com>
2018-12-08 13:56:06 +0000

    976. Largest Perimeter Triangle

---
+++ problems/p976.py
@@ -0,0 +1,36 @@
+import unittest
+import utils
+
+
+# O(nlog(n)) time. O(1) space. Geometry, sorting.
+class Solution(object):
+    def largestPerimeter(self, a):
+        """
+        :type a: List[int]
+        :rtype: int
+        """
+        a.sort(reverse=True)
+
+        for i in xrange(2, len(a)):
+            u = a[i - 2]
+            v = a[i - 1]
+            w = a[i]
+
+            if u < v + w:
+                return u + v + w
+
+        return 0
+
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p976.json').test_cases
+
+        for case in cases:
+            actual = Solution().largestPerimeter(case.a)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
