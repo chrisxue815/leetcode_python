bcf00817dcfca11e591cfd624b91b54c9116b69e
Chris Xue <chrisxue815@gmail.com>
2018-12-11 10:31:52 +0000

    985. Sum of Even Numbers After Queries

---
+++ problems/p985.py
@@ -0,0 +1,40 @@
+import unittest
+import utils
+
+
+# O(n) time. O(1) space. Algebra.
+class Solution(object):
+    def sumEvenAfterQueries(self, a, queries):
+        """
+        :type a: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
+        answer = sum(val for val in a if val & 1 == 0)
+
+        for val, index in queries:
+            if a[index] & 1 == 0:
+                answer -= a[index]
+
+            a[index] += val
+
+            if a[index] & 1 == 0:
+                answer += a[index]
+
+            result.append(answer)
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_json_from_path('../leetcode_test_cases/p985.json').test_cases
+
+        for case in cases:
+            actual = Solution().sumEvenAfterQueries(case.a, case.queries)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
