e0be58345b280097a3ac1b460f1e484a5cdb249d
Chris Xue <chrisxue815@gmail.com>
2019-01-07 23:13:40 +0000

    1010. Pairs of Songs With Total Durations Divisible by 60

---
+++ problems/p1010_bruteforce.py
@@ -0,0 +1,27 @@
+import itertools
+import unittest
+
+import utils
+
+
+# O(n^2) time. O(n) space. Brute-force, combination, backtracking.
+class Solution(object):
+    def numPairsDivisibleBy60(self, time):
+        """
+        :type time: List[int]
+        :rtype: int
+        """
+        return sum((a + b) % 60 == 0 for a, b in itertools.combinations(time, 2))
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().numPairsDivisibleBy60(case.time)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
