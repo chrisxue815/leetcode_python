6285d95349f837f3e9d9b9995f84f16f328f90ca
Chris Xue <chrisxue815@gmail.com>
2019-01-10 19:28:12 +0000

    1018. Binary Prefix Divisible By 5

---
+++ problems/p1018.py
@@ -0,0 +1,33 @@
+import unittest
+
+import utils
+
+
+# O(n) time. O(1) space. Bit manipulation.
+class Solution(object):
+    def prefixesDivBy5(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[bool]
+        """
+        result = [False] * len(A)
+        num = 0
+
+        for i, bit in enumerate(A):
+            num = (num << 1) | bit
+            result[i] = num % 5 == 0
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().prefixesDivBy5(case.A)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
