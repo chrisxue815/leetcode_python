17c7f430344f57e29f1731403c42c135b22ca5be
Chris Xue <chrisxue815@gmail.com>
2019-01-13 19:36:14 +0000

    1022. Sum of Root To Leaf Binary Numbers

---
+++ problems/p1022_dfs.py
@@ -0,0 +1,40 @@
+import unittest
+
+import utils
+from tree import TreeNode
+
+
+# O(n) time. O(log(n)) space. Recursive post-order DFS.
+class Solution(object):
+    def sumRootToLeaf(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        def dfs(curr, num):
+            if not curr:
+                return 0
+
+            num = (num << 1) | curr.val
+
+            if curr.left or curr.right:
+                return dfs(curr.left, num) + dfs(curr.right, num)
+            else:
+                return num
+
+        return dfs(root, 0)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            root = TreeNode.from_array(case.root)
+            actual = Solution().sumRootToLeaf(root)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
