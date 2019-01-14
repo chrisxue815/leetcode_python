bd95d82f83c3a856ec70a64582b6e833e18d0e6e
Chris Xue <chrisxue815@gmail.com>
2019-01-14 22:33:43 +0000

    1025. Divisor Game

---
+++ problems/p1025.py
@@ -0,0 +1,26 @@
+import unittest
+
+import utils
+
+
+# O(1) time. O(1) space. Number theory, mathematical induction.
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        """
+        return N & 1 == 0
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().divisorGame(case.N)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
