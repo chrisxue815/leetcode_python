c546974ca5ee1e5780bc0bf55b6c16264ba5f29c
Chris Xue <chrisxue815@gmail.com>
2019-01-15 15:32:29 +0000

    1029. Two City Scheduling

---
+++ problems/p1029_greedy.py
@@ -0,0 +1,31 @@
+import unittest
+
+import utils
+
+
+# O(nlog(n)) time. O(n) space. Greedy, intuition, deductive reasoning, sort.
+class Solution(object):
+    def twoCitySchedCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        costs = sorted(((a - b, a, b) for a, b in costs))
+
+        sum_a = sum(a for _, a, _ in costs[:len(costs) // 2])
+        sum_b = sum(b for _, _, b in costs[len(costs) // 2:])
+
+        return sum_a + sum_b
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().twoCitySchedCost(case.costs)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
