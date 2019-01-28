2e9dbcfe34da472fb5fe5f3ebcc5dcf0fb736224
Chris Xue <chrisxue815@gmail.com>
2019-01-28 14:04:14 +0000

    1046. Last Stone Weight

---
+++ problems/p1046.py
@@ -0,0 +1,35 @@
+import heapq
+import unittest
+
+import utils
+
+
+# O(nlog(n)) time. O(n) space. Heap.
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        q = [-stone for stone in stones]
+        heapq.heapify(q)
+
+        while len(q) > 1:
+            y = heapq.heappop(q)
+            x = q[0]
+            heapq.heapreplace(q, y - x)
+
+        return -q[0]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().lastStoneWeight(**vars(case.args))
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
