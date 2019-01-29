9874b0e3262a72dd754c4162fa1442d83196d1d8
Chris Xue <chrisxue815@gmail.com>
2019-01-29 17:26:09 +0000

    1047. Remove All Adjacent Duplicates In String

--- problems/p1047.py
+++ problems/p1047.py
@@ -3,24 +3,23 @@ import unittest
 import utils
 
 
-# O(n) time. O(n) space. String.
+# O(n) time. O(n) space. Stack.
 class Solution(object):
     def removeDuplicates(self, S):
         """
         :type S: str
         :rtype: str
         """
-        i = 1
+        stack = []
 
-        while i < len(S):
-            if S[i - 1] == S[i]:
-                S = S[:i - 1] + S[i + 1:]
-                if i > 1:
-                    i -= 1
-            else:
-                i += 1
+        for ch in S:
+            stack.append(ch)
 
-        return S
+            while len(stack) >= 2 and stack[-1] == stack[-2]:
+                stack.pop()
+                stack.pop()
+
+        return ''.join(stack)
 
 
 class Test(unittest.TestCase):
