c8744419eb93b2bb5b532a9b785a1af683312f87
Chris Xue <chrisxue815@gmail.com>
2019-02-05 21:38:01 +0000

    1078. Occurrences After Bigram

---
+++ problems/test_1078.py
@@ -0,0 +1,24 @@
+import unittest
+from typing import List
+
+import utils
+
+
+# O(n) time. O(n) space. String.
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split()
+        return [words[i + 2] for i in range(len(words) - 2) if words[i] == first and words[i + 1] == second]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().findOcurrences(**case.args._asdict())
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
