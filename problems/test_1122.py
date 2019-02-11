3c7f0df4d1ca2a2bbd6f24882af2d7bcb8d019e2
Chris Xue <chrisxue815@gmail.com>
2019-02-11 14:15:42 +0000

    1122. Relative Sort Array

---
+++ problems/test_1122.py
@@ -0,0 +1,34 @@
+import collections
+import unittest
+from typing import List
+
+import utils
+
+
+# O(nlog(n)) time. O(n) space. Counting sort, hash table, sort.
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        counts = collections.Counter(arr1)
+
+        for num in arr2:
+            result += [num] * counts[num]
+            del counts[num]
+
+        for num, count in sorted(counts.items()):
+            result += [num] * count
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().relativeSortArray(**case.args._asdict())
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
