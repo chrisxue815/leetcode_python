ef0dec1aabe185dbd2dd243d4e5356c24c8cd959
Chris Xue <chrisxue815@gmail.com>
2019-02-13 15:16:25 +0000

    1128. Number of Equivalent Domino Pairs

---
+++ problems/test_1128.py
@@ -0,0 +1,25 @@
+import collections
+import unittest
+from typing import List
+
+import utils
+
+
+# O(n) time. O(n) space. Counting, combination.
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        counts = collections.Counter((a, b) if a <= b else (b, a) for a, b in dominoes)
+        return sum(count * (count - 1) // 2 for count in counts.values())
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().numEquivDominoPairs(**case.args._asdict())
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
