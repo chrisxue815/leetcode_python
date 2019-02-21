3ca62317e50288aefaa92b0622860db5c032d015
Chris Xue <chrisxue815@gmail.com>
2019-02-21 16:10:19 +0000

    152. Maximum Product Subarray

--- problems/test_0152.py
+++ problems/test_0152.py
@@ -4,63 +4,20 @@ from typing import List
 import utils
 
 
-def calc_max_product(a, b, c, d, e):
-    if b == 0:
-        return a
-    if d == 0:
-        if c >= 0:
-            return max(a, c)
-        else:
-            return (a if a else 1) * b * c
-    if c >= 0:
-        return (a if a else 1) * b * (c if c else 1) * d * (e if e else 1)
-    else:
-        return max((a if a else 1) * b * c, e, a, c * d * (e if e else 1))
-
-
-# O(n) time. O(1) space. Logic.
+# O(n) time. O(1) space. DP.
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-
-        result = 0
-        a = b = c = d = e = 0
+        result = nums[0]
+        left_max = 0
+        right_max = 0
 
         for num in nums:
-            if num > 0:
-                if e:
-                    e *= num
-                else:
-                    e = num
-            elif num < 0:
-                if d < 0:
-                    c = (c if c else 1) * d * (e if e else 1)
-                    d = num
-                elif b < 0:
-                    c = e
-                    d = num
-                else:
-                    a = e
-                    b = num
-                e = 0
-            else:
-                if b == 0:
-                    a = e
-                    e = 0
-                elif d == 0:
-                    c = e
-                    e = 0
-                result = max(result, calc_max_product(a, b, c, d, e))
-                a = b = c = d = e = 0
+            left_max = left_max * num if left_max else num
+            result = max(result, left_max)
 
-        if b == 0:
-            a = e
-            e = 0
-        elif d == 0:
-            c = e
-            e = 0
-        result = max(result, calc_max_product(a, b, c, d, e))
+        for num in reversed(nums):
+            right_max = right_max * num if right_max else num
+            result = max(result, right_max)
 
         return result
 
