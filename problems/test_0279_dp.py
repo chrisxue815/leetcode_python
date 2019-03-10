dd56688370a6d67d04c983318df5b0c93fb77eec
Chris Xue <chrisxue815@gmail.com>
2019-03-10 21:41:49 +0000

    279. Perfect Squares

---
+++ problems/test_0279_dp.py
@@ -0,0 +1,40 @@
+import unittest
+
+import math
+
+import utils
+
+DP = [0, 1]
+
+
+# O(n * sqrt(n)) time. O(n) space. DP, cheating.
+# Better solution: Lagrange's four-square theorem.
+# See https://leetcode.com/problems/perfect-squares/discuss/71488
+class Solution:
+    def numSquares(self, n: int) -> int:
+        for num in range(len(DP), n + 1):
+            start = int(math.sqrt(num))
+            min_ = 0x7fffffff
+
+            for root in range(start, 0, -1):
+                count = DP[num - root * root]
+                min_ = min(min_, count)
+                if min_ <= 1:
+                    break
+
+            DP.append(min_ + 1)
+
+        return DP[n]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().numSquares(**case.args._asdict())
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
