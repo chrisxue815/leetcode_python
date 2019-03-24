daad258ae3a722572ea07c6ce1ea1bb8a61644c9
Chris Xue <chrisxue815@gmail.com>
2019-03-24 19:37:37 +0000

    376. Wiggle Subsequence

---
+++ problems/test_0376_dp.py
@@ -0,0 +1,39 @@
+import unittest
+from typing import List
+
+import utils
+
+
+# O(n) time. O(n) space. DP.
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        inc = [0] * len(nums)  # inc[i]: the length of the longest wiggle subsequence in nums[:i+1] where nums[i-1] < nums[i]
+        dec = [0] * len(nums)  # dec[i]: the length of the longest wiggle subsequence in nums[:i+1] where nums[i-1] > nums[i]
+        inc[0] = 1
+        dec[0] = 1
+
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                inc[i] = dec[i - 1] + 1
+                dec[i] = dec[i - 1]
+            elif nums[i - 1] > nums[i]:
+                inc[i] = inc[i - 1]
+                dec[i] = inc[i - 1] + 1
+            else:
+                inc[i] = inc[i - 1]
+                dec[i] = dec[i - 1]
+
+        return max(inc[-1], dec[-1])
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().wiggleMaxLength(**case.args._asdict())
+            self.assertEqual(case.expected, actual, case.args)
+
+
+if __name__ == '__main__':
+    unittest.main()
