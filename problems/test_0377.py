454bac1115de7a831ef826da43dbf8a5aa7604ae
Chris Xue <chrisxue815@gmail.com>
2019-03-26 15:39:20 +0000

    377. Combination Sum IV

---
+++ problems/test_0377.py
@@ -0,0 +1,29 @@
+import unittest
+from typing import List
+
+import utils
+
+
+# O(target * len(nums)) time. O(target) space. DP.
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for i in range(1, target + 1):
+            dp[i] = sum(dp[i - num] for num in nums if i >= num)
+
+        return dp[-1]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            actual = Solution().combinationSum4(**case.args._asdict())
+            self.assertEqual(case.expected, actual, case.args)
+
+
+if __name__ == '__main__':
+    unittest.main()
