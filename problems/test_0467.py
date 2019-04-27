fcf35f5c0b824ae48b8f08e4855ce9f1b1dede9c
Chris Xue <chrisxue815@gmail.com>
2019-04-27 18:25:40 +0100

    467. Unique Substrings in Wraparound String

--- problems/test_0467.py
+++ problems/test_0467.py
@@ -6,40 +6,19 @@ import utils
 # O(n) time. O(1) space. String, DP.
 class Solution:
     def findSubstringInWraproundString(self, p: str) -> int:
-        if not p:
-            return 0
+        count = [0] * 26
+        sub_len = 0
 
-        lengths = [0] * 26
-        lo = 0
+        for i, c in enumerate(p):
+            if ord(p[i - 1]) + 1 == ord(c) or c == 'a' and p[i - 1] == 'z':
+                sub_len += 1
+            else:
+                sub_len = 1
 
-        for hi in range(1, len(p)):
-            if ord(p[hi - 1]) + 1 == ord(p[hi]) or p[hi - 1] == 'z' and p[hi] == 'a':
-                continue
+            index = ord(c) - ord('a')
+            count[index] = max(count[index], sub_len)
 
-            start = ord(p[lo]) - ord('a')
-            lengths[start] = max(lengths[start], hi - lo)
-            lo = hi
-
-        hi = len(p)
-        start = ord(p[lo]) - ord('a')
-        lengths[start] = max(lengths[start], hi - lo)
-
-        count = [[0] * 26 for _ in range(26)]
-
-        for start in range(26):
-            max_length = lengths[start]
-            if max_length == 0:
-                continue
-
-            for offset in range(max_length):
-                q, r = divmod(max_length - offset, 26)
-                lo = (start + offset) % 26
-
-                for length in range(26):
-                    hi = (lo + length) % 26
-                    count[lo][hi] = max(count[lo][hi], q + (length < r))
-
-        return sum(sum(c) for c in count)
+        return sum(count)
 
 
 class Test(unittest.TestCase):
