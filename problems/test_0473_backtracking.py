e5d430ad1d0a342ba62c51ae21f6f06946305e69
Chris Xue <chrisxue815@gmail.com>
2019-05-07 10:59:43 +0100

    473. Matchsticks to Square

--- problems/test_0473_backtracking.py
+++ problems/test_0473_backtracking.py
@@ -4,7 +4,7 @@ from typing import List
 import utils
 
 
-# O(n!) time. O(n) space. Partition problem, backtracking.
+# O(4^n) time. O(n) space. Partition problem, backtracking.
 class Solution:
     def makesquare(self, nums: List[int]) -> bool:
         if len(nums) < 4:
@@ -16,26 +16,22 @@ class Solution:
         if r:
             return False
 
-        def dfs(start, target):
-            if start == len(nums):
-                return target == 0
+        sums = [0] * 4
 
-            if target == 0:
-                target = side
+        def dfs(index):
+            if index == len(nums):
+                return True
 
-            for i in range(start, len(nums)):
-                new_target = target - nums[i]
-                if new_target >= 0:
-                    nums[start], nums[i] = nums[i], nums[start]
-
-                    if dfs(start + 1, new_target):
+            for i in range(4):
+                if sums[i] + nums[index] <= side:
+                    sums[i] += nums[index]
+                    if dfs(index + 1):
                         return True
-
-                    nums[start], nums[i] = nums[i], nums[start]
+                    sums[i] -= nums[index]
 
             return False
 
-        return dfs(0, side)
+        return dfs(0)
 
 
 class Test(unittest.TestCase):
