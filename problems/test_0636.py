f99abd34f96e8d0baf2e029ab65293761890397c
Chris Xue <chrisxue815@gmail.com>
2019-06-01 19:50:50 +0100

    636. Exclusive Time of Functions

--- problems/test_0636.py
+++ problems/test_0636.py
@@ -1,29 +1,30 @@
 import unittest
+from typing import List
+
+import utils
 
 
+# O(n). O(depth) space. Stack.
 class Solution:
-    def exclusiveTime(self, n, logs):
-        """
-        :type n: int
-        :type logs: List[str]
-        :rtype: List[int]
-        """
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         result = [0] * n
         stack = []
         prev_time = 0
 
         for log in logs:
-            parts = log.split(':')
-            func = int(parts[0])
-            time = int(parts[2])
-            if parts[1] == 'end':
-                time += 1
-                result[func] += time - prev_time
-                stack.pop()
-            else:
+            func, action, time = log.split(':')
+            func = int(func)
+            time = int(time)
+
+            if action.startswith('s'):
                 if stack:
                     result[stack[-1]] += time - prev_time
                 stack.append(func)
+            else:
+                time += 1
+                result[func] += time - prev_time
+                stack.pop()
+
             prev_time = time
 
         return result
@@ -31,16 +32,12 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test(2, [
-            '0:start:0',
-            '1:start:2',
-            '1:end:5',
-            '0:end:6',
-        ], [3, 4])
+        cases = utils.load_test_json(__file__).test_cases
 
-    def _test(self, n, logs, expected):
-        actual = Solution().exclusiveTime(n, logs)
-        self.assertEqual(expected, actual)
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().exclusiveTime(**case.args.__dict__)
+            self.assertEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
