e377890d863ee1c14154246a030c32dbd25fc051
Chris Xue <chrisxue815@gmail.com>
2019-06-03 18:24:08 +0100

    416. Partition Equal Subset Sum

--- problems/test_0416_hashset.py
+++ problems/test_0416_hashset.py
@@ -4,7 +4,7 @@ from typing import List
 import utils
 
 
-# O(n * 2^n) time. O(2^n) space. DP.
+# O(sum(nums)) time. O(sum(nums)) space. DP, hash set.
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         s = sum(nums)
@@ -13,18 +13,21 @@ class Solution:
 
         target = s >> 1
         sums = set()
+        new_sums = []
 
         for num in nums:
             if num == target:
                 return True
-            new_sums = []
+
             for s in sums:
                 s += num
                 if s == target:
                     return True
                 new_sums.append(s)
+
             sums.add(num)
             sums.update(new_sums)
+            new_sums.clear()
 
         return False
 
