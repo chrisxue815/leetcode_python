1074c98ca43eec87b24a858680e0c2d77bde8d78
Chris Xue <chrisxue815@gmail.com>
2019-06-06 22:34:37 +0100

    322. Coin Change

--- problems/test_0322.py
+++ problems/test_0322.py
@@ -3,23 +3,21 @@ from typing import List
 
 import utils
 
-MAX_INT = 0x7fffffff
-
 
 # O(len(coins) * amount) time. O(amount) space. Space-optimized DP, unbounded knapsack.
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        max_num_coins = amount + 1
+
         # dp[j]: the fewest number of coins you need in coins[:i] to make up amount j
-        dp = [MAX_INT] * (amount + 1)
+        dp = [max_num_coins] * max_num_coins
         dp[0] = 0
 
-        for i in range(1, len(coins) + 1):
-            for j in range(coins[i - 1], amount + 1):
-                num = dp[j - coins[i - 1]]
-                if num != MAX_INT:
-                    dp[j] = min(dp[j], num + 1)
+        for coin in coins:
+            for j in range(coin, max_num_coins):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
 
-        return dp[amount] if dp[amount] != MAX_INT else -1
+        return dp[amount] if dp[amount] < max_num_coins else -1
 
 
 class Test(unittest.TestCase):
