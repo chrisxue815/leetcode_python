92ded15e177967136cb21b43c4f44b219ff3a807
Chris Xue <chrisxue815@gmail.com>
2019-06-07 11:30:50 +0100

    377. Combination Sum IV

--- problems/test_0377.py
+++ problems/test_0377.py
@@ -4,16 +4,17 @@ from typing import List
 import utils
 
 
-# O(target * len(nums)) time. O(target) space. DP.
+# O(target * len(nums)) time. O(target) space. DP, unbounded knapsack.
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        # dp[i]: the number of possible permutations that add up to i
         dp = [0] * (target + 1)
         dp[0] = 1
 
         for i in range(1, target + 1):
             dp[i] = sum(dp[i - num] for num in nums if i >= num)
 
-        return dp[-1]
+        return dp[target]
 
 
 class Test(unittest.TestCase):
