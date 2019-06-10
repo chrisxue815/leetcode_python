a2d5e7a49606a49251709b309f01bd9af12048ea
Chris Xue <chrisxue815@gmail.com>
2019-06-10 12:23:00 +0100

    474. Ones and Zeroes

--- problems/test_0474.py
+++ problems/test_0474.py
@@ -4,24 +4,21 @@ from typing import List
 import utils
 
 
-# O(m * n * len(strs)) time. O(m * n * len(strs)) space. DP, 2D 0-1 knapsack.
+# O(m * n * len(strs)) time. O(m * n) space. Space-optimized DP, 2D 0-1 knapsack.
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        counts = []
+        # dp[a][b]: the maximum number of strings in strs[:i] that you can form with given a 0s and b 1s
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
         for s in strs:
             zeros = sum(c == '0' for c in s)
-            counts.append((zeros, len(s) - zeros))
+            ones = len(s) - zeros
 
-        # dp[i][a][b]: the maximum number of strings in strs[:i] that you can form with given a 0s and b 1s
-        dp = [[[0] * (n + 1) for _ in range(m + 1)] for _ in range(len(strs) + 1)]
+            for a in range(m, zeros - 1, -1):
+                for b in range(n, ones - 1, -1):
+                    dp[a][b] = max(dp[a][b], dp[a - zeros][b - ones] + 1)
 
-        for i in range(1, len(strs) + 1):
-            zeros, ones = counts[i - 1]
-            for a in range(m + 1):
-                for b in range(n + 1):
-                    dp[i][a][b] = max(dp[i - 1][a][b], dp[i - 1][a - zeros][b - ones] + 1 if a >= zeros and b >= ones else 0)
-
-        return dp[len(strs)][m][n]
+        return dp[m][n]
 
 
 class Test(unittest.TestCase):
