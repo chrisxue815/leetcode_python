887d113e25e786ee6bbd76b57fb1edb44ed62907
Chris Xue <chrisxue815@gmail.com>
2019-08-01 17:45:43 +0100

    1029. Two City Scheduling

--- problems/test_1029_greedy.py
+++ problems/test_1029_greedy.py
@@ -1,19 +1,16 @@
 import unittest
+from typing import List
 
 import utils
 
 
 # O(nlog(n)) time. O(n) space. Greedy, intuition, deductive reasoning, sort.
 class Solution:
-    def twoCitySchedCost(self, costs):
-        """
-        :type costs: List[List[int]]
-        :rtype: int
-        """
-        costs = sorted(((a - b, a, b) for a, b in costs))
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda cost: cost[0] - cost[1])
 
-        sum_a = sum(a for _, a, _ in costs[:len(costs) // 2])
-        sum_b = sum(b for _, _, b in costs[len(costs) // 2:])
+        sum_a = sum(a for a, _ in costs[:len(costs) // 2])
+        sum_b = sum(b for _, b in costs[len(costs) // 2:])
 
         return sum_a + sum_b
 
