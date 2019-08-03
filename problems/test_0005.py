6852e25c7c15294756f3ef89f4b3c8d6b628eca1
Chris Xue <chrisxue815@gmail.com>
2019-08-03 10:45:11 +0100

    5. Longest Palindromic Substring

--- problems/test_0005.py
+++ problems/test_0005.py
@@ -1,27 +1,31 @@
 import unittest
 
+import utils
 
+
+# O(n^2) time. O(1) space. Palindrome.
 class Solution:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+    def longestPalindrome(self, s: str) -> str:
         n = len(s)
         max_lo = 0
         max_len = 0
-        i = 0
+        start = 0
 
-        while i < n:
-            hi = i + 1
+        while start < n:
+            lo = start - 1
+            hi = start + 1
+
             while hi < n and s[hi] == s[hi - 1]:
                 hi += 1
-            lo = i - 1
-            i = hi
+
+            start = hi
+
             while lo >= 0 and hi < n and s[lo] == s[hi]:
                 lo -= 1
                 hi += 1
+
             length = hi - lo
+
             if length > max_len:
                 max_len = length
                 max_lo = lo
@@ -31,12 +35,12 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test('babad', 'bab')
-        self._test('cbbd', 'bb')
+        cases = utils.load_test_json(__file__).test_cases
 
-    def _test(self, s, expected):
-        actual = Solution().longestPalindrome(s)
-        self.assertEqual(expected, actual)
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().longestPalindrome(**case.args.__dict__)
+            self.assertEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
