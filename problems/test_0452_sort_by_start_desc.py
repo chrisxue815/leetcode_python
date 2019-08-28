1e351a13233d8b490b595b53714c3603a56f1890
Chris Xue <chrisxue815@gmail.com>
2019-08-28 19:37:21 +0100

    452. Minimum Number of Arrows to Burst Balloons

---
+++ problems/test_0452_sort_by_start_desc.py
@@ -0,0 +1,36 @@
+import unittest
+from typing import List
+
+import utils
+
+
+# O(nlog(n)) time. O(1) space. Interval, sorting by start in descending order, greedy.
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        points.sort(reverse=True)
+        result = 0
+        prev_start = points[0][1] + 1
+
+        for s, e in points:
+            if e < prev_start:
+                result += 1
+                prev_start = s
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().findMinArrowShots(**case.args.__dict__)
+            self.assertEqual(case.expected, actual, msg=args)
+
+
+if __name__ == '__main__':
+    unittest.main()
