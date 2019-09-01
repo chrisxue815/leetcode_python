a35c30a5066574917171b1fd5bbc99a042cc85ea
Chris Xue <chrisxue815@gmail.com>
2019-09-01 16:41:09 +0100

    56. Merge Intervals

--- problems/test_0056_sort_by_end_desc.py
+++ problems/test_0056_sort_by_end_desc.py
@@ -4,20 +4,20 @@ from typing import List
 import utils
 
 
-# O(nlog(n)) time. O(1) space. Interval, sorting by start, greedy.
+# O(nlog(n)) time. O(1) space. Interval, sorting by end in descending order, greedy.
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
 
-        intervals.sort()
+        intervals.sort(key=lambda interval: (interval[1], interval[0]), reverse=True)
         result = [intervals[0]]
 
         for start, end in intervals:
-            if result[-1][1] < start:
+            if end < result[-1][0]:
                 result.append([start, end])
             else:
-                result[-1][1] = max(result[-1][1], end)
+                result[-1][0] = min(result[-1][0], start)
 
         return result
 
@@ -29,7 +29,7 @@ class Test(unittest.TestCase):
         for case in cases:
             args = str(case.args)
             actual = Solution().merge(**case.args.__dict__)
-            self.assertEqual(case.expected, actual, msg=args)
+            self.assertCountEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
---
+++ problems/test_0056_sort_by_start_asc.py
@@ -0,0 +1,36 @@
+import unittest
+from typing import List
+
+import utils
+
+
+# O(nlog(n)) time. O(1) space. Interval, sorting by start in ascending order, greedy.
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        intervals.sort()
+        result = [intervals[0]]
+
+        for start, end in intervals:
+            if result[-1][1] < start:
+                result.append([start, end])
+            else:
+                result[-1][1] = max(result[-1][1], end)
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().merge(**case.args.__dict__)
+            self.assertCountEqual(case.expected, actual, msg=args)
+
+
+if __name__ == '__main__':
+    unittest.main()
