642bca1202afada89cd6c66bb929c8e3d2673b09
Chris Xue <chrisxue815@gmail.com>
2019-09-08 11:07:54 +0100

    436. Find Right Interval

--- problems/test_0436_binarysearch.py
+++ problems/test_0436_binarysearch.py
@@ -1,62 +1,50 @@
-import functools
 import unittest
+from typing import List
+
+import utils
 
 
-# Definition for an interval.
-class Interval:
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+def bisect_left(starts, target):
+    lo = 0
+    hi = len(starts) - 1
+
+    while lo <= hi:
+        mid = lo + ((hi - lo) >> 1)
+        mid_val = starts[mid][0]
+        if mid_val < target:
+            lo = mid + 1
+        elif mid_val > target:
+            hi = mid - 1
+        else:
+            return mid
+
+    return lo
 
 
+# O(nlog(n)) time. O(n) space. Interval, sorting by start, binary search.
 class Solution:
-    def findRightInterval(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[int]
-        """
-        result = []
-        starts = []
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        result = [0] * len(intervals)
+        starts = sorted((start, i) for i, (start, end) in enumerate(intervals))
 
-        for i, interval in enumerate(intervals):
-            starts.append((interval.start, i))
-
-        starts.sort()
-
-        for i in intervals:
-            lo = 0
-            hi = len(starts) - 1
-            end = i.end
-
-            while lo <= hi:
-                mid = lo + ((hi - lo) >> 1)
-                mid_val = starts[mid][0]
-
-                if mid_val < end:
-                    lo = mid + 1
-                elif mid_val > end:
-                    hi = mid - 1
-                else:
-                    lo = mid
-                    break
-            if lo < len(starts):
-                result.append(starts[lo][1])
+        for i, (start, end) in enumerate(intervals):
+            index = bisect_left(starts, end)
+            if index < len(intervals):
+                result[i] = starts[index][1]
             else:
-                result.append(-1)
+                result[i] = -1
 
         return result
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([[1, 2]], [-1])
-        self._test([[3, 4], [2, 3], [1, 2]], [-1, 0, 1])
-        self._test([[1, 4], [2, 3], [3, 4]], [-1, 2, -1])
+        cases = utils.load_test_json(__file__).test_cases
 
-    def _test(self, intervals, expected):
-        intervals = [Interval(a[0], a[1]) for a in intervals]
-        actual = Solution().findRightInterval(intervals)
-        self.assertEqual(expected, actual)
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().findRightInterval(**case.args.__dict__)
+            self.assertEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
