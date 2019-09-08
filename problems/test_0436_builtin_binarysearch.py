fc5bc4b39eeab4a416226b048a14ed5522832b99
Chris Xue <chrisxue815@gmail.com>
2019-09-08 11:20:05 +0100

    436. Find Right Interval

--- problems/test_0436_builtin_binarysearch.py
+++ problems/test_0436_builtin_binarysearch.py
@@ -5,31 +5,15 @@ from typing import List
 import utils
 
 
-class KeyedSequence:
-    def __init__(self, sequence, key_func):
-        self.sequence = sequence
-        self.key_func = key_func
-
-    def __getitem__(self, i):
-        return self.key_func(self.sequence[i])
-
-    def __len__(self):
-        return len(self.sequence)
-
-
 # O(nlog(n)) time. O(n) space. Interval, sorting by start, binary search.
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
         result = [0] * len(intervals)
         starts = sorted((start, i) for i, (start, end) in enumerate(intervals))
-        keyed_starts = KeyedSequence(starts, lambda start: start[0])
 
         for i, (start, end) in enumerate(intervals):
-            index = bisect.bisect_left(keyed_starts, end)
-            if index < len(intervals):
-                result[i] = starts[index][1]
-            else:
-                result[i] = -1
+            index = bisect.bisect_left(starts, (end,))
+            result[i] = starts[index][1] if index < len(intervals) else -1
 
         return result
 
