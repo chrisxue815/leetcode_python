c25d058d8f1a35a33bbe01e82efd0ebe71c6874c
Chris Xue <chrisxue815@gmail.com>
2019-09-13 13:40:40 +0100

    743. Network Delay Time

--- problems/test_0743_bfs.py
+++ problems/test_0743_bfs.py
@@ -1,24 +1,20 @@
 import heapq
 import unittest
+from typing import List
+
 import utils
 
 
-# O(V+E) time. O(V) space. BFS.
+# O(V + Elog(E)) time. O(V + E) space. Graph, BFS, Dijkstra.
 class Solution:
-    def networkDelayTime(self, times, n, k):
-        """
-        :type times: List[List[int]]
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
-        graph = [[] for _ in range(n + 1)]
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        graph = [[] for _ in range(N + 1)]
 
         for u, v, w in times:
             graph[u].append((v, w))
 
-        q = [(0, k)]
-        visited = [False] * (n + 1)
+        q = [(0, K)]
+        visited = [False] * (N + 1)
         num_visited = 0
         longest_path = 0
 
@@ -27,23 +23,23 @@ class Solution:
 
             if visited[curr]:
                 continue
-
             visited[curr] = True
             num_visited += 1
+
             longest_path = path_len
 
-            for next, edge_len in graph[curr]:
-                if not visited[next]:
-                    heapq.heappush(q, (path_len + edge_len, next))
+            for nxt, edge_len in graph[curr]:
+                if not visited[nxt]:
+                    heapq.heappush(q, (path_len + edge_len, nxt))
 
-        return longest_path if num_visited == n else -1
+        return longest_path if num_visited == N else -1
 
 
 class Test(unittest.TestCase):
     def test(self):
         cases = utils.load_test_json(__file__).test_cases
 
-        for i, case in enumerate(cases):
+        for case in cases:
             args = str(case.args)
             actual = Solution().networkDelayTime(**case.args.__dict__)
             self.assertEqual(case.expected, actual, msg=args)
