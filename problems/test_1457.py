c95872b7c4234d8a8baf6f034adbf0d3d0d7cad6
Chris Xue <chrisxue815@gmail.com>
2020-05-27 23:27:18 -0700

    1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ problems/test_1457.py
@@ -0,0 +1,41 @@
+import unittest
+import utils
+from tree import TreeNode
+
+
+# O(n) time. O(log(n)) space. Recursive DFS.
+class Solution:
+    def pseudoPalindromicPaths(self, root: TreeNode) -> int:
+        count = [0] * 10
+
+        def dfs(curr):
+            if not curr:
+                return 0
+
+            count[curr.val] += 1
+
+            if curr.left or curr.right:
+                result = dfs(curr.left) + dfs(curr.right)
+            else:
+                num_odds = sum(c & 1 for c in count)
+                result = 1 if num_odds <= 1 else 0
+
+            count[curr.val] -= 1
+            return result
+
+        return dfs(root)
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            args = str(case.args)
+            root = TreeNode.from_array(case.args.root)
+            actual = Solution().pseudoPalindromicPaths(root)
+            self.assertEqual(case.expected, actual, msg=args)
+
+
+if __name__ == '__main__':
+    unittest.main()
