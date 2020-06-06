1086169765bf814bcff16cfd2d7060ed241ee501
Chris Xue <chrisxue815@gmail.com>
2020-06-05 19:36:34 -0700

    94. Binary Tree Inorder Traversal

---
+++ problems/test_0094_stack_and_node_single_loop.py
@@ -0,0 +1,39 @@
+import unittest
+from typing import List
+
+import utils
+from tree import TreeNode
+
+
+# O(n) time. O(log(n)) space. Iterative in-order DFS, stack.
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                result.append(curr.val)
+                curr = curr.right
+
+        return result
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        cases = utils.load_test_json(__file__).test_cases
+
+        for case in cases:
+            args = str(case.args)
+            root = TreeNode.from_array(case.args.root)
+            actual = Solution().inorderTraversal(root)
+            self.assertEqual(case.expected, actual, msg=args)
+
+
+if __name__ == '__main__':
+    unittest.main()
