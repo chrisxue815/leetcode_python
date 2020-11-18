161a4c8502102f430ea02468337ddadaf3a7059a
Chris Xue <chrisxue815@gmail.com>
2020-11-18 15:16:16 -0800

    654. Maximum Binary Tree

--- problems/test_0654.py
+++ problems/test_0654.py
@@ -1,33 +1,37 @@
 import unittest
+from typing import List
+
+import utils
 from tree import TreeNode
 
 
+# O(nlog(n)) time. O(log(n)) space. Recursive post-order DFS.
 class Solution:
-    def constructMaximumBinaryTree(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: TreeNode
-        """
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
         if not nums:
             return None
+
         max_i, max_num = 0, nums[0]
         for i, num in enumerate(nums):
-            if num > max_num:
+            if max_num < num:
                 max_i, max_num = i, num
+
         root = TreeNode(max_num)
         root.left = self.constructMaximumBinaryTree(nums[:max_i])
         root.right = self.constructMaximumBinaryTree(nums[max_i + 1:])
+
         return root
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([3, 2, 1, 6, 0, 5], [6, 3, 5, None, 2, 0, None, None, 1])
+        cases = utils.load_test_json(__file__).test_cases
 
-    def _test(self, nums, expected):
-        actual = Solution().constructMaximumBinaryTree(nums)
-        actual = actual.to_array()
-        self.assertEqual(expected, actual)
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().constructMaximumBinaryTree(**case.args.__dict__)
+            actual = actual.to_array()
+            self.assertEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
