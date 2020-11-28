acfa7fb3ddbe2ad3e806519003687d92b19dea18
Chris Xue <chrisxue815@gmail.com>
2020-11-27 23:52:27 -0800

    611. Valid Triangle Number

--- problems/test_0611.py
+++ problems/test_0611.py
@@ -1,19 +1,20 @@
 import unittest
+from typing import List
+
+import utils
 
 
+# O(n^2) time. O(1) space. Sort, counting pairs in a sorted array.
 class Solution:
-    def triangleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort()
+    def triangleNumber(self, nums: List[int]) -> int:
         result = 0
+        nums.sort()
 
-        for i in range(len(nums) - 1, 1, -1):
+        for i in range(2, len(nums)):
             side = nums[i]
             lo = 0
             hi = i - 1
+
             while lo < hi:
                 if nums[lo] + nums[hi] > side:
                     result += hi - lo
@@ -26,11 +27,12 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([2, 2, 3, 4], 3)
+        cases = utils.load_test_json(__file__).test_cases
 
-    def _test(self, n, expected):
-        actual = Solution().triangleNumber(n)
-        self.assertEqual(expected, actual)
+        for case in cases:
+            args = str(case.args)
+            actual = Solution().triangleNumber(**case.args.__dict__)
+            self.assertEqual(case.expected, actual, msg=args)
 
 
 if __name__ == '__main__':
