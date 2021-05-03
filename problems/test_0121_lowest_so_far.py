bd7012e4d7df0b30c4016e649c73a882c65f91df
Chris Xue <chrisxue815@gmail.com>
2021-05-02 22:38:19 -0700

    121. Best Time to Buy and Sell Stock

--- problems/test_0121_lowest_so_far.py
+++ problems/test_0121_lowest_so_far.py
@@ -4,17 +4,17 @@ from typing import List
 import utils
 
 
-# O(n) time. O(1) space. Space-optimized DP.
+# O(n) time. O(1) space. Keeping the lowest price.
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        sell = 0
-        buy = -0x80000000
+        result = 0
+        lowest_so_far = 0x7FFFFFFF
 
         for price in prices:
-            sell = max(sell, buy + price)
-            buy = max(buy, -price)
+            result = max(result, price - lowest_so_far)
+            lowest_so_far = min(lowest_so_far, price)
 
-        return sell
+        return result
 
 
 class Test(unittest.TestCase):
