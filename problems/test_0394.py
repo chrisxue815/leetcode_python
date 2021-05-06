18ece2a82adfc43bf676c238fed807a688ed09be
Chris Xue <chrisxue815@gmail.com>
2021-05-05 22:18:53 -0700

    394. Decode String

--- problems/test_0394.py
+++ problems/test_0394.py
@@ -1,47 +1,36 @@
 import unittest
 
+import utils
 
+
+# O(n) time. O(n) space. Stack.
 class Solution:
-    def decodeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        n = len(s)
+    def decodeString(self, s: str) -> str:
         stack = []
         buf = []
-        i = 0
         k = 0
-        while i < n:
-            ch = s[i]
-            if ch.isdigit():
-                k = k * 10 + ord(ch) - ord('0')
-            elif ch == '[':
+
+        for c in s:
+            if c.isdigit():
+                k = k * 10 + ord(c) - ord('0')
+            elif c == '[':
                 stack.append((buf, k))
                 buf = []
                 k = 0
-            elif ch == ']':
+            elif c == ']':
                 sub = ''.join(buf)
                 buf, k = stack.pop()
                 buf.append(sub * k)
                 k = 0
             else:
-                buf.append(ch)
-            i += 1
+                buf.append(c)
 
         return ''.join(buf)
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test('3[a]2[bc]', 'aaabcbc')
-        self._test('3[a2[c]]', 'accaccacc')
-        self._test('2[abc]3[cd]ef', 'abcabccdcdcdef')
-        self._test('2[a2[bc]]gh2[cd]ef', 'abcbcabcbcghcdcdef')
-
-    def _test(self, s, expected):
-        actual = Solution().decodeString(s)
-        self.assertEqual(expected, actual)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
