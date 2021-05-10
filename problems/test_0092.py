5967c9f631d108a2f2c3f0ad5ebf3d5cf9deb23c
Chris Xue <chrisxue815@gmail.com>
2021-05-09 18:52:10 -0700

    92. Reverse Linked List II

--- problems/test_0092.py
+++ problems/test_0092.py
@@ -1,54 +1,40 @@
 import unittest
+
+import utils
 from linkedlist import ListNode
 
 
+# O(n) time. O(1) space.
 class Solution:
-    def reverseBetween(self, head, m, n):
-        """
-        :type head: ListNode
-        :type m: int
-        :type n: int
-        :rtype: ListNode
-        """
-        result = ListNode(0)
-        result.next = head
-        head = result
-        for _ in range(m - 1):
-            head = head.next
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        dummy = start = ListNode(0)
+        dummy.next = head
 
-        left = head
-        prev = None
-        head = head.next
-        right = head
-        for _ in range(n - m + 1):
-            next_ = head.next
-            head.next = prev
-            prev = head
-            head = next_
-        left.next = prev
-        right.next = head
+        for _ in range(left - 1):
+            start = start.next
 
-        return result.next
+        end = start.next
+        prev = end
+        curr = prev.next
+
+        for _ in range(right - left):
+            next_ = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_
+
+        start.next = prev
+        end.next = curr
+
+        return dummy.next
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([1, 2, 3, 4, 5], 2, 4, [1, 4, 3, 2, 5])
-        self._test([1, 2, 3, 4, 5], 1, 5, [5, 4, 3, 2, 1])
-        self._test([1, 2, 3, 4, 5], 1, 3, [3, 2, 1, 4, 5])
-        self._test([1, 2, 3, 4, 5], 3, 5, [1, 2, 5, 4, 3])
-        self._test([1, 2, 3, 4, 5], 5, 5, [1, 2, 3, 4, 5])
-        self._test([1, 2, 3, 4, 5], 1, 1, [1, 2, 3, 4, 5])
-        self._test([1], 1, 1, [1])
+        utils.test(self, __file__, Solution,
+                   process_args=self.process_args,
+                   process_result=utils.linked_list_to_array)
 
-    def _test(self, head, m, n, expected):
-        head = ListNode.from_array(head)
-
-        actual = Solution().reverseBetween(head, m, n)
-
-        actual = ListNode.to_array(actual)
-        self.assertEqual(expected, actual)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    @staticmethod
+    def process_args(args):
+        args.head = ListNode.from_array(args.head)
