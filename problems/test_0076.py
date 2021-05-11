3f3e6387b0cff603b89bd56694416ef8a2ca6dbf
Chris Xue <chrisxue815@gmail.com>
2021-05-10 23:30:12 -0700

    76. Minimum Window Substring

--- problems/test_0076.py
+++ problems/test_0076.py
@@ -1,72 +1,38 @@
 import unittest
 
+import utils
 
+
+# O(n) time. O(1) space. Counting, sliding window.
 class Solution:
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
-        if not t or len(s) < len(t):
-            return ''
-
-        if len(t) == 1:
-            return t if t in s else ''
-
+    def minWindow(self, s: str, t: str) -> str:
         counts = [0] * 128
+        for c in t:
+            counts[ord(c)] -= 1
 
-        for ch in t:
-            counts[ord(ch)] += 1
-
+        result_length = 0x7FFFFFFF
+        result_lo = 0
+        result_hi = 0
         lo = 0
-        while lo < len(s) and counts[ord(s[lo])] == 0:
-            lo += 1
+        pending = sum(1 for count in counts if count < 0)
 
-        num_pending = len(t)
-        min_lo = 0
-        min_len = len(s) + 1
+        for hi, c in enumerate(s):
+            counts[ord(c)] += 1
+            if counts[ord(c)] == 0:
+                pending -= 1
 
-        for hi in range(lo, len(s)):
-            ch = ord(s[hi])
+            while lo <= hi and counts[ord(s[lo])] > 0:
+                counts[ord(s[lo])] -= 1
+                lo += 1
 
-            counts[ch] -= 1
-            if counts[ch] >= 0:
-                num_pending -= 1
+            if pending == 0 and result_length > hi - lo:
+                result_length = hi - lo
+                result_lo = lo
+                result_hi = hi + 1
 
-            if num_pending == 0:
-                while True:
-                    ch = ord(s[lo])
-                    lo += 1
-                    counts[ch] += 1
-                    if counts[ch] == 1:
-                        num_pending = 1
-                        break
-
-                new_len = hi - lo
-                if new_len < min_len:
-                    min_len = new_len
-                    min_lo = lo
-
-        if min_len > len(s):
-            return ''
-        return s[min_lo - 1:min_lo + min_len + 1]
+        return s[result_lo:result_hi]
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test('ADOBECODEBANC', 'ABC', 'BANC')
-        self._test('this is a test string', 'tist', 't stri')
-        self._test('geeksforgeeks', 'ork', 'ksfor')
-        self._test('abc', '', '')
-        self._test('abc', 'b', 'b')
-        self._test('abc', 'd', '')
-        self._test('abc', 'abcd', '')
-
-    def _test(self, s, t, expected):
-        actual = Solution().minWindow(s, t)
-        self.assertEqual(expected, actual)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        utils.test(self, __file__, Solution)
