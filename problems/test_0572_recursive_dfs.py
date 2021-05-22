97b1eba3cca1fed7efd6efac7cc9828ec604eff3
Chris Xue <chrisxue815@gmail.com>
2021-05-21 23:01:33 -0700

    572. Subtree of Another Tree

---
+++ problems/test_0572_recursive_dfs.py
@@ -0,0 +1,58 @@
+import unittest
+
+import utils
+from tree import TreeNode
+
+
+def height(root):
+    if not root:
+        return -1
+    return 1 + max(height(root.left), height(root.right))
+
+
+def match(a, b):
+    if not a:
+        return not b
+    if not b:
+        return False
+    return a.val == b.val and match(a.left, b.left) and match(a.right, b.right)
+
+
+# O(n) time. O(log(n)) space. Recursive DFS.
+class Solution:
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        subtree_height = height(subRoot)
+
+        def dfs(curr):
+            if not curr:
+                return False, -1
+
+            left_match, left_height = dfs(curr.left)
+            if left_match:
+                return True, -1
+
+            right_match, right_height = dfs(curr.right)
+            if right_match:
+                return True, -1
+
+            curr_height = 1 + max(left_height, right_height)
+            if curr_height == subtree_height and match(curr, subRoot):
+                return True, -1
+
+            return False, curr_height
+
+        return dfs(root)[0]
+
+
+class Test(unittest.TestCase):
+    def test(self):
+        utils.test(self, __file__, Solution, process_args=self.process_args)
+
+    @staticmethod
+    def process_args(args):
+        args.root = TreeNode.from_array(args.root)
+        args.subRoot = TreeNode.from_array(args.subRoot)
+
+
+if __name__ == '__main__':
+    unittest.main()
