ce2382d9fdf642e8d517c204ff092dba3fd8b6f9
Chris Xue <chrisxue815@gmail.com>
2021-06-01 00:15:01 -0700

    997. Find the Town Judge

--- problems/test_0997.py
+++ problems/test_0997.py
@@ -1,37 +1,26 @@
 import unittest
+from typing import List
+
 import utils
 
 
-# O(T + N) time. O(N) space. Graph.
+# O(len(trust) + n) time. O(n) space. Graph.
 class Solution:
-    def findJudge(self, N, trust):
-        """
-        :type N: int
-        :type trust: List[List[int]]
-        :rtype: int
-        """
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
         # in-degree - out-degree
-        count = [0] * (N + 1)
+        counts = [0] * (n + 1)
+        counts[0] = -2
 
         for a, b in trust:
-            count[a] -= 1
-            count[b] += 1
+            counts[a] -= 1
+            counts[b] += 1
 
-        for i in range(1, N + 1):
-            if count[i] == N - 1:
-                return i
-
-        return -1
+        return next((i for i, count in enumerate(counts) if count == n - 1), -1)
 
 
 class Test(unittest.TestCase):
     def test(self):
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            actual = Solution().findJudge(**case.args.__dict__)
-            self.assertEqual(case.expected, actual, msg=args)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
