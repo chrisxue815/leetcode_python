94ef612c5e0e77747274788cfd859b14737c8b54
Chris Xue <chrisxue815@gmail.com>
2021-06-05 02:43:28 -0700

    684. Redundant Connection

--- problems/test_0684_union_find.py
+++ problems/test_0684_union_find.py
@@ -1,20 +1,22 @@
 import unittest
+from typing import List
+
 import utils
 
 
 class UnionFind:
     def __init__(self, n):
-        self._id = list(range(n))
-        self._sz = [1] * n
-        self._count = n
+        self.parents = list(range(n))
+        self.sizes = [1] * n
+        self.count = n
 
     def find(self, p):
         root = p
-        while root != self._id[root]:
-            root = self._id[root]
+        while root != self.parents[root]:
+            root = self.parents[root]
 
         while p != root:
-            p, self._id[p] = self._id[p], root
+            p, self.parents[p] = self.parents[p], root
 
         return root
 
@@ -25,29 +27,22 @@ class UnionFind:
         if p == q:
             return False
 
-        if self._sz[p] < self._sz[q]:
+        if self.sizes[p] < self.sizes[q]:
             p, q = q, p
 
-        self._id[q] = p
-        self._sz[p] += self._sz[q]
-        self._count -= 1
+        self.parents[q] = p
+        self.sizes[p] += self.sizes[q]
+        self.count -= 1
 
         return True
 
     def connected(self, p, q):
         return self.find(p) == self.find(q)
 
-    def count(self):
-        return self._count
-
 
 # O(nlog(n)) time. O(n) space. Union-find.
 class Solution:
-    def findRedundantConnection(self, edges):
-        """
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         uf = UnionFind(len(edges) + 1)
 
         for edge in edges:
@@ -57,12 +52,7 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test(self):
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            actual = Solution().findRedundantConnection(**case.args.__dict__)
-            self.assertEqual(case.expected, actual, msg=args)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
