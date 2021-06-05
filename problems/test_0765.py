4403fe37db4e2cd413c7d2dfb194b3d6f39a7a7d
Chris Xue <chrisxue815@gmail.com>
2021-06-05 03:26:46 -0700

    765. Couples Holding Hands

--- problems/test_0765.py
+++ problems/test_0765.py
@@ -1,14 +1,12 @@
 import unittest
+from typing import List
+
 import utils
 
 
 # O(n) time. O(n) space. Greedy.
 class Solution:
-    def minSwapsCouples(self, row):
-        """
-        :type row: List[int]
-        :rtype: int
-        """
+    def minSwapsCouples(self, row: List[int]) -> int:
         seats = [0] * len(row)
         for seat, person in enumerate(row):
             seats[person] = seat
@@ -22,20 +20,17 @@ class Solution:
             if person_a != person_c:
                 count += 1
                 seat_c = seats[person_c]
-                row[seat_a], row[seat_c] = person_c, person_a
-                seats[person_a], seats[person_c] = seat_c, seat_a
+                row[seat_a] = person_c
+                row[seat_c] = person_a
+                seats[person_a] = seat_c
+                seats[person_c] = seat_a
 
         return count
 
 
 class Test(unittest.TestCase):
     def test(self):
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            actual = Solution().minSwapsCouples(**case.args.__dict__)
-            self.assertEqual(case.expected, actual, msg=args)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
