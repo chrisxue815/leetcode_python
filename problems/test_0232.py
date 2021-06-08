a624db7829a1e9bad7e5a6579db9ad2d6b104af8
Chris Xue <chrisxue815@gmail.com>
2021-06-07 21:13:19 -0700

    232. Implement Queue using Stacks

--- problems/test_0232.py
+++ problems/test_0232.py
@@ -3,62 +3,57 @@ import unittest
 import utils
 
 
+# Stack.
 class MyQueue:
 
+    # O(1) time. O(1) space.
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self._push_stack = []
-        self._pop_stack = []
+        self.push_stack = []
+        self.pop_stack = []
 
+    # O(1) time. O(1) space.
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-        self._push_stack.append(x)
+        self.push_stack.append(x)
 
+    # O(n) time. O(1) space.
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
-        self._refill_pop_stack()
-        return self._pop_stack.pop()
+        self.refill_pop_stack()
+        return self.pop_stack.pop()
 
+    # O(n) time. O(1) space.
     def peek(self) -> int:
         """
         Get the front element.
         """
-        self._refill_pop_stack()
-        return self._pop_stack[-1]
+        self.refill_pop_stack()
+        return self.pop_stack[-1]
 
-    def _refill_pop_stack(self):
-        if not self._pop_stack:
-            while self._push_stack:
-                self._pop_stack.append(self._push_stack.pop())
+    # O(n) time. O(1) space.
+    def refill_pop_stack(self):
+        if not self.pop_stack:
+            while self.push_stack:
+                self.pop_stack.append(self.push_stack.pop())
 
+    # O(1) time. O(1) space.
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-        return not self._push_stack and not self._pop_stack
+        return not self.push_stack and not self.pop_stack
 
 
 class Test(unittest.TestCase):
     def test(self):
-        cls = MyQueue
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            obj = None
-
-            for func, parameters, expected in zip(case.functions, case.args, case.expected):
-                if func == cls.__name__:
-                    obj = cls(*parameters)
-                else:
-                    actual = getattr(obj, func)(*parameters)
-                    self.assertEqual(expected, actual, msg=args)
+        utils.test_invocations(self, __file__, MyQueue)
 
 
 if __name__ == '__main__':
