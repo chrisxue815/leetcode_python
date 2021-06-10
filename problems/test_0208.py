03e6139ff3be83524489f750f99d8823bcfa1412
Chris Xue <chrisxue815@gmail.com>
2021-06-10 08:13:57 -0700

    208. Implement Trie (Prefix Tree)

--- problems/test_0208.py
+++ problems/test_0208.py
@@ -63,19 +63,7 @@ class Trie:
 
 class Test(unittest.TestCase):
     def test(self):
-        cls = Trie
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            obj = None
-
-            for func, parameters, expected in zip(case.functions, case.args, case.expected):
-                if func == cls.__name__:
-                    obj = cls(*parameters)
-                else:
-                    actual = getattr(obj, func)(*parameters)
-                    self.assertEqual(expected, actual, msg=args)
+        utils.test_invocations(self, __file__, Trie)
 
 
 if __name__ == '__main__':
