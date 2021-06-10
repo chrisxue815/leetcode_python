3cbe9634a5f17096345af651874b1c1d0f534f63
Chris Xue <chrisxue815@gmail.com>
2021-06-10 08:12:24 -0700

    211. Design Add and Search Words Data Structure

--- problems/test_0211.py
+++ problems/test_0211.py
@@ -12,9 +12,6 @@ class WordDictionary:
         self.root = {}
 
     def addWord(self, word: str) -> None:
-        """
-        Adds a word into the data structure.
-        """
         curr = self.root
 
         for ch in word:
@@ -26,9 +23,6 @@ class WordDictionary:
         curr['#'] = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
-        """
 
         def search(curr, start):
             for i in range(start, len(word)):
@@ -50,19 +44,7 @@ class WordDictionary:
 
 class Test(unittest.TestCase):
     def test(self):
-        cls = WordDictionary
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            obj = None
-
-            for func, parameters, expected in zip(case.functions, case.args, case.expected):
-                if func == cls.__name__:
-                    obj = cls(*parameters)
-                else:
-                    actual = getattr(obj, func)(*parameters)
-                    self.assertEqual(expected, actual, msg=args)
+        utils.test_invocations(self, __file__, WordDictionary)
 
 
 if __name__ == '__main__':
