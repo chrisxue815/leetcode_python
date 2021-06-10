d8e9dd122dc47cb3011a49b80a4c4d7a5a5a7c0a
Chris Xue <chrisxue815@gmail.com>
2021-06-10 08:10:43 -0700

    155. Min Stack

--- problems/test_0155.py
+++ problems/test_0155.py
@@ -1,5 +1,7 @@
 import unittest
 
+import utils
+
 
 class MinStack:
     def __init__(self):
@@ -8,51 +10,24 @@ class MinStack:
         """
         self.stack = []
 
-    def push(self, x):
-        """
-        :type x: int
-        :rtype: void
-        """
-        min_ = x if not self.stack or x < self.stack[-1][1] else self.stack[-1][1]
-        self.stack.append((x, min_))
+    def push(self, val: int) -> None:
+        min_ = val if not self.stack or val < self.stack[-1][1] else self.stack[-1][1]
+        self.stack.append((val, min_))
 
-    def pop(self):
-        """
-        :rtype: void
-        """
+    def pop(self) -> None:
         if self.stack:
             self.stack.pop()
 
-    def top(self):
-        """
-        :rtype: int
-        """
+    def top(self) -> int:
         return self.stack[-1][0] if self.stack else None
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
+    def getMin(self) -> int:
         return self.stack[-1][1] if self.stack else None
 
 
 class Test(unittest.TestCase):
     def test(self):
-        stack = MinStack()
-        stack.push(-2)
-        stack.push(0)
-        stack.push(-3)
-
-        self.assertEqual(-3, stack.top())
-        self.assertEqual(-3, stack.getMin())
-
-        stack.pop()
-        self.assertEqual(0, stack.top())
-        self.assertEqual(-2, stack.getMin())
-
-        stack.pop()
-        self.assertEqual(-2, stack.top())
-        self.assertEqual(-2, stack.getMin())
+        utils.test_invocations(self, __file__, MinStack)
 
 
 if __name__ == '__main__':
