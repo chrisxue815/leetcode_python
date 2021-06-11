988e36a90a17de178b60b0afd2b3d23299b34c5a
Chris Xue <chrisxue815@gmail.com>
2021-06-10 18:36:12 -0700

    706. Design HashMap

--- problems/test_0706.py
+++ problems/test_0706.py
@@ -20,12 +20,9 @@ class MyHashMap:
         """
         self.table = [None] * CAPACITY
 
-    def put(self, key, value):
+    def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
-        :type key: int
-        :type value: int
-        :rtype: void
         """
         h = hash(key)
         bucket_index = h % CAPACITY
@@ -46,11 +43,9 @@ class MyHashMap:
         else:
             self.table[bucket_index] = curr
 
-    def get(self, key):
+    def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
         """
         h = hash(key)
         bucket_index = h % CAPACITY
@@ -63,11 +58,9 @@ class MyHashMap:
 
         return -1
 
-    def remove(self, key):
+    def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: void
         """
         h = hash(key)
         bucket_index = h % CAPACITY
@@ -88,19 +81,7 @@ class MyHashMap:
 
 class Test(unittest.TestCase):
     def test(self):
-        cls = MyHashMap
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            obj = None
-
-            for func, parameters, expected in zip(case.functions, case.args, case.expected):
-                if func == cls.__name__:
-                    obj = cls(*parameters)
-                else:
-                    actual = getattr(obj, func)(*parameters)
-                    self.assertEqual(expected, actual, msg=args)
+        utils.test_invocations(self, __file__, MyHashMap)
 
 
 if __name__ == '__main__':
