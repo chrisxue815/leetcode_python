b8760661bebaed8a0612df1e9b6f02465b42335f
Chris Xue <chrisxue815@gmail.com>
2021-06-15 19:42:43 -0700

    452. Minimum Number of Arrows to Burst Balloons

--- problems/test_0452_sort_by_end_desc.py
+++ problems/test_0452_sort_by_end_desc.py
@@ -26,12 +26,7 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test(self):
-        cases = utils.load_test_json(__file__).test_cases
-
-        for case in cases:
-            args = str(case.args)
-            actual = Solution().findMinArrowShots(**case.args.__dict__)
-            self.assertEqual(case.expected, actual, msg=args)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
