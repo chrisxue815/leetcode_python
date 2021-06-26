ba05cf8b05c1bbae9cd6adf1a16f7c26b137829a
Chris Xue <chrisxue815@gmail.com>
2021-06-25 21:46:10 -0700

    300. Longest Increasing Subsequence

--- problems/test_0300.py
+++ problems/test_0300.py
@@ -1,42 +1,36 @@
 import unittest
+from typing import List
+
+import utils
 
 
+# O(nlog(n)) time. O(n) space. Binary search.
 class Solution:
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def lengthOfLIS(self, nums: List[int]) -> int:
         last_item_in_sequence = []
-        for i in nums:
+        for num in nums:
             lo = 0
             hi = len(last_item_in_sequence) - 1
             while lo <= hi:
                 mid = lo + ((hi - lo) >> 1)
                 mid_val = last_item_in_sequence[mid]
-                if mid_val < i:
+                if mid_val < num:
                     lo = mid + 1
-                elif mid_val > i:
+                elif mid_val > num:
                     hi = mid - 1
                 else:
                     lo = mid
                     break
             if lo == len(last_item_in_sequence):
-                last_item_in_sequence.append(i)
+                last_item_in_sequence.append(num)
             else:
-                last_item_in_sequence[lo] = i
+                last_item_in_sequence[lo] = num
         return len(last_item_in_sequence)
 
 
 class Test(unittest.TestCase):
     def test(self):
-        self._test([10, 9, 2, 5, 3, 7, 101, 18], 4)
-        self._test([10, 9, 2, 8, 3, 7, 101, 18], 4)
-        self._test([10, 9, 2, 8, 3, 7, 101, 101, 18], 4)
-
-    def _test(self, nums, expected):
-        actual = Solution().lengthOfLIS(nums)
-        self.assertEqual(expected, actual)
+        utils.test(self, __file__, Solution)
 
 
 if __name__ == '__main__':
