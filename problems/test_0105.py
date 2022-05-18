10f3406df01f365196c7795b443efb957fc6d64b
Chris Xue <chrisxue815@gmail.com>
2022-05-18 04:17:29 -0400

    Update

--- problems/test_0105.py
+++ problems/test_0105.py
@@ -1,52 +1,36 @@
 import unittest
+from typing import List, Optional
+
+import utils
 from tree import TreeNode
 
 
+# O(n*2) time. O(log(n)) space. DFS
 class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def dfs(preorder_index, inorder_left, inorder_right):
+            if inorder_left >= inorder_right:
+                return None
 
-    def __init__(self):
-        self.preorder = None
-        self.inorder = None
+            root_val = preorder[preorder_index]
 
-    def buildTree(self, preorder, inorder):
-        """
-        :type preorder: List[int]
-        :type inorder: List[int]
-        :rtype: TreeNode
-        """
-        self.preorder = preorder
-        self.inorder = inorder
-        return self._build(0, 0, len(inorder))
+            for i in range(inorder_left, inorder_right):
+                if inorder[i] == root_val:
+                    break
 
-    def _build(self, preorder_index, inorder_left, inorder_right):
-        if inorder_left >= inorder_right:
-            return None
+            left_tree_size = i - inorder_left
+            root = TreeNode(root_val)
+            root.left = dfs(preorder_index + 1, inorder_left, i)
+            root.right = dfs(preorder_index + left_tree_size + 1, i + 1, inorder_right)
 
-        root_val = self.preorder[preorder_index]
+            return root
 
-        for i in range(inorder_left, inorder_right):
-            if self.inorder[i] == root_val:
-                break
-
-        root = TreeNode(root_val)
-        root.left = self._build(preorder_index + 1, inorder_left, i)
-
-        left_tree_size = i - inorder_left
-        root.right = self._build(preorder_index + left_tree_size + 1,
-                                 i + 1, inorder_right)
-        return root
+        return dfs(0, 0, len(inorder))
 
 
 class Test(unittest.TestCase):
-
     def test(self):
-        self._test(
-            [4, 2, 1, 3, 6, 5, 7],
-            [1, 2, 3, 4, 5, 6, 7],
-            [4, 2, 6, 1, 3, 5, 7])
-
-    def _test(self, preorder, inorder, levelorder):
-        self.assertEqual(levelorder, Solution().buildTree(preorder, inorder).to_array())
+        utils.test(self, __file__, Solution, process_result=TreeNode.to_array_static)
 
 
 if __name__ == '__main__':
