8c5ae82b791d1db5fad902f7bdb3b8e18c2f6387
Chris Xue <chrisxue815@gmail.com>
2022-05-24 12:46:08 -0400

    355. Design Twitter

--- problems/test_0355.py
+++ problems/test_0355.py
@@ -13,33 +13,21 @@ class User:
 
 class Twitter:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.users = collections.defaultdict(User)
         self.timer = itertools.count(step=-1)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        """
-        Compose a new tweet.
-        """
         tweets = self.users[userId].tweets
         if len(tweets) >= 10:
             tweets.pop()
         tweets.appendleft((next(self.timer), tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        """
-        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
-        """
         user = self.users[userId]
         tweets = heapq.merge(*(followee.tweets for followee in user.followees))
         return [x[1] for x in itertools.islice(tweets, 10)]
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        """
-        Follower follows a followee. If the operation is invalid, it should be a no-op.
-        """
         if followerId == followeeId:
             return
         follower = self.users[followerId]
@@ -47,9 +35,6 @@ class Twitter:
         follower.followees.add(followee)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        """
-        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
-        """
         if followerId == followeeId:
             return
         follower = self.users[followerId]
