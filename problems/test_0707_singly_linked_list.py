138679c150922128441d4ce0bc2990ae9eec3b1e
Chris Xue <chrisxue815@gmail.com>
2022-05-24 12:47:57 -0400

    707. Design LinkedList

--- problems/test_0707_singly_linked_list.py
+++ problems/test_0707_singly_linked_list.py
@@ -11,15 +11,9 @@ class ListNode:
 
 class MyLinkedList:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.head = None
 
     def get(self, index: int) -> int:
-        """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        """
         if not self.head:
             return -1
 
@@ -33,17 +27,11 @@ class MyLinkedList:
         return curr.val
 
     def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-        """
         inserted = ListNode(val)
         inserted.next = self.head
         self.head = inserted
 
     def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
         inserted = ListNode(val)
 
         if self.head:
@@ -55,9 +43,6 @@ class MyLinkedList:
             self.head = inserted
 
     def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-        """
         if index == 0:
             self.addAtHead(val)
             return
@@ -77,9 +62,6 @@ class MyLinkedList:
         curr.next = inserted
 
     def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
         if index == 0:
             if self.head:
                 self.head = self.head.next
