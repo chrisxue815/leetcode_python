211db8ee039ffc3f04a4aa78c7d4214d639bc83e
Chris Xue <chrisxue815@gmail.com>
2022-05-26 01:10:53 -0400

    432. All O'one Data Structure

--- problems/test_0432.py
+++ problems/test_0432.py
@@ -8,16 +8,10 @@ import utils
 class AllOne:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.counts = {}
         self.keys = sortedcontainers.SortedDict()
 
     def inc(self, key: str) -> None:
-        """
-        Inserts a new key <Key> with value 1. Or increments an existing key by 1.
-        """
         if key in self.counts:
             count = self.counts[key]
             keys = self.keys[count]
@@ -37,9 +31,6 @@ class AllOne:
         keys.add(key)
 
     def dec(self, key: str) -> None:
-        """
-        Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
-        """
         count = self.counts[key]
         keys = self.keys[count]
         keys.remove(key)
@@ -58,15 +49,9 @@ class AllOne:
             del self.counts[key]
 
     def getMaxKey(self) -> str:
-        """
-        Returns one of the keys with maximal value.
-        """
         return next(iter(self.keys.peekitem()[1])) if self.keys else ''
 
     def getMinKey(self) -> str:
-        """
-        Returns one of the keys with Minimal value.
-        """
         return next(iter(self.keys.peekitem(0)[1])) if self.keys else ''
 
 
