13c3effd87f6bb76c63640b93df2f6d54aef0478
Chris Xue <chrisxue815@gmail.com>
2022-05-26 18:57:12 -0400

    706. Design HashMap

--- problems/test_0706.py
+++ problems/test_0706.py
@@ -15,15 +15,9 @@ class Entry:
 
 class MyHashMap:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.table = [None] * CAPACITY
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
         h = hash(key)
         bucket_index = h % CAPACITY
         curr = self.table[bucket_index]
@@ -44,9 +38,6 @@ class MyHashMap:
             self.table[bucket_index] = curr
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
         h = hash(key)
         bucket_index = h % CAPACITY
         curr = self.table[bucket_index]
@@ -59,9 +50,6 @@ class MyHashMap:
         return -1
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
         h = hash(key)
         bucket_index = h % CAPACITY
         curr = self.table[bucket_index]
